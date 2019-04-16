15c02b1335d61ae8e73c74ddef505ec8d366f175
Bruce Lv <lvkun@outlook.com>
2019-04-17 00:12:47 +0800

    solve longest_substring_without_repeating_characters.

--- src/site/lvkun/leetcode/longest_substring_without_repeating_characters/Solution.kt
+++ src/site/lvkun/leetcode/longest_substring_without_repeating_characters/Solution.kt
@@ -1,2 +1,23 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.longest_substring_without_repeating_characters
 
+class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+        if (s.isEmpty()) {
+            return 0
+        }
+
+        val map = HashMap<Char, Int>()
+        var length = 1
+        var start = 0
+        for (i in s.indices) {
+            val c = s[i]
+            if (map.containsKey(c)) {
+                start = Math.max(map[c]!! + 1, start)
+            }
+            length = Math.max(i - start + 1, length)
+            map[c] = i
+        }
+
+        return length
+    }
+}
--- src/site/lvkun/leetcode/longest_substring_without_repeating_characters/SolutionTest.kt
+++ src/site/lvkun/leetcode/longest_substring_without_repeating_characters/SolutionTest.kt
@@ -1,2 +1,25 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.longest_substring_without_repeating_characters
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testLengthOfLongestSubstring() {
+        val solution = Solution()
+        var result = solution.lengthOfLongestSubstring("abcabcbb")
+        Assert.assertEquals(3, result)
+
+        result = solution.lengthOfLongestSubstring("bbbbb")
+        Assert.assertEquals(1, result)
+
+        result = solution.lengthOfLongestSubstring("pwwkew")
+        Assert.assertEquals(3, result)
+
+        result = solution.lengthOfLongestSubstring("au")
+        Assert.assertEquals(2, result)
+
+        result = solution.lengthOfLongestSubstring("abba")
+        Assert.assertEquals(2, result)
+    }
+}
