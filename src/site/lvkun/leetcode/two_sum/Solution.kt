41fea8aca3e65149eb7e439bdbba3cefda46e3be
lvkun <lvkun@xiaomi.com>
2019-04-16 12:38:40 +0800

    solve two_sum.

--- src/site/lvkun/leetcode/two_sum/Solution.kt
+++ src/site/lvkun/leetcode/two_sum/Solution.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.two_sum
 
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = HashMap<Int, Int>()
+
+        for (i in nums.indices) {
+            val r = target - nums[i]
+
+            if (map.containsKey(r)) {
+                return intArrayOf(map[r]!!, i)
+            }
+
+            map[nums[i]] = i
+        }
+
+        return intArrayOf(-1, -1)
+    }
+}
--- src/site/lvkun/leetcode/two_sum/SolutionTest.kt
+++ src/site/lvkun/leetcode/two_sum/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.two_sum
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testTwoSum() {
+        val solution = Solution()
+        val result = solution.twoSum(intArrayOf(2, 7, 11, 15), 9)
+        Assert.assertArrayEquals(intArrayOf(0, 1), result)
+    }
+}
