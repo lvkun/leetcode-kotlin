f38189e1563389b189c086f391b8587a5090d30e
Bruce Lv <lvkun@outlook.com>
2019-04-22 23:21:10 +0800

    add zigzag_conversion.

--- src/site/lvkun/leetcode/zigzag_conversion/Solution.kt
+++ src/site/lvkun/leetcode/zigzag_conversion/Solution.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.zigzag_conversion
 
+class Solution {
+    fun convert(s: String, numRows: Int): String {
+        var result = ArrayList<Char>()
+        for (i in 0..numRows) {
+            var start = i
+            var round = 2 * numRows - 2
+            if (i == 0 || i == numRows) {
+
+            }
+        }
+        return ""
+    }
+}
--- src/site/lvkun/leetcode/zigzag_conversion/SolutionTest.kt
+++ src/site/lvkun/leetcode/zigzag_conversion/SolutionTest.kt
@@ -1,2 +1,16 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.zigzag_conversion
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testConvert() {
+        val solution = Solution()
+        var result = solution.convert("PAYPALISHIRING", 3)
+        Assert.assertEquals("PAHNAPLSIIGYIR", result)
+
+        result = solution.convert("PAYPALISHIRING", 4)
+        Assert.assertEquals("PINALSIGYAHRPI", result)
+    }
+}
