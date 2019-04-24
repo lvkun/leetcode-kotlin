064d9889add2ea62b271ebebe6ff770299be4aaa
Bruce Lv <lvkun@outlook.com>
2019-04-24 22:06:31 +0800

    solve reverse_integer.

--- src/site/lvkun/leetcode/reverse_integer/Solution.kt
+++ src/site/lvkun/leetcode/reverse_integer/Solution.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.reverse_integer
 
+class Solution {
+    fun reverse(x: Int): Int {
+        var r = 0
+        var n = x
+        while (n != 0) {
+            if (Math.abs(r) > Integer.MAX_VALUE / 10) {
+                return 0
+            }
+
+            r = 10 * r + n % 10
+            n /= 10
+        }
+
+        return r
+    }
+}
--- src/site/lvkun/leetcode/reverse_integer/SolutionTest.kt
+++ src/site/lvkun/leetcode/reverse_integer/SolutionTest.kt
@@ -1,2 +1,17 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.reverse_integer
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testReverse() {
+        val solution = Solution()
+        var result = solution.reverse(123)
+        Assert.assertEquals(321, result)
+        result = solution.reverse(-123)
+        Assert.assertEquals(-321, result)
+        result = solution.reverse(120)
+        Assert.assertEquals(21, result)
+    }
+}
