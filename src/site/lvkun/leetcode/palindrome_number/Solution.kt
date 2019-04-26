7aede5445c6fcc8306b8be36a69dc13e3eb05230
lvkun <lvkun@xiaomi.com>
2019-04-26 18:33:01 +0800

    solve palindrome_number.

--- src/site/lvkun/leetcode/palindrome_number/Solution.kt
+++ src/site/lvkun/leetcode/palindrome_number/Solution.kt
@@ -1,2 +1,23 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.palindrome_number
 
+class Solution {
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) {
+            return false
+        }
+
+        var reverse = 0
+        var origin = x
+        while (origin > 0) {
+            val d = origin % 10
+            if (reverse > Integer.MAX_VALUE / 10) {
+                return false
+            }
+
+            reverse = reverse * 10 + d
+            origin /= 10
+        }
+
+        return x == reverse
+    }
+}
--- src/site/lvkun/leetcode/palindrome_number/SolutionTest.kt
+++ src/site/lvkun/leetcode/palindrome_number/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.palindrome_number
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIsPalindrome() {
+        val solution = Solution()
+        Assert.assertTrue(solution.isPalindrome(121))
+        Assert.assertFalse(solution.isPalindrome(-121))
+        Assert.assertFalse(solution.isPalindrome(10))
+    }
+}
