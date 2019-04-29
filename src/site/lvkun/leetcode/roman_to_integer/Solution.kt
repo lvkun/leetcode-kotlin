48c9c8d4af7be4b27eff90597fec5b81e9fcbd3e
lvkun <lvkun@xiaomi.com>
2019-04-29 17:47:33 +0800

    solve roman_to_integer.

--- src/site/lvkun/leetcode/roman_to_integer/Solution.kt
+++ src/site/lvkun/leetcode/roman_to_integer/Solution.kt
@@ -1,7 +1,27 @@
 package site.lvkun.leetcode.roman_to_integer
 
 class Solution {
+    val map = mapOf(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000
+        )
+
     fun romanToInt(s: String): Int {
-        return 0
+        var ret = 0
+
+        for (i in s.indices) {
+            if (i == s.length - 1 || map.getValue(s[i]) >= map.getValue(s[i + 1])) {
+                ret += map.getValue(s[i])
+            } else {
+                ret -= map.getValue(s[i])
+            }
+        }
+
+        return ret
     }
 }
