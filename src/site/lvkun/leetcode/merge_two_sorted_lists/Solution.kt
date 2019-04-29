29e63702d661e8bce4dc4c9b091e6b5a2fc5f925
lvkun <lvkun@xiaomi.com>
2019-04-29 18:23:19 +0800

    solve merge_two_sorted_lists.

--- src/site/lvkun/leetcode/merge_two_sorted_lists/Solution.kt
+++ src/site/lvkun/leetcode/merge_two_sorted_lists/Solution.kt
@@ -1,2 +1,33 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.merge_two_sorted_lists
 
+import site.lvkun.leetcode.common.ListNode
+
+class Solution {
+    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+        val dummy = ListNode(-1)
+        var cur = dummy
+        var n1 = l1
+        var n2 = l2
+        while (n1 != null && n2 != null) {
+            if (n1.value <= n2.value) {
+                cur.next = n1
+                cur = n1
+                n1 = n1.next
+            } else {
+                cur.next = n2
+                cur = n2
+                n2 = n2.next
+            }
+        }
+
+        if (n1 != null) {
+            cur.next = n1
+        }
+
+        if (n2 != null) {
+            cur.next = n2
+        }
+
+        return dummy.next
+    }
+}
--- src/site/lvkun/leetcode/merge_two_sorted_lists/SolutionTest.kt
+++ src/site/lvkun/leetcode/merge_two_sorted_lists/SolutionTest.kt
@@ -1,2 +1,17 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.merge_two_sorted_lists
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.ListNode
+
+class SolutionTest {
+    @Test
+    fun testMergeTwoLists() {
+        var l1 = ListNode.fromString("1->2->4")
+        var l2 = ListNode.fromString("1->3->4")
+
+        val solution = Solution()
+        var result = solution.mergeTwoLists(l1, l2)
+        Assert.assertEquals("1->1->2->3->4->4", result.toString())
+    }
+}
