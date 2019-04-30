c21139f25f9bdc799979f6b9def6edb1917a30a0
lvkun <lvkun@xiaomi.com>
2019-04-30 17:41:22 +0800

    solve remove_duplicates_from_sorted_array.

--- src/site/lvkun/leetcode/remove_duplicates_from_sorted_array/Solution.kt
+++ src/site/lvkun/leetcode/remove_duplicates_from_sorted_array/Solution.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.remove_duplicates_from_sorted_array
 
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.size <= 1) {
+            return nums.size
+        }
+
+        var cur = 0
+        var pos = 1
+
+        while (pos < nums.size) {
+            if (nums[cur] != nums[pos]) {
+                nums[cur + 1] = nums[pos]
+                cur++
+            }
+            pos++
+        }
+
+        return cur + 1
+    }
+}
--- src/site/lvkun/leetcode/remove_duplicates_from_sorted_array/SolutionTest.kt
+++ src/site/lvkun/leetcode/remove_duplicates_from_sorted_array/SolutionTest.kt
@@ -1,2 +1,20 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.remove_duplicates_from_sorted_array
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testRemoveDuplicates() {
+        val solution = Solution()
+        var nums = intArrayOf(1,1,2)
+        var length = solution.removeDuplicates(nums)
+        var result = nums.sliceArray(0 until length)
+        Assert.assertArrayEquals(intArrayOf(1,2), result)
+
+        nums = intArrayOf(0,0,1,1,1,2,2,3,3,4)
+        length = solution.removeDuplicates(nums)
+        result = nums.sliceArray(0 until length)
+        Assert.assertArrayEquals(intArrayOf(0,1,2,3,4), result)
+    }
+}
