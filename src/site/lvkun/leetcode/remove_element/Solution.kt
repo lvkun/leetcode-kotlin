194c1498f4991454806385709246fe8651c57eff
lvkun <lvkun@xiaomi.com>
2019-05-06 16:25:44 +0800

    remove_elementpartly.

--- src/site/lvkun/leetcode/remove_element/Solution.kt
+++ src/site/lvkun/leetcode/remove_element/Solution.kt
@@ -1,2 +1,25 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.remove_element
 
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        if (nums.isEmpty()) {
+            return 0
+        }
+
+        val value = `val`
+
+        var start = 0
+        var end = nums.lastIndex
+
+        while (start <= end) {
+            if (nums[start] == value) {
+                nums[start] = nums[end]
+                end--
+            } else {
+                start++
+            }
+        }
+
+        return start + 1
+    }
+}
--- src/site/lvkun/leetcode/remove_element/SolutionTest.kt
+++ src/site/lvkun/leetcode/remove_element/SolutionTest.kt
@@ -1,2 +1,24 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.remove_element
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testRemoveElement() {
+        val solution = Solution()
+        var nums = intArrayOf(3,2,2,3)
+        var length = solution.removeElement(nums, 3)
+        var result = nums.sliceArray(0 until length)
+        Assert.assertArrayEquals(intArrayOf(2,2), result)
+
+        nums = intArrayOf(0,1,2,2,3,0,4,2)
+        length = solution.removeElement(nums, 2)
+        result = nums.sliceArray(0 until length)
+        Assert.assertArrayEquals(intArrayOf(0,1,4,0,3), result)
+
+        nums = intArrayOf(1)
+        length = solution.removeElement(nums, 1)
+        Assert.assertEquals(0, length)
+    }
+}
