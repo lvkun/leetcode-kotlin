22089ad821e7e848ec6ce38e08135ea0d758a3a1
Bruce Lv <lvkun@outlook.com>
2019-05-22 22:51:20 +0800

    solve string_to_integer_atoi.

--- src/site/lvkun/leetcode/string_to_integer_atoi/Solution.kt
+++ src/site/lvkun/leetcode/string_to_integer_atoi/Solution.kt
@@ -1,2 +1,99 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.string_to_integer_atoi
 
+class Solution {
+
+    class StateManager {
+
+        enum class State {
+            START,
+            SIGN,
+            NUMBER,
+            END
+        }
+
+        var sign = 1
+        var ret = 0
+
+        val result: Int
+            get() = sign * ret
+
+        var state: State = State.START
+
+        fun match(c: Char): Boolean {
+            state =
+                when(state) {
+                    State.START -> {
+                        when {
+                            c == '-' -> {
+                                sign = -1
+                                State.SIGN
+                            }
+                            c == '+' -> {
+                                sign = 1
+                                State.SIGN
+                            }
+                            c.isDigit() -> {
+                                ret = (c - '0') + ret * 10
+                                State.NUMBER
+                            }
+                            c.isWhitespace() -> {
+                                State.START
+                            }
+                            else -> {
+                                State.END
+                            }
+                        }
+                    }
+                    State.SIGN -> {
+                        when {
+                            c.isDigit() -> {
+                                ret = (c - '0') + ret * 10
+                                State.NUMBER
+                            }
+                            else -> {
+                                State.END
+                            }
+                        }
+                    }
+                    State.NUMBER -> {
+                        when {
+                            c.isDigit() -> {
+                                if (ret > Int.MAX_VALUE / 10) {
+                                    ret = if (sign > 0) Int.MAX_VALUE else Int.MIN_VALUE
+                                    State.END
+                                } else if (ret == Int.MAX_VALUE / 10) {
+                                    ret = if (c - '0' > 7)
+                                        if (sign > 0) Int.MAX_VALUE else Int.MIN_VALUE
+                                        else (c - '0') + ret * 10
+                                    State.END
+                                } else {
+                                    ret = (c - '0') + ret * 10
+                                    State.NUMBER
+                                }
+                            }
+                            else -> {
+                                State.END
+                            }
+                        }
+                    }
+                    else -> {
+                        State.END
+                    }
+                }
+
+            return state != State.END
+        }
+    }
+
+    fun myAtoi(str: String): Int {
+        val stateManager = StateManager()
+
+        for (c in str) {
+            if (!stateManager.match(c)) {
+                break
+            }
+        }
+
+        return stateManager.result
+    }
+}
--- src/site/lvkun/leetcode/string_to_integer_atoi/SolutionTest.kt
+++ src/site/lvkun/leetcode/string_to_integer_atoi/SolutionTest.kt
@@ -1,2 +1,43 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.string_to_integer_atoi
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMyAtoi() {
+        val solution = Solution()
+        var result = solution.myAtoi("")
+        Assert.assertEquals(0, result)
+
+        result = solution.myAtoi("42")
+        Assert.assertEquals(42, result)
+
+        result = solution.myAtoi("   -42")
+        Assert.assertEquals(-42, result)
+
+        result = solution.myAtoi("4193 with words")
+        Assert.assertEquals(4193, result)
+
+        result = solution.myAtoi("words and 987")
+        Assert.assertEquals(0, result)
+
+        result = solution.myAtoi("-91283472332")
+        Assert.assertEquals(-2147483648, result)
+
+        result = solution.myAtoi("+1")
+        Assert.assertEquals(1, result)
+
+        result = solution.myAtoi("+-2")
+        Assert.assertEquals(0, result)
+
+        result = solution.myAtoi("2147483648")
+        Assert.assertEquals(2147483647, result)
+
+        result = solution.myAtoi("2147483646")
+        Assert.assertEquals(2147483646, result)
+
+        result = solution.myAtoi("-2147483647")
+        Assert.assertEquals(-2147483647, result)
+    }
+}
