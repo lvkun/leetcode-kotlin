d8d86a7825f6c80288c8e16f1f01f39682b16282
Bruce Lv <lvkun@outlook.com>
2019-05-26 15:26:24 +0800

    solve implement_strstr.

--- src/site/lvkun/leetcode/implement_strstr/Solution.kt
+++ src/site/lvkun/leetcode/implement_strstr/Solution.kt
@@ -1,2 +1,43 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.implement_strstr
 
+class Solution {
+    fun nextOf(needle: String): IntArray {
+        val next = IntArray(needle.length)
+
+        var n = 0
+        for (i in 1 until needle.length) {
+            while (n > 0 && needle[n] != needle[i]) {
+                n = next[n - 1]
+            }
+
+            if (needle[n] == needle[i]) {
+                n++
+            }
+            next[i] = n
+        }
+
+        return next
+    }
+
+    fun strStr(haystack: String, needle: String): Int {
+        if (needle.isEmpty()) {
+            return 0
+        }
+
+        val next = nextOf(needle)
+        var n = 0
+        for (i in 0 until haystack.length) {
+            while (n > 0 && haystack[i] != needle[n]) {
+                n = next[n - 1]
+            }
+            if (haystack[i] == needle[n]) {
+                n++
+            }
+            if (n == needle.length) {
+                return i - n + 1
+            }
+        }
+
+        return -1
+    }
+}
--- src/site/lvkun/leetcode/implement_strstr/SolutionTest.kt
+++ src/site/lvkun/leetcode/implement_strstr/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.implement_strstr
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testStrStr() {
+        val solution = Solution()
+//        Assert.assertEquals(2, solution.strStr("hello", "ll"))
+//        Assert.assertEquals(-1, solution.strStr("aaaaa", "bba"))
+        Assert.assertEquals(-1, solution.strStr("a", ""))
+    }
+}
