2e41b6b084b981d89adbc75a03ec1bdd9a56743b
Bruce Lv <lvkun@outlook.com>
2019-05-27 21:14:50 +0800

    solve count_and_say.

--- src/site/lvkun/leetcode/count_and_say/Solution.kt
+++ src/site/lvkun/leetcode/count_and_say/Solution.kt
@@ -1,2 +1,29 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.count_and_say
 
+class Solution {
+    fun countAndSay(n: Int): String {
+        if (n == 1) {
+            return "1"
+        }
+
+        val lastResult = countAndSay(n - 1)
+        var count = 1
+        var lastChar = lastResult[0]
+        var result = ""
+        for (i in 1 until lastResult.length) {
+            val curChar = lastResult[i]
+            if (curChar != lastChar) {
+                result += "" + count + lastChar
+
+                lastChar = curChar
+                count = 1
+            } else {
+                count++
+            }
+        }
+
+        result += "" + count + lastChar
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/count_and_say/SolutionTest.kt
+++ src/site/lvkun/leetcode/count_and_say/SolutionTest.kt
@@ -1,2 +1,16 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.count_and_say
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testCountAndSay() {
+        val solution = Solution()
+        Assert.assertEquals("1", solution.countAndSay(1))
+        Assert.assertEquals("11", solution.countAndSay(2))
+        Assert.assertEquals("21", solution.countAndSay(3))
+        Assert.assertEquals("1211", solution.countAndSay(4))
+        Assert.assertEquals("111221", solution.countAndSay(5))
+    }
+}
