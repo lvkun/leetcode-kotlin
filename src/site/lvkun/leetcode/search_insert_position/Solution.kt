f81c756c9a781680255d6719ba9ba2f6e1b908f0
Bruce Lv <lvkun@outlook.com>
2019-05-27 20:51:52 +0800

    solve search_insert_position.

--- src/site/lvkun/leetcode/search_insert_position/Solution.kt
+++ src/site/lvkun/leetcode/search_insert_position/Solution.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.search_insert_position
 
+class Solution {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        for (i in nums.indices) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+
+        return nums.size
+    }
+}
--- src/site/lvkun/leetcode/search_insert_position/SolutionTest.kt
+++ src/site/lvkun/leetcode/search_insert_position/SolutionTest.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.search_insert_position
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testSearchInsert() {
+        val solution = Solution()
+        Assert.assertEquals(2,
+            solution.searchInsert(intArrayOf(1,3,5,6), 5))
+        Assert.assertEquals(1,
+            solution.searchInsert(intArrayOf(1,3,5,6), 2))
+        Assert.assertEquals(4,
+            solution.searchInsert(intArrayOf(1,3,5,6), 7))
+        Assert.assertEquals(0,
+            solution.searchInsert(intArrayOf(1,3,5,6), 0))
+    }
+}
