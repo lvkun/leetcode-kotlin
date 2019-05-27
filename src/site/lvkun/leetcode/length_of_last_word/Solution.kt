962ee8a9e9423d1ddddd270fbd150cedabeedbc8
Bruce Lv <lvkun@outlook.com>
2019-05-27 22:00:59 +0800

    solve length_of_last_word.

--- src/site/lvkun/leetcode/length_of_last_word/Solution.kt
+++ src/site/lvkun/leetcode/length_of_last_word/Solution.kt
@@ -1,2 +1,20 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.length_of_last_word
 
+class Solution {
+    fun lengthOfLastWord(s: String): Int {
+        var length = 0
+        var lastLength = 0
+        for (c in s) {
+            if (c.isWhitespace()) {
+                if (length != 0) {
+                    lastLength = length
+                }
+                length = 0
+            } else {
+                length++
+            }
+        }
+
+        return if (length == 0) lastLength else length
+    }
+}
--- src/site/lvkun/leetcode/length_of_last_word/SolutionTest.kt
+++ src/site/lvkun/leetcode/length_of_last_word/SolutionTest.kt
@@ -1,2 +1,16 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.length_of_last_word
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testLengthOfLastWord() {
+        val solution = Solution()
+        Assert.assertEquals(5, solution.lengthOfLastWord("Hello World"))
+        Assert.assertEquals(5, solution.lengthOfLastWord("Hello"))
+        Assert.assertEquals(1, solution.lengthOfLastWord("a"))
+        Assert.assertEquals(1, solution.lengthOfLastWord("a "))
+        Assert.assertEquals(1, solution.lengthOfLastWord("b   a   "))
+    }
+}
