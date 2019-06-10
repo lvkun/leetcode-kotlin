edf7577e305aed0feb26ba1e53df1e1e726112c0
Bruce Lv <lvkun@outlook.com>
2019-06-10 21:52:27 +0800

    solve plus_one.

--- src/site/lvkun/leetcode/plus_one/Solution.kt
+++ src/site/lvkun/leetcode/plus_one/Solution.kt
@@ -1,2 +1,24 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.plus_one
 
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+        var plus = 1
+        for (i in digits.size - 1 downTo 0) {
+            val v = digits[i]
+            if (v + plus > 9) {
+                plus = 1
+                digits[i] = 0
+            } else {
+                digits[i] = v + plus
+                plus = 0
+                break
+            }
+        }
+
+        if (plus != 0) {
+            return intArrayOf(plus).plus(digits)
+        }
+
+        return digits
+    }
+}
--- src/site/lvkun/leetcode/plus_one/SolutionTest.kt
+++ src/site/lvkun/leetcode/plus_one/SolutionTest.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.plus_one
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testPlusOne() {
+        val solution = Solution()
+        var result = solution.plusOne(intArrayOf(1,2,3))
+        Assert.assertArrayEquals(intArrayOf(1,2,4), result)
+
+        result = solution.plusOne(intArrayOf(4,3,2,1))
+        Assert.assertArrayEquals(intArrayOf(4,3,2,2), result)
+
+        result = solution.plusOne(intArrayOf(9,9,9))
+        Assert.assertArrayEquals(intArrayOf(1,0,0,0), result)
+    }
+}
