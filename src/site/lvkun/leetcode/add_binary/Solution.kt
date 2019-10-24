5f1d66fe115086e213dde5ebe8ca27478bff469b
Bruce Lv <bruce@duolingo.com>
2019-10-24 11:11:44 -0400

    solve add_binary

--- src/site/lvkun/leetcode/add_binary/Solution.kt
+++ src/site/lvkun/leetcode/add_binary/Solution.kt
@@ -1,2 +1,32 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.add_binary
 
+class Solution {
+    private fun String.binAt(index: Int): Int {
+        if (index < 0 || index >= this.length) {
+            return 0
+        }
+        return this[index] - '0'
+    }
+
+    fun addBinary(a: String, b: String): String {
+        var carry = 0
+        val result = mutableListOf<Char>()
+
+        val maxLength = maxOf(a.length, b.length)
+
+        for (i in 1..maxLength) {
+            val aValue = a.binAt(a.length - i)
+            val bValue = b.binAt(b.length - i)
+
+            val sum = carry + aValue + bValue
+            carry = sum / 2
+            result.add('0' + sum % 2)
+        }
+
+        if (carry != 0) {
+            result.add('1')
+        }
+
+        return result.reversed().joinToString("")
+    }
+}
--- src/site/lvkun/leetcode/add_binary/SolutionTest.kt
+++ src/site/lvkun/leetcode/add_binary/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.add_binary
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testAddBinary() {
+        val solution = Solution()
+        Assert.assertEquals("100", solution.addBinary("11", "1"))
+        Assert.assertEquals("10101", solution.addBinary("1010", "1011"))
+    }
+}
