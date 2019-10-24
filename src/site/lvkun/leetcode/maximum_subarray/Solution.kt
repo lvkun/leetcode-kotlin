71deb73d19a3984dbf9552c5cdb578f205043fa9
Bruce Lv <bruce@duolingo.com>
2019-10-24 14:58:21 -0400

    solve maximum_subarray

--- src/site/lvkun/leetcode/maximum_subarray/Solution.kt
+++ src/site/lvkun/leetcode/maximum_subarray/Solution.kt
@@ -1,2 +1,20 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.maximum_subarray
 
+import kotlin.math.max
+
+class Solution {
+    fun maxSubArray(nums: IntArray): Int {
+        var sum = 0
+        var maxSum = Int.MIN_VALUE
+
+        for (n in nums) {
+            sum += n
+            maxSum = max(sum, maxSum)
+            if (sum < 0) {
+                sum = 0
+            }
+        }
+
+        return maxSum
+    }
+}
--- src/site/lvkun/leetcode/maximum_subarray/SolutionTest.kt
+++ src/site/lvkun/leetcode/maximum_subarray/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.maximum_subarray
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMaxSubArray() {
+        val solution = Solution()
+        Assert.assertEquals(6, solution.maxSubArray(intArrayOf(-2,1,-3,4,-1,2,1,-5,4)))
+        Assert.assertEquals(-1, solution.maxSubArray(intArrayOf(-1)))
+    }
+}
