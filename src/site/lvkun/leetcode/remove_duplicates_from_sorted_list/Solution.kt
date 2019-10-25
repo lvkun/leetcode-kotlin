24a65896aada7a2f2e5c6a2a5eb8385ab9b371fe
Bruce Lv <bruce@duolingo.com>
2019-10-25 15:56:41 -0400

    solve remove_duplicates_from_sorted_list

--- src/site/lvkun/leetcode/remove_duplicates_from_sorted_list/Solution.kt
+++ src/site/lvkun/leetcode/remove_duplicates_from_sorted_list/Solution.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.remove_duplicates_from_sorted_list
 
+import site.lvkun.leetcode.common.ListNode
+
+class Solution {
+    fun deleteDuplicates(head: ListNode?): ListNode? {
+        var cur = head
+        while (cur != null) {
+            if (cur.next?.value == cur.value) {
+                cur.next = cur.next?.next
+            } else {
+                cur = cur.next
+            }
+        }
+
+        return head
+    }
+}
--- src/site/lvkun/leetcode/remove_duplicates_from_sorted_list/SolutionTest.kt
+++ src/site/lvkun/leetcode/remove_duplicates_from_sorted_list/SolutionTest.kt
@@ -1,2 +1,16 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.remove_duplicates_from_sorted_list
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.ListNode
+
+class SolutionTest {
+    @Test
+    fun testDeleteDuplicates() {
+        val solution = Solution()
+        var result = solution.deleteDuplicates(ListNode.fromString("1->1->2"))
+        Assert.assertEquals("1->2", result.toString())
+        result = solution.deleteDuplicates(ListNode.fromString("1->1->2->3->3"))
+        Assert.assertEquals("1->2->3", result.toString())
+    }
+}
