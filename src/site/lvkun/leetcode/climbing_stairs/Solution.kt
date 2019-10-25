01bb51fe18d8aba47b64543902460b41ce85b70e
Bruce Lv <bruce@duolingo.com>
2019-10-25 14:34:04 -0400

    solve climbing_stairs

--- src/site/lvkun/leetcode/climbing_stairs/Solution.kt
+++ src/site/lvkun/leetcode/climbing_stairs/Solution.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.climbing_stairs
 
+class Solution {
+    fun climbStairs(n: Int): Int {
+        var rn2 = 0
+        var rn1 = 1
+        for (i in 0 until n) {
+            val r = rn1 + rn2
+            rn2 = rn1
+            rn1 = r
+        }
+
+        return rn1
+    }
+}
--- src/site/lvkun/leetcode/climbing_stairs/SolutionTest.kt
+++ src/site/lvkun/leetcode/climbing_stairs/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.climbing_stairs
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testClimbStairs() {
+        val solution = Solution()
+        Assert.assertEquals(2, solution.climbStairs(2))
+        Assert.assertEquals(3, solution.climbStairs(3))
+        Assert.assertEquals(1836311903, solution.climbStairs(45))
+    }
+}
