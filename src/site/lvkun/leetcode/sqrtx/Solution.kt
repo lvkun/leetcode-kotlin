443d434a30a3bc7f9c0fdd56de9ef97db3d76dac
Bruce Lv <bruce@duolingo.com>
2019-10-25 12:21:18 -0400

    Solve sqrtx

--- src/site/lvkun/leetcode/sqrtx/Solution.kt
+++ src/site/lvkun/leetcode/sqrtx/Solution.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.sqrtx
 
+class Solution {
+    fun mySqrt(x: Int): Int {
+        var start = 0
+        var end = x
+
+        if (x == 1) {
+            return 1
+        }
+
+        while (start < end - 1) {
+            val y = (start + end) / 2
+
+            if (x / y == y) {
+                return y
+            }
+
+            if (x / y < y) {
+                end = y
+            } else {
+                start = y
+            }
+        }
+
+        return start
+    }
+}
--- src/site/lvkun/leetcode/sqrtx/SolutionTest.kt
+++ src/site/lvkun/leetcode/sqrtx/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.sqrtx
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMySqrt() {
+        val solution = Solution()
+        Assert.assertEquals(2, solution.mySqrt(4))
+        Assert.assertEquals(2, solution.mySqrt(8))
+        Assert.assertEquals(1, solution.mySqrt(1))
+    }
+}
