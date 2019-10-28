68746c2051efeb0dcbb29473d27f84ff37291566
Bruce Lv <bruce@duolingo.com>
2019-10-28 17:01:12 +0800

    Solve merge_sorted_array

--- src/site/lvkun/leetcode/merge_sorted_array/Solution.kt
+++ src/site/lvkun/leetcode/merge_sorted_array/Solution.kt
@@ -1,2 +1,27 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.merge_sorted_array
 
+import java.util.*
+
+class Solution {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+        var i = m - 1
+        var j = n - 1
+
+        var p = m + n - 1
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[p] = nums1[i]
+                i--
+            } else {
+                nums1[p] = nums2[j]
+                j--
+            }
+            p--
+        }
+
+        if (j >= 0) {
+            System.arraycopy(nums2, 0, nums1, 0, j + 1)
+        }
+    }
+}
--- src/site/lvkun/leetcode/merge_sorted_array/SolutionTest.kt
+++ src/site/lvkun/leetcode/merge_sorted_array/SolutionTest.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.merge_sorted_array
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMerge() {
+        val solution = Solution()
+
+        val nums1 = intArrayOf(1,2,3,0,0,0)
+        val nums2 = intArrayOf(2,5,6)
+        solution.merge(nums1, 3, nums2, 3)
+
+        Assert.assertArrayEquals(intArrayOf(1,2,2,3,5,6), nums1)
+    }
+
+    @Test
+    fun testMergeZero() {
+        val solution = Solution()
+
+        val nums1 = intArrayOf(0)
+        val nums2 = intArrayOf(1)
+        solution.merge(nums1, 0, nums2, 1)
+
+        Assert.assertArrayEquals(intArrayOf(1), nums1)
+    }
+}
