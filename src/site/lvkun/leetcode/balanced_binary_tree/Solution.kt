04efcfbf647870c3b046f725020c28e57d8ad31f
Bruce Lv <lvkun@outlook.com>
2019-10-31 23:55:34 +0800

    solve balanced_binary_tree

--- src/site/lvkun/leetcode/balanced_binary_tree/Solution.kt
+++ src/site/lvkun/leetcode/balanced_binary_tree/Solution.kt
@@ -1,2 +1,25 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.balanced_binary_tree
 
+import site.lvkun.leetcode.common.TreeNode
+import kotlin.math.abs
+
+class Solution {
+    fun isBalanced(root: TreeNode?): Boolean {
+        return isBalancedInternal(root) != -1
+    }
+
+    private fun isBalancedInternal(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+
+        val left = isBalancedInternal(root.left)
+        val right = isBalancedInternal(root.right)
+
+        if (left == -1 || right == -1 || abs(right - left) > 1) {
+            return -1
+        }
+
+        return maxOf(left, right) + 1
+    }
+}
--- src/site/lvkun/leetcode/balanced_binary_tree/SolutionTest.kt
+++ src/site/lvkun/leetcode/balanced_binary_tree/SolutionTest.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.balanced_binary_tree
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testIsBalanced() {
+        val solution = Solution()
+        var tree = TreeNode(3,
+            TreeNode(9),
+            TreeNode(20, TreeNode(15), TreeNode(7)))
+        Assert.assertTrue(solution.isBalanced(tree))
+
+        tree = TreeNode(1,
+            TreeNode(2,
+                TreeNode(3,
+                    TreeNode(4),
+                    TreeNode(4)
+                ),
+                TreeNode(3)
+            ),
+            TreeNode(2)
+        )
+        Assert.assertFalse(solution.isBalanced(tree))
+    }
+}
