041fef5b45bc88b44791491fc8427c87e16e0982
Bruce Lv <bruce@duolingo.com>
2019-10-31 09:48:34 +0800

    solve symmetric_tree

--- src/site/lvkun/leetcode/symmetric_tree/Solution.kt
+++ src/site/lvkun/leetcode/symmetric_tree/Solution.kt
@@ -1,2 +1,21 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.symmetric_tree
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun isSymmetric(root: TreeNode?): Boolean {
+        if (root == null) {
+            return true
+        }
+
+        return isSymmetric(root.left, root.right)
+    }
+
+    fun isSymmetric(left: TreeNode?, right: TreeNode?): Boolean {
+        if (left == null && right == null) {
+            return true
+        }
+
+        return left?.`val` == right?.`val` && isSymmetric(left?.left, right?.right) && isSymmetric(left?.right, right?.left)
+    }
+}
--- src/site/lvkun/leetcode/symmetric_tree/SolutionTest.kt
+++ src/site/lvkun/leetcode/symmetric_tree/SolutionTest.kt
@@ -1,2 +1,23 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.symmetric_tree
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testIsSymmetric() {
+        val solution = Solution()
+        Assert.assertTrue(
+                solution.isSymmetric(
+                        TreeNode(1,
+                                TreeNode(2, TreeNode(3), TreeNode(4)),
+                                TreeNode(2, TreeNode(4), TreeNode(3)))))
+
+        Assert.assertFalse(
+                solution.isSymmetric(
+                        TreeNode(1,
+                                TreeNode(2, null, TreeNode(3)),
+                                TreeNode(2, null, TreeNode(3)))))
+    }
+}
