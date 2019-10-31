48c3c7a33f5150e3cb270e86c0c9c783726a599b
Bruce Lv <bruce@duolingo.com>
2019-10-31 10:29:43 +0800

    solve maximum_depth_of_binary_tree

--- src/site/lvkun/leetcode/maximum_depth_of_binary_tree/Solution.kt
+++ src/site/lvkun/leetcode/maximum_depth_of_binary_tree/Solution.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.maximum_depth_of_binary_tree
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+
+        return 1 + maxOf(maxDepth(root.left), maxDepth(root.right))
+    }
+}
--- src/site/lvkun/leetcode/maximum_depth_of_binary_tree/SolutionTest.kt
+++ src/site/lvkun/leetcode/maximum_depth_of_binary_tree/SolutionTest.kt
@@ -1,2 +1,16 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.maximum_depth_of_binary_tree
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testMaxDepth() {
+        val solution = Solution()
+        val tree = TreeNode(3,
+                TreeNode(9),
+                TreeNode(20, TreeNode(15), TreeNode(7)))
+        Assert.assertEquals(3, solution.maxDepth(tree))
+    }
+}
