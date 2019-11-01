5d35489a369914f616fd7780f76c160a0b7477ef
Bruce Lv <bruce@duolingo.com>
2019-11-01 22:47:37 +0800

    solve minimum_depth_of_binary_tree

--- src/site/lvkun/leetcode/minimum_depth_of_binary_tree/Solution.kt
+++ src/site/lvkun/leetcode/minimum_depth_of_binary_tree/Solution.kt
@@ -1,2 +1,21 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.minimum_depth_of_binary_tree
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun minDepth(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+
+        if (root.left == null) {
+            return 1 + minDepth(root.right)
+        }
+
+        if (root.right == null) {
+            return 1 + minDepth(root.left)
+        }
+
+        return 1 + minOf(minDepth(root.left), minDepth(root.right))
+    }
+}
--- src/site/lvkun/leetcode/minimum_depth_of_binary_tree/SolutionTest.kt
+++ src/site/lvkun/leetcode/minimum_depth_of_binary_tree/SolutionTest.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.minimum_depth_of_binary_tree
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testMinDepth() {
+        val solution = Solution()
+        val tree = TreeNode(3,
+                TreeNode(9),
+                TreeNode(20,
+                        TreeNode(15),
+                        TreeNode(7)))
+        Assert.assertEquals(2, solution.minDepth(tree))
+
+        Assert.assertEquals(2, solution.minDepth(TreeNode(1, TreeNode(2))))
+
+        Assert.assertEquals(0, solution.minDepth(null))
+    }
+}
