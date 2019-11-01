c1d8b613cb0cc71ea8f701d6e41c72af9f3213b7
Bruce Lv <bruce@duolingo.com>
2019-11-01 21:55:00 +0800

    solve binary_tree_level_order_traversal_ii

--- src/site/lvkun/leetcode/binary_tree_level_order_traversal_ii/Solution.kt
+++ src/site/lvkun/leetcode/binary_tree_level_order_traversal_ii/Solution.kt
@@ -1,2 +1,32 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_tree_level_order_traversal_ii
 
+import site.lvkun.leetcode.common.TreeNode
+import java.util.*
+
+class Solution {
+    fun levelOrderBottom(root: TreeNode?): List<List<Int>> {
+        val result = ArrayDeque<List<Int>>()
+
+        var level = mutableListOf<TreeNode>()
+        if (root != null) {
+            level.add(root)
+        }
+
+        while (level.isNotEmpty()) {
+            val levelResult = mutableListOf<Int>()
+            val nextLevel = mutableListOf<TreeNode>()
+
+            for (node in level) {
+                levelResult.add(node.`val`)
+                node.left ?.let { nextLevel.add(it) }
+                node.right ?.let { nextLevel.add(it) }
+            }
+
+            result.addFirst(levelResult)
+
+            level = nextLevel
+        }
+
+        return result.toList()
+    }
+}
