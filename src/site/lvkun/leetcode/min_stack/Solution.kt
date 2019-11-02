5caea71a584ac67857d12a928c1512b05e222c41
Bruce Lv <bruce@duolingo.com>
2019-11-02 22:50:03 +0800

    Solve min_stack

--- src/site/lvkun/leetcode/min_stack/Solution.kt
+++ src/site/lvkun/leetcode/min_stack/Solution.kt
@@ -1,2 +1,31 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.min_stack
 
+import java.util.*
+
+class MinStack() {
+
+    private val stack = Stack<Int>()
+    private val minStack = Stack<Int>()
+
+    fun push(x: Int) {
+        stack.push(x)
+        if (minStack.empty() || x <= minStack.peek()) {
+            minStack.push(x)
+        }
+    }
+
+    fun pop() {
+        val x = stack.pop()
+        if (x == minStack.peek()) {
+            minStack.pop()
+        }
+    }
+
+    fun top(): Int {
+        return stack.peek()
+    }
+
+    fun getMin(): Int {
+        return minStack.peek()
+    }
+}
--- src/site/lvkun/leetcode/min_stack/SolutionTest.kt
+++ src/site/lvkun/leetcode/min_stack/SolutionTest.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.min_stack
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMinStack() {
+        val minStack = MinStack()
+        minStack.push(-2)
+        minStack.push(0)
+        minStack.push(-3)
+        Assert.assertEquals(-3, minStack.getMin())
+        minStack.pop()
+        Assert.assertEquals(0, minStack.top())
+        Assert.assertEquals(-2, minStack.getMin())
+    }
+}
