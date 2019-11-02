bf2daa54c69f1cee2a40627fc9ce4dd19dcf9744
Bruce Lv <bruce@duolingo.com>
2019-11-02 17:03:39 +0800

    Solve pascals_triangle_ii

--- src/site/lvkun/leetcode/pascals_triangle_ii/Solution.kt
+++ src/site/lvkun/leetcode/pascals_triangle_ii/Solution.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.pascals_triangle_ii
 
+class Solution {
+    fun getRow(rowIndex: Int): List<Int> {
+        if (rowIndex < 0) {
+            return emptyList()
+        }
+
+        val result = IntArray(rowIndex + 1) { 0 }
+        result[0] = 1
+        for (row in 1..rowIndex) {
+            var lastItem = 0
+            for (col in 0..row) {
+                val temp = result[col]
+                result[col] = result[col] + lastItem
+                lastItem = temp
+            }
+        }
+
+        return result.toList()
+    }
+}
--- src/site/lvkun/leetcode/pascals_triangle_ii/SolutionTest.kt
+++ src/site/lvkun/leetcode/pascals_triangle_ii/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.pascals_triangle_ii
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testGetRow() {
+        val solution = Solution()
+        Assert.assertEquals(listOf(1, 3, 3, 1), solution.getRow(3))
+    }
+}
