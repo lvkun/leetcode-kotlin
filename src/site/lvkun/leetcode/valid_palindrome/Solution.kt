ba6046dfb73ea00a54a78b30a28bce1a54876d4b
Bruce Lv <bruce@duolingo.com>
2019-11-02 22:19:21 +0800

    Solve valid_palindrome

--- src/site/lvkun/leetcode/valid_palindrome/Solution.kt
+++ src/site/lvkun/leetcode/valid_palindrome/Solution.kt
@@ -1,2 +1,31 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.valid_palindrome
 
+class Solution {
+    fun isPalindrome(s: String): Boolean {
+        var start = 0
+        var end = s.length - 1
+
+        val arr = s.toCharArray()
+        while (start <= end) {
+            while (start < s.length && !arr[start].isDigit() && !arr[start].isLetter()) {
+                start++
+            }
+            while (end >= 0 && !arr[end].isDigit() && !arr[end].isLetter()) {
+                end--
+            }
+
+            if (start == s.length || end == 0) {
+                return true
+            }
+
+            if (arr[start].toLowerCase() != arr[end].toLowerCase()) {
+                return false
+            }
+
+            start++
+            end--
+        }
+
+        return true
+    }
+}
--- src/site/lvkun/leetcode/valid_palindrome/SolutionTest.kt
+++ src/site/lvkun/leetcode/valid_palindrome/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.valid_palindrome
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIsPalindrome() {
+        val solution = Solution()
+//        Assert.assertTrue(solution.isPalindrome("A man, a plan, a canal: Panama"))
+        Assert.assertFalse(solution.isPalindrome("race a car"))
+    }
+}
