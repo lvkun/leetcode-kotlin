2063b54190f1a726d883e8305403f832546f3029
Bruce Lv <bruce@duolingo.com>
2019-11-02 17:13:38 +0800

    Solve package site.lvkun.leetcode.best_time_to_buy_and_sell_stock

--- src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock/Solution.kt
+++ src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock/Solution.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.best_time_to_buy_and_sell_stock
 
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+        var result = 0
+
+        var low = Integer.MAX_VALUE
+        for (price in prices) {
+            if (price < low) {
+                low = price
+            }
+
+            result = maxOf(price - low, result)
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock/SolutionTest.kt
+++ src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.best_time_to_buy_and_sell_stock
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMaxProfit() {
+        val solution = Solution()
+        Assert.assertEquals(5, solution.maxProfit(intArrayOf(7,1,5,3,6,4)))
+        Assert.assertEquals(0, solution.maxProfit(intArrayOf(7,6,4,3,1)))
+    }
+}
