441daf3b92bf7570a74f71e22e2152acf6174af1
Bruce Lv <bruce@duolingo.com>
2019-11-02 15:24:36 +0800

    Solve path_sum

--- src/site/lvkun/leetcode/path_sum/Solution.kt
+++ src/site/lvkun/leetcode/path_sum/Solution.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.path_sum
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun hasPathSum(root: TreeNode?, sum: Int): Boolean {
+            if (root == null) {
+                return false
+            }
+
+            if (root.left == null && root.right == null) {
+                return sum == root.`val`
+            }
+
+            val nextSum = sum - root.`val`
+            return hasPathSum(root.left, nextSum) || hasPathSum(root.right, nextSum)
+    }
+}
--- src/site/lvkun/leetcode/path_sum/SolutionTest.kt
+++ src/site/lvkun/leetcode/path_sum/SolutionTest.kt
@@ -1,2 +1,25 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.path_sum
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testHasPathSum() {
+        val solution = Solution()
+        val tree = TreeNode(5,
+                TreeNode(4,
+                        TreeNode(11,
+                                TreeNode(7),
+                                TreeNode(2))),
+                TreeNode(8,
+                        TreeNode(13),
+                        TreeNode(4,
+                                null,
+                                TreeNode(1))))
+        Assert.assertTrue(solution.hasPathSum(tree, 22))
+
+        Assert.assertFalse(solution.hasPathSum(null, 0))
+    }
+}
