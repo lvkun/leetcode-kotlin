82e48c843aee7730f81f8165aab37f0014619ee8
Bruce Lv <bruce@duolingo.com>
2019-11-02 15:47:39 +0800

    Solve pascals_triangle

--- src/site/lvkun/leetcode/pascals_triangle/Solution.kt
+++ src/site/lvkun/leetcode/pascals_triangle/Solution.kt
@@ -1,2 +1,27 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.pascals_triangle
 
+class Solution {
+    fun generate(numRows: Int): List<List<Int>> {
+        if (numRows <= 0) {
+            return emptyList()
+        }
+
+        val result = mutableListOf<List<Int>>()
+
+        var lastRow = mutableListOf(1)
+        result.add(lastRow)
+
+        for (row in 1 until numRows) {
+            val curRow = mutableListOf<Int>()
+            for (i in 0..lastRow.size) {
+                val item = lastRow.getOrElse(i - 1) {0} + lastRow.getOrElse(i) {0}
+                curRow.add(item)
+            }
+
+            lastRow = curRow
+            result.add(lastRow)
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/pascals_triangle/SolutionTest.kt
+++ src/site/lvkun/leetcode/pascals_triangle/SolutionTest.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.pascals_triangle
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testGenerate() {
+        val solution = Solution()
+        val result = solution.generate(5)
+        val expected = listOf(
+                listOf(1),
+                listOf(1, 1),
+                listOf(1, 2, 1),
+                listOf(1, 3, 3, 1),
+                listOf(1, 4, 6, 4, 1))
+        Assert.assertEquals(expected, result)
+    }
+}
