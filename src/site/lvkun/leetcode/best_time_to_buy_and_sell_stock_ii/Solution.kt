d8a66667805954a72415a771607718ff6cdd4ce3
Bruce Lv <bruce@duolingo.com>
2019-11-02 21:33:41 +0800

    Solve best_time_to_buy_and_sell_stock_ii

--- src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock_ii/Solution.kt
+++ src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock_ii/Solution.kt
@@ -1,2 +1,16 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.best_time_to_buy_and_sell_stock_ii
 
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+        var profit = 0
+
+        for (pair in prices.toList().zipWithNext()) {
+            val p = pair.second - pair.first
+            if (p > 0) {
+                profit += p
+            }
+        }
+
+        return profit
+    }
+}
--- src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock_ii/SolutionTest.kt
+++ src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock_ii/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.best_time_to_buy_and_sell_stock_ii
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMaxProfit() {
+        val solution = Solution()
+        Assert.assertEquals(7, solution.maxProfit(intArrayOf(7,1,5,3,6,4)))
+        Assert.assertEquals(4, solution.maxProfit(intArrayOf(1,2,3,4,5)))
+        Assert.assertEquals(0, solution.maxProfit(intArrayOf(7,6,4,3,1)))
+    }
+}
