3ee3a780ee0c422b9181fd6a95837b4bf23c5ac0
Bruce Lv <bruce@duolingo.com>
2019-11-03 18:43:24 +0800

    Solve excel_sheet_column_title

--- src/site/lvkun/leetcode/excel_sheet_column_title/Solution.kt
+++ src/site/lvkun/leetcode/excel_sheet_column_title/Solution.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.excel_sheet_column_title
 
+class Solution {
+    private fun toChar(n: Int): Char {
+        return (n + 'A'.toInt()).toChar()
+    }
+
+    fun convertToTitle(n: Int): String {
+        var result = mutableListOf<Char>()
+        var remain = n
+        while (remain > 0) {
+            remain--
+            result.add(toChar(remain % 26))
+            remain /= 26
+        }
+        return result.reversed().joinToString("")
+    }
+}
--- src/site/lvkun/leetcode/excel_sheet_column_title/SolutionTest.kt
+++ src/site/lvkun/leetcode/excel_sheet_column_title/SolutionTest.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.excel_sheet_column_title
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testConvertToTitle() {
+        val solution = Solution()
+        Assert.assertEquals("A", solution.convertToTitle(1))
+        Assert.assertEquals("AB", solution.convertToTitle(28))
+        Assert.assertEquals("AZ", solution.convertToTitle(52))
+        Assert.assertEquals("ZY", solution.convertToTitle(701))
+    }
+}
