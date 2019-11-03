cbb0c75dae9f18291dd5ba0d374b357bc2406cf4
Bruce Lv <bruce@duolingo.com>
2019-11-03 16:38:16 +0800

    Solve two_sum_ii_input_array_is_sorted

--- src/site/lvkun/leetcode/two_sum_ii_input_array_is_sorted/Solution.kt
+++ src/site/lvkun/leetcode/two_sum_ii_input_array_is_sorted/Solution.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.two_sum_ii_input_array_is_sorted
 
+class Solution {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        var start = 0
+        var end = numbers.lastIndex
+
+        while (start < end) {
+            when {
+                numbers[start] + numbers[end] < target -> start++
+                numbers[start] + numbers[end] > target -> end--
+                else -> return intArrayOf(start + 1, end + 1)
+            }
+        }
+
+        return intArrayOf(0)
+    }
+}
--- src/site/lvkun/leetcode/two_sum_ii_input_array_is_sorted/SolutionTest.kt
+++ src/site/lvkun/leetcode/two_sum_ii_input_array_is_sorted/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.two_sum_ii_input_array_is_sorted
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testTwoSum() {
+        val solution = Solution()
+        Assert.assertArrayEquals(intArrayOf(1, 2), solution.twoSum(intArrayOf(2, 7, 11, 15), 9))
+        Assert.assertArrayEquals(intArrayOf(1, 3), solution.twoSum(intArrayOf(2, 3, 4), 6))
+    }
+}
