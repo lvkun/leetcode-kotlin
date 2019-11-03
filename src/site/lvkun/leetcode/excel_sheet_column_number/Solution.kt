9b5cc3c9e314db514248862851c5af99095bc5e5
Bruce Lv <bruce@duolingo.com>
2019-11-03 19:42:54 +0800

    Solve excel_sheet_column_number

--- src/site/lvkun/leetcode/excel_sheet_column_number/Solution.kt
+++ src/site/lvkun/leetcode/excel_sheet_column_number/Solution.kt
@@ -1,2 +1,16 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.excel_sheet_column_number
 
+class Solution {
+    private fun toInt(char: Char) =
+            (char - 'A').toInt()
+
+    fun titleToNumber(s: String): Int {
+        var result = 0
+        for (c in s.toCharArray()) {
+            result *= 26
+            result += toInt(c) + 1
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/excel_sheet_column_number/SolutionTest.kt
+++ src/site/lvkun/leetcode/excel_sheet_column_number/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.excel_sheet_column_number
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testTitleToNumber() {
+        val solution = Solution()
+        Assert.assertEquals(1, solution.titleToNumber("A"))
+        Assert.assertEquals(28, solution.titleToNumber("AB"))
+        Assert.assertEquals(701, solution.titleToNumber("ZY"))
+    }
+}
