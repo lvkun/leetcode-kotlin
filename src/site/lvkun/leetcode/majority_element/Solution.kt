588a3e7622bab48d3f8d1c69ab6315a37b8f1403
Bruce Lv <bruce@duolingo.com>
2019-11-03 19:34:14 +0800

    Solve majority_element

--- src/site/lvkun/leetcode/majority_element/Solution.kt
+++ src/site/lvkun/leetcode/majority_element/Solution.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.majority_element
 
+class Solution {
+    fun majorityElement(nums: IntArray): Int {
+        var count = 1
+        var result : Int? = null
+
+        for (n in nums) {
+            if (result != n) {
+                count -= 1
+                if (count == 0) {
+                    result = n
+                    count = 1
+                }
+            } else {
+                count++
+            }
+        }
+
+        return result!!
+    }
+}
--- src/site/lvkun/leetcode/majority_element/SolutionTest.kt
+++ src/site/lvkun/leetcode/majority_element/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.majority_element
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMajorityElement() {
+        val solution = Solution()
+        Assert.assertEquals(3, solution.majorityElement(intArrayOf(3, 2, 3)))
+        Assert.assertEquals(2, solution.majorityElement(intArrayOf(2, 2, 1, 1, 1, 2, 2)))
+    }
+}
