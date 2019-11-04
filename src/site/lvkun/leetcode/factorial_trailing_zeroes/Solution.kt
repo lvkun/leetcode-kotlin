d4a84858f2b2a9e996ec71db5450a2e61e85ebf2
Bruce Lv <bruce@duolingo.com>
2019-11-04 11:47:33 +0800

    Solve factorial_trailing_zeroes

--- src/site/lvkun/leetcode/factorial_trailing_zeroes/Solution.kt
+++ src/site/lvkun/leetcode/factorial_trailing_zeroes/Solution.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.factorial_trailing_zeroes
 
+class Solution {
+    fun trailingZeroes(n: Int): Int {
+        var result = 0
+        var num = n
+
+        while (num >= 5) {
+            result += num / 5
+            num /= 5
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/factorial_trailing_zeroes/SolutionTest.kt
+++ src/site/lvkun/leetcode/factorial_trailing_zeroes/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.factorial_trailing_zeroes
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testTrailingZeroes() {
+        val solution = Solution()
+        Assert.assertEquals(0, solution.trailingZeroes(3))
+        Assert.assertEquals(1, solution.trailingZeroes(5))
+    }
+}
