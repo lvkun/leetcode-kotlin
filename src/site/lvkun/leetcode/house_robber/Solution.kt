d6c473d627371e2dfe3ff41bde7569897444da52
Bruce Lv <bruce@duolingo.com>
2019-11-06 00:31:58 +0800

    Solve house_robber

--- src/site/lvkun/leetcode/house_robber/Solution.kt
+++ src/site/lvkun/leetcode/house_robber/Solution.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.house_robber
 
+class Solution {
+    fun rob(nums: IntArray): Int {
+        for (i in 2 until nums.size) {
+            nums[i] = nums[i] + maxOf(nums.getOrElse(i - 2) {0}, nums.getOrElse(i - 3) {0})
+        }
+
+        return nums.takeLast(2).max() ?: 0
+    }
+
+}
--- src/site/lvkun/leetcode/house_robber/SolutionTest.kt
+++ src/site/lvkun/leetcode/house_robber/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.house_robber
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testRob() {
+        val solution = Solution()
+        Assert.assertEquals(4, solution.rob(intArrayOf(1,2,3,1)))
+        Assert.assertEquals(12, solution.rob(intArrayOf(2,7,9,3,1)))
+    }
+}
