baed81db3072170790e15118addf75616c6663b5
Bruce Lv <bruce@duolingo.com>
2019-11-05 23:50:15 +0800

    Solve rotate_array

--- src/site/lvkun/leetcode/rotate_array/Solution.kt
+++ src/site/lvkun/leetcode/rotate_array/Solution.kt
@@ -1,2 +1,31 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.rotate_array
 
+class Solution {
+    fun rotate(nums: IntArray, k: Int): Unit {
+        val remainder = k % nums.size
+        if (remainder == 0) {
+            return
+        }
+
+        var index = 0
+        var count = nums.size
+
+        var temp = nums[index]
+        var next = index
+        while (count > 0) {
+            next = (next + remainder) % nums.size
+            if (next == index) {
+                count--
+                nums[index] = temp
+                index++
+                next++
+                temp = nums[index]
+            } else {
+                val nextValue = nums[next]
+                count--
+                nums[next] = temp
+                temp = nextValue
+            }
+        }
+    }
+}
--- src/site/lvkun/leetcode/rotate_array/SolutionTest.kt
+++ src/site/lvkun/leetcode/rotate_array/SolutionTest.kt
@@ -1,2 +1,34 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.rotate_array
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun test() {
+        val solution = Solution()
+        var result = intArrayOf(1,2,3,4,5,6,7)
+        solution.rotate(result, 3)
+        Assert.assertArrayEquals(intArrayOf(5,6,7,1,2,3,4), result)
+
+        result = intArrayOf(-1,-100,3,99)
+        solution.rotate(result, 2)
+        Assert.assertArrayEquals(intArrayOf(3,99,-1,-100), result)
+
+        result = intArrayOf(1,2)
+        solution.rotate(result, 1)
+        Assert.assertArrayEquals(intArrayOf(2,1), result)
+
+        result = intArrayOf(1)
+        solution.rotate(result, 0)
+        Assert.assertArrayEquals(intArrayOf(1), result)
+
+        result = intArrayOf(1)
+        solution.rotate(result, 1)
+        Assert.assertArrayEquals(intArrayOf(1), result)
+
+        result = intArrayOf(1, 2)
+        solution.rotate(result, 2)
+        Assert.assertArrayEquals(intArrayOf(1, 2), result)
+    }
+}
