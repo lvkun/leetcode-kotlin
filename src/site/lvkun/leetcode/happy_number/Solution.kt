4feed2dd88dada2ccc421c5a2667c36841e359e1
Bruce Lv <bruce@duolingo.com>
2019-11-06 00:45:09 +0800

    Solve happy_number

--- src/site/lvkun/leetcode/happy_number/Solution.kt
+++ src/site/lvkun/leetcode/happy_number/Solution.kt
@@ -1,2 +1,31 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.happy_number
 
+class Solution {
+    fun isHappy(n: Int): Boolean {
+
+        val set = mutableSetOf<Int>()
+
+        var number = n
+        while (!set.contains(number)) {
+            set.add(number)
+
+            number = next(number)
+            if (number == 1) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun next(n: Int): Int {
+        var result = 0
+        var num = n
+        while (num > 0) {
+            val digit = num % 10
+            result += digit * digit
+            num /= 10
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/happy_number/SolutionTest.kt
+++ src/site/lvkun/leetcode/happy_number/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.happy_number
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIsHappy() {
+        val solution = Solution()
+//        Assert.assertTrue(solution.isHappy(19))
+        Assert.assertFalse(solution.isHappy(3))
+    }
+}
