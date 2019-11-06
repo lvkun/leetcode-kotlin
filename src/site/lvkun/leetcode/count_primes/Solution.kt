22069e77ab34406e38998cc21a5566a954cb9f34
Bruce Lv <bruce@duolingo.com>
2019-11-07 00:39:11 +0800

    Solve count_primes

--- src/site/lvkun/leetcode/count_primes/Solution.kt
+++ src/site/lvkun/leetcode/count_primes/Solution.kt
@@ -1,2 +1,31 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.count_primes
 
+class Solution {
+    fun countPrimes(n: Int): Int {
+        if (n < 3) {
+            return 0
+        }
+        val isPrime = BooleanArray(n)
+        for (i in 2 until n) {
+            isPrime[i] = true
+        }
+
+        var start = 2
+        while (start < n / 2 + 1) {
+            var time = 2
+            var nonPrime = start * time
+            while (nonPrime < n) {
+                isPrime[nonPrime] = false
+                time++
+                nonPrime = start * time
+            }
+
+            start++
+            while (!isPrime[start] && start < n / 2 + 1) {
+                start++
+            }
+        }
+
+        return isPrime.count { it }
+    }
+}
--- src/site/lvkun/leetcode/count_primes/SolutionTest.kt
+++ src/site/lvkun/leetcode/count_primes/SolutionTest.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.count_primes
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testCountPrimes() {
+        val solution = Solution()
+        Assert.assertEquals(4, solution.countPrimes(10))
+        Assert.assertEquals(0, solution.countPrimes(0))
+        Assert.assertEquals(1, solution.countPrimes(3))
+        Assert.assertEquals(2, solution.countPrimes(5))
+    }
+}
