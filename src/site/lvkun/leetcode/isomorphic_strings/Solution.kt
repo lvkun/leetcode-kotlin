39b6cd40ec70fc1a60f98745a347ef376ad77974
Bruce Lv <bruce@duolingo.com>
2019-11-07 15:11:28 +0800

    Solve isomorphic_strings

--- src/site/lvkun/leetcode/isomorphic_strings/Solution.kt
+++ src/site/lvkun/leetcode/isomorphic_strings/Solution.kt
@@ -1,2 +1,35 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.isomorphic_strings
 
+class Solution {
+    private fun Char.index() = this.toInt()
+
+    fun isIsomorphic(s: String, t: String): Boolean {
+        if (s.length != t.length) {
+            return false
+        }
+
+        val mapS = IntArray(128) {-1}
+        val mapT = IntArray(128) {-1}
+        for (i in s.indices) {
+            val sChar = s[i]
+            val tChar = t[i]
+
+            if (mapS[sChar.index()] == -1) {
+                mapS[sChar.index()] = i
+            }
+            val indexS = mapS[sChar.index()]
+
+            if (mapT[tChar.index()] == -1) {
+                mapT[tChar.index()] = i
+            }
+
+            val indexT = mapT[tChar.index()]
+
+            if (indexS != indexT) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
--- src/site/lvkun/leetcode/isomorphic_strings/SolutionTest.kt
+++ src/site/lvkun/leetcode/isomorphic_strings/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.isomorphic_strings
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIsIsomorphic() {
+        val solution = Solution()
+        Assert.assertTrue(solution.isIsomorphic("egg", "add"))
+        Assert.assertFalse(solution.isIsomorphic("foo", "bar"))
+        Assert.assertTrue(solution.isIsomorphic("paper", "title"))
+    }
+}
