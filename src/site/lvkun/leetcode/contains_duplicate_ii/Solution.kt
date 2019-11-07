70a99426e1d74bcb2274b66b3b608500d7ac5a7a
Bruce Lv <bruce@duolingo.com>
2019-11-07 23:14:56 +0800

    Solve contains_duplicate_ii

--- src/site/lvkun/leetcode/contains_duplicate_ii/Solution.kt
+++ src/site/lvkun/leetcode/contains_duplicate_ii/Solution.kt
@@ -1,2 +1,31 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.contains_duplicate_ii
 
+class Solution {
+    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        if (k == 0) {
+            return false
+        }
+
+        val set = mutableSetOf<Int>()
+        for (i in 0 until minOf(k, nums.size)) {
+            if (set.contains(nums[i])) {
+                return true
+            }
+            set.add(nums[i])
+        }
+
+        if (k >= nums.size) {
+            return false
+        }
+
+        for (i in k until nums.size) {
+            if (set.contains(nums[i])) {
+                return true
+            }
+
+            set.remove(nums[i - k])
+            set.add(nums[i])
+        }
+        return false
+    }
+}
--- src/site/lvkun/leetcode/contains_duplicate_ii/SolutionTest.kt
+++ src/site/lvkun/leetcode/contains_duplicate_ii/SolutionTest.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.contains_duplicate_ii
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testContainsNearbyDuplicate() {
+        val solution = Solution()
+        Assert.assertTrue(solution.containsNearbyDuplicate(intArrayOf(1,2,3,1), 3))
+        Assert.assertTrue(solution.containsNearbyDuplicate(intArrayOf(1,0,1,1), 1))
+        Assert.assertFalse(solution.containsNearbyDuplicate(intArrayOf(1,2,3,1,2,3), 2))
+        Assert.assertTrue(solution.containsNearbyDuplicate(intArrayOf(99,99), 2))
+    }
+}
