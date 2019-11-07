692166b60a8bbae519e0378fc0947ea21d604e6b
Bruce Lv <bruce@duolingo.com>
2019-11-07 20:57:57 +0800

    Solve reverse_linked_list

--- src/site/lvkun/leetcode/reverse_linked_list/Solution.kt
+++ src/site/lvkun/leetcode/reverse_linked_list/Solution.kt
@@ -1,2 +1,24 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.reverse_linked_list
 
+import site.lvkun.leetcode.common.ListNode
+
+class Solution {
+    fun reverseList(head: ListNode?): ListNode? {
+        val dummy = ListNode(-1)
+        dummy.next = head
+        var cur = head
+        var next = cur?.next
+        var nnext = next?.next
+
+        while (next != null) {
+            next.next = cur
+            cur = next
+            next = nnext
+            nnext = next?.next
+        }
+
+        dummy.next?.next = null
+
+        return cur
+    }
+}
--- src/site/lvkun/leetcode/reverse_linked_list/SolutionTest.kt
+++ src/site/lvkun/leetcode/reverse_linked_list/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.reverse_linked_list
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.ListNode
+
+class SolutionTest {
+    @Test
+    fun testReverseList() {
+        val solution = Solution()
+        Assert.assertEquals("5->4->3->2->1", solution.reverseList(ListNode.fromString("1->2->3->4->5")).toString())
+    }
+}
