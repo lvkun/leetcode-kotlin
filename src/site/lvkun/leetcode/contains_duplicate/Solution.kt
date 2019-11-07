a9c6823017056b0aaa84cedd194b2044aa0118b0
Bruce Lv <bruce@duolingo.com>
2019-11-07 21:44:54 +0800

    Solve contains_duplicate

--- src/site/lvkun/leetcode/contains_duplicate/Solution.kt
+++ src/site/lvkun/leetcode/contains_duplicate/Solution.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.contains_duplicate
 
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val set = mutableSetOf<Int>()
+        for (n in nums) {
+            if (set.contains(n)) {
+                return true
+            }
+            set.add(n)
+        }
+
+        return false
+    }
+}
--- src/site/lvkun/leetcode/contains_duplicate/SolutionTest.kt
+++ src/site/lvkun/leetcode/contains_duplicate/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.contains_duplicate
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testContainsDuplicate() {
+        val solution = Solution()
+        Assert.assertTrue(solution.containsDuplicate(intArrayOf(1,2,3,1)))
+        Assert.assertFalse(solution.containsDuplicate(intArrayOf(1,2,3,4)))
+        Assert.assertTrue(solution.containsDuplicate(intArrayOf(1,1,1,3,3,4,3,2,4,2)))
+    }
+}
