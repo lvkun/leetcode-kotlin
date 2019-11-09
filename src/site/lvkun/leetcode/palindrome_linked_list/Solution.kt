cf4af83d1da2493d0e24737167b18dc2a7b317cf
Bruce Lv <bruce@duolingo.com>
2019-11-09 21:56:57 +0800

    Solve palindrome_linked_list

--- src/site/lvkun/leetcode/palindrome_linked_list/Solution.kt
+++ src/site/lvkun/leetcode/palindrome_linked_list/Solution.kt
@@ -1,2 +1,69 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.palindrome_linked_list
 
+import site.lvkun.leetcode.common.ListNode
+
+class Solution {
+    fun reverseList(head: ListNode?, end: ListNode?): ListNode? {
+        var dummy = ListNode(-1)
+        dummy.next = head
+        var cur = head
+        var next: ListNode? = cur?.next ?: return head
+        var nnext = next?.next
+
+        while (next != end) {
+            val temp = nnext
+            next?.next = cur
+            cur = next
+            nnext = nnext?.next
+            next = temp
+        }
+
+        dummy.next?.next = null
+
+        return cur
+    }
+
+    fun isPalindrome(head: ListNode?): Boolean {
+        if (head == null) {
+            return true
+        }
+
+        if (head.next == null) {
+            return true
+        }
+
+        var fast = head
+        var slow = head
+
+        var size = 1
+
+        while (fast?.next != null) {
+            size += if (fast.next?.next == null) {
+                1
+            } else {
+                2
+            }
+
+            fast = fast.next?.next
+            slow = slow?.next
+        }
+
+        var h1 = reverseList(head, slow)
+        var h2 = slow
+
+        if (size % 2 == 1) {
+            h2 = h2?.next
+        }
+
+        while (h1 != null) {
+            if (h1.value != h2?.value) {
+                return false
+            }
+            h1 = h1.next
+            h2 = h2.next
+        }
+
+
+        return h2 == null
+    }
+}
--- src/site/lvkun/leetcode/palindrome_linked_list/SolutionTest.kt
+++ src/site/lvkun/leetcode/palindrome_linked_list/SolutionTest.kt
@@ -1,2 +1,17 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.palindrome_linked_list
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.ListNode
+
+class SolutionTest {
+    @Test
+    fun testIsPalindrome() {
+        val solution = Solution()
+        Assert.assertFalse(solution.isPalindrome(ListNode.fromString("1->2")))
+        Assert.assertTrue(solution.isPalindrome(ListNode.fromString("1")))
+        Assert.assertTrue(solution.isPalindrome(ListNode.fromString("1->2->2->1")))
+        Assert.assertTrue(solution.isPalindrome(ListNode.fromString("1->2->2->2->1")))
+        Assert.assertFalse(solution.isPalindrome(ListNode.fromString("1->3->2->4->3->2->1")))
+    }
+}
