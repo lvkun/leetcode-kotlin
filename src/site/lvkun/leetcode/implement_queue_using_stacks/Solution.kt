c4e12fcc3c5343129345a1368e2dab2ba80901ae
Bruce Lv <bruce@duolingo.com>
2019-11-09 13:50:43 +0800

    Solve implement_queue_using_stacks

--- src/site/lvkun/leetcode/implement_queue_using_stacks/Solution.kt
+++ src/site/lvkun/leetcode/implement_queue_using_stacks/Solution.kt
@@ -1,2 +1,45 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.implement_queue_using_stacks
 
+import java.util.*
+
+class MyQueue() {
+
+    /** Initialize your data structure here. */
+    private val stack1 = Stack<Int>()
+    private val stack2 = Stack<Int>()
+
+    var front: Int? = null
+
+    /** Push element x to the back of queue. */
+    fun push(x: Int) {
+        if (stack1.empty()) {
+            front = x
+        }
+        stack1.push(x)
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    fun pop(): Int {
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop())
+            }
+        }
+        return stack2.pop()
+    }
+
+    /** Get the front element. */
+    fun peek(): Int {
+        if (!stack2.empty()) {
+            return stack2.peek()
+        }
+
+        return front!!
+    }
+
+    /** Returns whether the queue is empty. */
+    fun empty(): Boolean {
+        return stack1.empty() && stack2.empty()
+    }
+
+}
--- src/site/lvkun/leetcode/implement_queue_using_stacks/SolutionTest.kt
+++ src/site/lvkun/leetcode/implement_queue_using_stacks/SolutionTest.kt
@@ -1,2 +1,17 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.implement_queue_using_stacks
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMyQueue() {
+        val queue = MyQueue()
+
+        queue.push(1);
+        queue.push(2);
+        Assert.assertEquals(1, queue.peek())
+        Assert.assertEquals(1, queue.pop())
+        Assert.assertFalse(queue.empty())
+    }
+}
