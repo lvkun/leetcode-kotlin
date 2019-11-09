cb3467e5c866568a93bb757e65bec1fa0804eed5
Bruce Lv <bruce@duolingo.com>
2019-11-09 23:31:56 +0800

    Solve add_digits

--- src/site/lvkun/leetcode/add_digits/Solution.kt
+++ src/site/lvkun/leetcode/add_digits/Solution.kt
@@ -1,2 +1,11 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.add_digits
 
+class Solution {
+    fun addDigits(num: Int): Int {
+        if (num < 10) {
+            return num
+        }
+        val result = num % 9
+        return if (result == 0) 9 else result
+    }
+}
--- src/site/lvkun/leetcode/add_digits/SolutionTest.kt
+++ src/site/lvkun/leetcode/add_digits/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.add_digits
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testAddDigits() {
+        val solution = Solution()
+
+        Assert.assertEquals(2, solution.addDigits(38))
+    }
+}
