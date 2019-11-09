c21fc4a53b610208cba8752fd84596358c63229a
Bruce Lv <bruce@duolingo.com>
2019-11-09 23:10:42 +0800

    Solve binary_tree_paths

--- src/site/lvkun/leetcode/binary_tree_paths/Solution.kt
+++ src/site/lvkun/leetcode/binary_tree_paths/Solution.kt
@@ -1,2 +1,41 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_tree_paths
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun binaryTreePaths(root: TreeNode?): List<String> {
+        if (root == null) {
+            return emptyList()
+        }
+
+        val result = binaryTreePathsInternal(root.left)
+        result.addAll(binaryTreePathsInternal((root.right)))
+
+        if (result.isEmpty()) {
+            result.add(mutableListOf())
+        }
+
+        return result.map {
+            it.add(0, root.`val`.toString())
+            it
+        }.map { it.joinToString("->") }
+    }
+
+    private fun binaryTreePathsInternal(root: TreeNode?): MutableList<MutableList<String>> {
+        if (root == null) {
+            return mutableListOf()
+        }
+
+        val result = binaryTreePathsInternal(root.left)
+        result.addAll(binaryTreePathsInternal((root.right)))
+
+        if (result.isEmpty()) {
+            result.add(mutableListOf())
+        }
+
+        return result.map {
+            it.add(0, root.`val`.toString())
+            it
+        }.toMutableList()
+    }
+}
--- src/site/lvkun/leetcode/binary_tree_paths/SolutionTest.kt
+++ src/site/lvkun/leetcode/binary_tree_paths/SolutionTest.kt
@@ -1,2 +1,20 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_tree_paths
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+import kotlin.math.exp
+
+class SolutionTest {
+    @Test
+    fun testBinaryTreePaths() {
+        val solution = Solution()
+        val result = solution.binaryTreePaths(TreeNode(1,
+                TreeNode(2,
+                        null,
+                        TreeNode(5)),
+                TreeNode(3)))
+        val expected = listOf("1->2->5", "1->3")
+        Assert.assertEquals(expected, result)
+    }
+}
