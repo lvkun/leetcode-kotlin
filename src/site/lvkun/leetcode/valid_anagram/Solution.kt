35df960b5a26508465ab779a8ce3822239d1265f
Bruce Lv <bruce@duolingo.com>
2019-11-09 22:26:50 +0800

    Solve valid_anagram

--- src/site/lvkun/leetcode/valid_anagram/Solution.kt
+++ src/site/lvkun/leetcode/valid_anagram/Solution.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.valid_anagram
 
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) {
+            return false
+        }
+
+        val map = IntArray(128)
+
+        for (i in s.indices) {
+            map[s[i].toInt()]++
+            map[t[i].toInt()]--
+        }
+
+        return map.all { it == 0 }
+    }
+}
--- src/site/lvkun/leetcode/valid_anagram/SolutionTest.kt
+++ src/site/lvkun/leetcode/valid_anagram/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.valid_anagram
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIsAnagram() {
+        val solution = Solution()
+        Assert.assertTrue(solution.isAnagram("anagram", "nagaram"))
+        Assert.assertFalse(solution.isAnagram("rat", "car"))
+    }
+}
