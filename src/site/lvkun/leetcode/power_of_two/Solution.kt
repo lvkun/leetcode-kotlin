4d70e0cf261548d40185f827dcebbdd495b5d76d
Bruce Lv <bruce@duolingo.com>
2019-11-09 12:41:05 +0800

    Solve power_of_two

--- src/site/lvkun/leetcode/power_of_two/Solution.kt
+++ src/site/lvkun/leetcode/power_of_two/Solution.kt
@@ -1,2 +1,25 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.power_of_two
 
+class Solution {
+    fun isPowerOfTwo(n: Int): Boolean {
+        if (n < 1) {
+            return false
+        }
+
+        if (n == 1) {
+            return true
+        }
+
+        var num = n
+
+        while (num % 2 == 0) {
+            num /= 2
+
+            if (num == 1) {
+                return true
+            }
+        }
+
+        return false
+    }
+}
--- src/site/lvkun/leetcode/power_of_two/SolutionTest.kt
+++ src/site/lvkun/leetcode/power_of_two/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.power_of_two
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIsPowerOfTwo() {
+        val solution = Solution()
+        Assert.assertTrue(solution.isPowerOfTwo(1))
+        Assert.assertTrue(solution.isPowerOfTwo(16))
+        Assert.assertFalse(solution.isPowerOfTwo(218))
+    }
+}
