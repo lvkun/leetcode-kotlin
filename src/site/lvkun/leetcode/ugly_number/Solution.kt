d16983056b9316520357928e70a597195a836e58
Bruce Lv <bruce@duolingo.com>
2019-11-09 23:43:50 +0800

    Solve ugly_number

--- src/site/lvkun/leetcode/ugly_number/Solution.kt
+++ src/site/lvkun/leetcode/ugly_number/Solution.kt
@@ -1,2 +1,25 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.ugly_number
 
+class Solution {
+    fun isUgly(num: Int): Boolean {
+        if (num == 0) {
+            return false
+        }
+        var n = num
+
+        n = divide(n, 2)
+        n = divide(n, 3)
+        n = divide(n, 5)
+
+        return n == 1
+    }
+
+    fun divide(num: Int, d: Int): Int {
+        var n = num
+        while (n % d == 0) {
+            n /= d
+        }
+
+        return n
+    }
+}
--- src/site/lvkun/leetcode/ugly_number/SolutionTest.kt
+++ src/site/lvkun/leetcode/ugly_number/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.ugly_number
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIsUgly() {
+        val solution = Solution()
+        Assert.assertTrue(solution.isUgly(6))
+        Assert.assertTrue(solution.isUgly(8))
+        Assert.assertFalse(solution.isUgly(14))
+    }
+}
