77e4a4f5416812d2e14526981c6551299577d6b5
Bruce Lv <bruce@duolingo.com>
2019-11-10 00:17:57 +0800

    Solve missing_number

--- src/site/lvkun/leetcode/missing_number/Solution.kt
+++ src/site/lvkun/leetcode/missing_number/Solution.kt
@@ -1,2 +1,9 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.missing_number
 
+class Solution {
+    fun missingNumber(nums: IntArray): Int {
+        val n = nums.size
+        val totalSum = n * (n + 1) / 2
+        return totalSum - nums.sum()
+    }
+}
--- src/site/lvkun/leetcode/missing_number/SolutionTest.kt
+++ src/site/lvkun/leetcode/missing_number/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.missing_number
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMissingNumber() {
+        val solution = Solution()
+        Assert.assertEquals(2, solution.missingNumber(intArrayOf(3,0,1)))
+        Assert.assertEquals(8, solution.missingNumber(intArrayOf(9,6,4,2,3,5,7,0,1)))
+    }
+}
