ab0a328edb3068bf770f5c4c568d2564d91da4dd
Bruce Lv <bruce@duolingo.com>
2019-11-10 17:34:07 +0800

    Solve move_zeroes

--- src/site/lvkun/leetcode/move_zeroes/Solution.kt
+++ src/site/lvkun/leetcode/move_zeroes/Solution.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.move_zeroes
 
+class Solution {
+    fun moveZeroes(nums: IntArray): Unit {
+        var numberOfZero = 0
+        for (i in nums.indices) {
+            if (nums[i] == 0) {
+                numberOfZero++
+            } else {
+                if (numberOfZero > 0) {
+                    nums[i - numberOfZero] = nums[i]
+                }
+            }
+        }
+
+        if (numberOfZero > 0) {
+            for (i in nums.size - numberOfZero .. nums.lastIndex) {
+                nums[i] = 0
+            }
+        }
+    }
+}
--- src/site/lvkun/leetcode/move_zeroes/SolutionTest.kt
+++ src/site/lvkun/leetcode/move_zeroes/SolutionTest.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.move_zeroes
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMoveZeroes() {
+        val solution = Solution()
+        val result = intArrayOf(0,1,0,3,12)
+        solution.moveZeroes(result)
+        val expected = intArrayOf(1,3,12,0,0)
+        Assert.assertArrayEquals(expected, result)
+    }
+}
