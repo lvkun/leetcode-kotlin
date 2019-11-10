e86da47033d7f626c6c24a349ea25e265ce004c4
Bruce Lv <bruce@duolingo.com>
2019-11-10 19:13:34 +0800

    Solve regular_expression_matching(need redo)

--- src/site/lvkun/leetcode/regular_expression_matching/Solution.kt
+++ src/site/lvkun/leetcode/regular_expression_matching/Solution.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.regular_expression_matching
 
+/** TODO: redo */
+class Solution {
+    fun isMatch(s: String, p: String): Boolean {
+        val dp = Array(s.length + 1) {
+            BooleanArray(p.length + 1)
+        }
+
+        dp[s.length][p.length] = true
+
+        for (i in s.length downTo 0) {
+            for (j in p.length - 1 downTo 0) {
+                val firstMatch = i < s.length &&
+                        (p[j] == s[i] || p[j] == '.')
+
+                if (j + 1 < p.length && p[j + 1] == '*') {
+                    dp[i][j] = dp[i][j+2] || firstMatch && dp[i + 1][j]
+                } else {
+                    dp[i][j] = firstMatch && dp[i + 1][j + 1]
+                }
+            }
+        }
+
+
+        return dp[0][0]
+    }
+}
--- src/site/lvkun/leetcode/regular_expression_matching/SolutionTest.kt
+++ src/site/lvkun/leetcode/regular_expression_matching/SolutionTest.kt
@@ -1,2 +1,16 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.regular_expression_matching
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIsMatch() {
+        val solution = Solution()
+        Assert.assertFalse(solution.isMatch("aa", "a"))
+        Assert.assertTrue(solution.isMatch("aa", "a*"))
+        Assert.assertTrue(solution.isMatch("ab", ".*"))
+        Assert.assertTrue(solution.isMatch("aab", "c*a*b"))
+        Assert.assertFalse(solution.isMatch("mississippi", "mis*is*p*."))
+    }
+}
