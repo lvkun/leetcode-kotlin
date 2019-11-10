d88a472216cb6e50b388f779244e939d4d29c4ac
Bruce Lv <bruce@duolingo.com>
2019-11-10 17:53:47 +0800

    Solve word_patter

--- src/site/lvkun/leetcode/word_pattern/Solution.kt
+++ src/site/lvkun/leetcode/word_pattern/Solution.kt
@@ -1,2 +1,33 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.word_pattern
 
+class Solution {
+    fun wordPattern(pattern: String, str: String): Boolean {
+        val patternCharArray = pattern.toCharArray()
+        val strList = str.split(" ")
+
+        if (patternCharArray.size != strList.size) {
+            return false
+        }
+
+        val mapPatterToString = mutableMapOf<Char, String>()
+        val mapStringToPattern = mutableMapOf<String, Char>()
+        for (i in patternCharArray.indices) {
+            val p = patternCharArray[i]
+            val string = strList[i]
+            if (!mapPatterToString.containsKey(p)) {
+                if (mapStringToPattern.containsKey(string)) {
+                    return false
+                }
+
+                mapPatterToString[p] = string
+                mapStringToPattern[string] = p
+            } else {
+                if (mapPatterToString[p] != string || mapStringToPattern[string] != p) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+}
--- src/site/lvkun/leetcode/word_pattern/SolutionTest.kt
+++ src/site/lvkun/leetcode/word_pattern/SolutionTest.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.word_pattern
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testWordPattern() {
+        val solution = Solution()
+        Assert.assertTrue(solution.wordPattern("abba", "dog cat cat dog"))
+        Assert.assertFalse(solution.wordPattern("abba", "dog cat cat fish"))
+        Assert.assertFalse(solution.wordPattern("aaaa", "dog cat cat dog"))
+        Assert.assertFalse(solution.wordPattern("abba", "dog dog dog dog"))
+    }
+}
