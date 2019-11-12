097ce5708f8185f2c0a1735ca4017ca18af8be71
Bruce Lv <bruce@duolingo.com>
2019-11-13 00:31:49 +0800

    Solve three_sum(Redo)

--- src/site/lvkun/leetcode/three_sum/Solution.kt
+++ src/site/lvkun/leetcode/three_sum/Solution.kt
@@ -1,2 +1,54 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.three_sum
 
+/** TODO: redo */
+class Solution {
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        if (nums.size < 3) {
+            return emptyList()
+        }
+
+        nums.sort()
+
+        var left = 0
+        var center = left + 1
+        var right = nums.lastIndex
+
+        val result = mutableListOf<List<Int>>()
+
+        for (left in 0 .. nums.size - 2) {
+            if (nums[left] > 0) {
+                return result
+            }
+
+            if (left > 0 && nums[left] == nums[left - 1]) {
+                continue
+            }
+
+            center = left + 1
+            right = nums.lastIndex
+
+            while (center < right) {
+                val sum = nums[left] + nums[center] + nums[right]
+                when {
+                    sum > 0 ->
+                        right--
+                    sum < 0 ->
+                        center++
+                    else -> {
+                        result.add(listOf(nums[left], nums[center], nums[right]))
+                        while (center < right && nums[center] == nums[center + 1]) {
+                            center++
+                        }
+                        while (center < right && nums[right] == nums[right - 1]) {
+                            right--
+                        }
+                        center++
+                        right--
+                    }
+                }
+            }
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/three_sum/SolutionTest.kt
+++ src/site/lvkun/leetcode/three_sum/SolutionTest.kt
@@ -1,2 +1,17 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.three_sum
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testThreeSum() {
+        val solution = Solution()
+        var result = solution.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))
+        var expected = listOf(listOf(-1, 0, 1),
+                listOf(-1, -1, 2))
+        Assert.assertEquals(expected.toSet(), result.toSet())
+
+        result = solution.threeSum(intArrayOf(4,4,3,-5,0,0,0,-2,3,-5,-5,0))
+    }
+}
