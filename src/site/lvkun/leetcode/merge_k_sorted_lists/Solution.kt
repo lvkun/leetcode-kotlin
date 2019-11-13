5e07eb24a67d2ef4cb3bb4da6d8d627f24cd1b3d
Bruce Lv <bruce@duolingo.com>
2019-11-13 22:52:08 +0800

    Solve merge_k_sorted_lists

--- src/site/lvkun/leetcode/merge_k_sorted_lists/Solution.kt
+++ src/site/lvkun/leetcode/merge_k_sorted_lists/Solution.kt
@@ -1,2 +1,33 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.merge_k_sorted_lists
 
+import site.lvkun.leetcode.common.ListNode
+import java.util.*
+import kotlin.Comparator
+
+class Solution {
+    fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+        val dummy = ListNode(0)
+        var prev = dummy
+
+        val queue = PriorityQueue<ListNode>(Comparator<ListNode> { node1, node2 -> node1.`val` - node2.`val` })
+
+        for (node in lists) {
+            if (node != null) {
+                queue.add(node)
+            }
+        }
+
+        while (queue.isNotEmpty()) {
+            val node = queue.poll()
+
+            prev.next = node
+            prev = node
+
+            if (prev.next != null) {
+                queue.add(prev.next)
+            }
+        }
+
+        return dummy.next
+    }
+}
--- src/site/lvkun/leetcode/merge_k_sorted_lists/SolutionTest.kt
+++ src/site/lvkun/leetcode/merge_k_sorted_lists/SolutionTest.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.merge_k_sorted_lists
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.ListNode
+
+class SolutionTest {
+    @Test
+    fun testMergeKLists() {
+        val solution = Solution()
+        val result = solution.mergeKLists(arrayOf(
+                ListNode.fromString("1->4->5"),
+                ListNode.fromString("1->3->4"),
+                ListNode.fromString("2->6")))
+        val expected = ListNode.fromString("1->1->2->3->4->4->5->6")
+        Assert.assertEquals(expected, result)
+    }
+}
