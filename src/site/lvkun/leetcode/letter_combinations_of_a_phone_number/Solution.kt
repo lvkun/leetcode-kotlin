cde60dc4d0dafc810e3e0d99dd20c920149b8bb4
Bruce Lv <bruce@duolingo.com>
2019-11-13 23:44:08 +0800

    Solve letter_combinations_of_a_phone_number

--- src/site/lvkun/leetcode/letter_combinations_of_a_phone_number/Solution.kt
+++ src/site/lvkun/leetcode/letter_combinations_of_a_phone_number/Solution.kt
@@ -1,2 +1,50 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.letter_combinations_of_a_phone_number
 
+class Solution {
+
+    fun letterCombinations(digits: String): List<String> {
+        if (digits.isEmpty()) {
+            return emptyList()
+        }
+
+        val resultList = mutableListOf<String>()
+        combine(digits.toCharArray(), 0, emptyList(), resultList)
+        return resultList
+    }
+
+    private fun combine(digits: CharArray, index: Int, result: List<Char>, resultList: MutableList<String>) {
+        if (index >= digits.size) {
+            resultList.add(result.joinToString(""))
+            return
+        }
+
+        val digit = digits[index]
+        if (!map.containsKey(digit)) {
+            resultList.add(result.joinToString(""))
+            return
+        }
+        val chars = map[digit]
+        if (chars != null) {
+            for (c in chars) {
+                val newResult = result.toMutableList()
+                newResult.add(c)
+                combine(digits, index + 1, newResult, resultList)
+            }
+        }
+
+    }
+
+    companion object {
+        private val map = mapOf(
+                '2' to "abc".toCharArray(),
+                '3' to "def".toCharArray(),
+                '4' to "ghi".toCharArray(),
+                '5' to "jkl".toCharArray(),
+                '6' to "mno".toCharArray(),
+                '7' to "pqrs".toCharArray(),
+                '8' to "tuv".toCharArray(),
+                '9' to "wxyz".toCharArray(),
+                '0' to " ".toCharArray()
+        )
+    }
+}
--- src/site/lvkun/leetcode/letter_combinations_of_a_phone_number/SolutionTest.kt
+++ src/site/lvkun/leetcode/letter_combinations_of_a_phone_number/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.letter_combinations_of_a_phone_number
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testLetterCombinations() {
+        val solution = Solution()
+        val result = solution.letterCombinations("23")
+        val expected = listOf("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf")
+        Assert.assertEquals(expected, result)
+    }
+}
