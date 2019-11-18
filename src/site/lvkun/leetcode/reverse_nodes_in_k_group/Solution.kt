bd90d458c2139396c5456e9d45004d5c8da40c67
Bruce Lv <bruce@duolingo.com>
2019-11-18 23:56:01 +0800

    Solve reverse_nodes_in_k_group

--- src/site/lvkun/leetcode/reverse_nodes_in_k_group/Solution.kt
+++ src/site/lvkun/leetcode/reverse_nodes_in_k_group/Solution.kt
@@ -1,2 +1,70 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.reverse_nodes_in_k_group
 
+import site.lvkun.leetcode.common.ListNode
+
+class Solution {
+
+    private class LinkedStack {
+        private val dummy = ListNode(0)
+
+        private var tail: ListNode? = null
+
+        fun push(node: ListNode?) {
+            if (tail == null) {
+                tail = node
+            }
+
+            node?.next = dummy.next
+            dummy.next = node
+        }
+
+        fun pop(): ListNode? {
+            val top = dummy.next
+            dummy.next = null
+            return top
+        }
+
+        fun tail(): ListNode? {
+            val t = tail
+            tail = null
+            return t
+        }
+    }
+
+    fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
+        if (k <= 1) {
+            return head
+        }
+
+        val dummy = ListNode(-1)
+
+        var cur = head
+        val stack = LinkedStack()
+        var newHead: ListNode? = dummy
+        while (lengthGreaterThan(cur, k)) {
+            for (i in 0 until k) {
+                val next = cur?.next
+                stack.push(cur)
+                cur = next
+            }
+            newHead?.next = stack.pop()
+            newHead = stack.tail()
+            newHead?.next = null
+        }
+
+        newHead?.next = cur
+
+        return dummy.next
+    }
+
+    private fun lengthGreaterThan(node: ListNode?, k: Int): Boolean {
+        var count = 0
+        var cur = node
+        while (cur != null && count < k) {
+            count++
+            cur = cur.next
+        }
+
+        return count == k
+    }
+}
--- src/site/lvkun/leetcode/reverse_nodes_in_k_group/SolutionTest.kt
+++ src/site/lvkun/leetcode/reverse_nodes_in_k_group/SolutionTest.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.reverse_nodes_in_k_group
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.ListNode
+
+class SolutionTest {
+    @Test
+    fun testReverseKGroup() {
+        val solution = Solution()
+        var result = solution.reverseKGroup(ListNode.fromString("1->2->3->4->5"), 2)
+        Assert.assertEquals(ListNode.fromString("2->1->4->3->5"), result)
+        result = solution.reverseKGroup(ListNode.fromString("1->2->3->4->5"), 3)
+        Assert.assertEquals(ListNode.fromString("3->2->1->4->5"), result)
+
+        result = solution.reverseKGroup(ListNode.fromString("1->2"), 2)
+        Assert.assertEquals(ListNode.fromString("2->1"), result)
+    }
+}
