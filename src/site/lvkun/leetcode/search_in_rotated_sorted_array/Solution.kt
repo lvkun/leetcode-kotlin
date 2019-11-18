2ba28b230bc2a715ceec866c7fee9819b7ebbd95
Bruce Lv <bruce@duolingo.com>
2019-11-19 00:43:23 +0800

    Solve search_in_rotated_sorted_array

--- src/site/lvkun/leetcode/search_in_rotated_sorted_array/Solution.kt
+++ src/site/lvkun/leetcode/search_in_rotated_sorted_array/Solution.kt
@@ -1,2 +1,31 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.search_in_rotated_sorted_array
 
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var low = 0
+        var high = nums.lastIndex
+
+        while (low < high) {
+            val mid = (low + high) / 2
+            if (nums[mid] == target) {
+                return mid
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid
+                } else {
+                    low = mid + 1
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1
+                } else {
+                    high = mid
+                }
+            }
+        }
+
+        return if (low == high && nums[low] == target) low else -1
+    }
+}
--- src/site/lvkun/leetcode/search_in_rotated_sorted_array/SolutionTest.kt
+++ src/site/lvkun/leetcode/search_in_rotated_sorted_array/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.search_in_rotated_sorted_array
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testSearch() {
+        val solution = Solution()
+        Assert.assertEquals(4, solution.search(intArrayOf(4,5,6,7,0,1,2), 0))
+        Assert.assertEquals(-1, solution.search(intArrayOf(4,5,6,7,0,1,2), 3))
+    }
+}
