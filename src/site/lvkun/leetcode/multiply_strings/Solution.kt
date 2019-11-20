4a63bf4353515cc586313e5631f166693ee3818a
Bruce Lv <bruce@duolingo.com>
2019-11-20 23:26:59 +0800

    Solve multiply_strings

--- src/site/lvkun/leetcode/multiply_strings/Solution.kt
+++ src/site/lvkun/leetcode/multiply_strings/Solution.kt
@@ -1,2 +1,45 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.multiply_strings
 
+class Solution {
+
+    fun String.at(i: Int): Int {
+        return this[this.length - i - 1] - '0'
+    }
+
+    fun IntArray.plus(index: Int, num: Int) {
+        val newNum = num + this[index]
+        this[index] = (newNum % 10)
+
+        if (newNum >= 10) {
+            this.plus(index + 1, newNum / 10)
+        }
+    }
+
+    fun multiply(num1: String, num2: String): String {
+        val n = num1.length
+        val m = num2.length
+
+        val result = IntArray(n + m)
+
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                val k = i + j
+                val sum = num1.at(i) * num2.at(j)
+
+                result.plus(k, sum)
+            }
+        }
+
+        result.reverse()
+        val strResult = result.joinToString("")
+        var startIndex = result.indexOfFirst { n ->
+            n != 0
+        }
+
+        if (startIndex == -1) {
+            startIndex = strResult.lastIndex
+        }
+
+        return strResult.substring(startIndex)
+    }
+}
--- src/site/lvkun/leetcode/multiply_strings/SolutionTest.kt
+++ src/site/lvkun/leetcode/multiply_strings/SolutionTest.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.multiply_strings
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun test() {
+        val solution = Solution()
+//        Assert.assertEquals("6", solution.multiply("2", "3"))
+//        Assert.assertEquals("264", solution.multiply("12", "22"))
+        Assert.assertEquals("56088", solution.multiply("123", "456"))
+        Assert.assertEquals("0", solution.multiply("9133", "0"))
+    }
+}
