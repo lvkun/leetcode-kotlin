ffc06ee35fc829b1c9064dc28cc49688cd529a10
Bruce Lv <bruce@duolingo.com>
2019-11-24 23:07:39 +0800

    Solve wildcard_matching

--- src/site/lvkun/leetcode/wildcard_matching/Solution.kt
+++ src/site/lvkun/leetcode/wildcard_matching/Solution.kt
@@ -1,2 +1,27 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.wildcard_matching
 
+/** TODO: redo */
+class Solution {
+    fun isMatch(s: String, p: String): Boolean {
+        val dp = Array(s.length + 1) {
+            BooleanArray(p.length + 1)
+        }
+
+        dp[0][0] = true
+        for (i in 1..p.length) {
+            dp[0][i] = dp[0][i - 1] && p[i - 1] == '*'
+        }
+
+        for (i in 1..s.length) {
+            for (j in 1..p.length) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1]
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1]
+                }
+            }
+        }
+
+        return dp[s.length][p.length]
+    }
+}
--- src/site/lvkun/leetcode/wildcard_matching/SolutionTest.kt
+++ src/site/lvkun/leetcode/wildcard_matching/SolutionTest.kt
@@ -1,2 +1,26 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.wildcard_matching
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIsMatch() {
+        val solution = Solution()
+        Assert.assertFalse(solution.isMatch("aa", "a"))
+        Assert.assertTrue(solution.isMatch("aa", "*"))
+        Assert.assertFalse(solution.isMatch("cb", "?a"))
+        Assert.assertTrue(solution.isMatch("adceb", "*a*b"))
+        Assert.assertFalse(solution.isMatch("acdcb", "a*c?b"))
+
+        // aa
+        // a
+
+        // aa
+        // *
+
+        // cb
+        // ?a
+
+    }
+}
