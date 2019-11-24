be600716560b979ae2616cd6dd3a17c97d783cab
Bruce Lv <bruce@duolingo.com>
2019-11-24 23:51:58 +0800

    Solve group_anagrams

--- src/site/lvkun/leetcode/group_anagrams/Solution.kt
+++ src/site/lvkun/leetcode/group_anagrams/Solution.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.group_anagrams
 
+import java.util.*
+
+class Solution {
+    fun String.key(): String {
+        val count = IntArray(26)
+        for (c in toCharArray()) {
+            count[c - 'a'] += 1
+        }
+        return count.joinToString(",")
+    }
+
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val value = 0
+        val map = mutableMapOf<String, MutableList<String>>()
+        for (str in strs) {
+            val key = str.key()
+            if (map.containsKey(key)) {
+                map[key]?.add(str)
+            } else {
+                map[key] = mutableListOf(str)
+            }
+        }
+
+        return map.values.toList()
+    }
+}
--- src/site/lvkun/leetcode/group_anagrams/SolutionTest.kt
+++ src/site/lvkun/leetcode/group_anagrams/SolutionTest.kt
@@ -1,2 +1,21 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.group_anagrams
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testGroupAnagrams() {
+        val solution = Solution()
+
+        val result = solution.groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat"))
+        val expected = listOf(listOf("ate","eat","tea"), listOf("nat","tan"), listOf("bat"))
+
+        Assert.assertEquals(result.size, expected.size)
+        for (i in result.indices) {
+            val resultItem = result[i].toSet()
+            val expectedItem = expected[i].toSet()
+            Assert.assertEquals(expectedItem, resultItem)
+        }
+    }
+}
