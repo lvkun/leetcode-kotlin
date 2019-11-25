28107848b14fc3d4b43a96d79447174ac4261e22
Bruce Lv <bruce@duolingo.com>
2019-11-25 09:47:12 +0800

    Solve powx_n

--- src/site/lvkun/leetcode/powx_n/Solution.kt
+++ src/site/lvkun/leetcode/powx_n/Solution.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.powx_n
 
+class Solution {
+    fun myPow(x: Double, n: Int): Double {
+        if (n < 0) {
+            return powInternal(1 / x, -n)
+        }
+        return powInternal(x, n)
+    }
+
+    private fun powInternal(x: Double, n: Int): Double {
+        var n = n
+        var x = x
+        var result = 1.0
+
+        while (n >= 1) {
+            if (n % 2 == 1) {
+                n -= 1
+                result *= x
+            } else {
+                n /= 2
+                x *= x
+            }
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/powx_n/SolutionTest.kt
+++ src/site/lvkun/leetcode/powx_n/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.powx_n
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMyPow() {
+        val solution = Solution()
+//        Assert.assertEquals(1024.00000, solution.myPow(2.0, 10), 0.01)
+//        Assert.assertEquals(9.26100, solution.myPow(2.10000, 3), 0.01)
+        Assert.assertEquals(0.25000, solution.myPow(2.00000, -2), 0.01)
+    }
+}
