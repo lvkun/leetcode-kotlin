9d9d063836fccef9944a83871f312e93ffaf162d
Bruce Lv <bruce@duolingo.com>
2019-11-25 22:25:59 +0800

    Solve merge_intervals

--- src/site/lvkun/leetcode/merge_intervals/Solution.kt
+++ src/site/lvkun/leetcode/merge_intervals/Solution.kt
@@ -1,2 +1,31 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.merge_intervals
 
+class Solution {
+    private fun isOverlap(interval: IntArray, prevInterval: IntArray) =
+            interval[0] <= prevInterval[1]
+
+    private fun merge(interval: IntArray, prevInterval: IntArray) =
+            intArrayOf(prevInterval[0], maxOf(prevInterval[1], interval[1]))
+
+    fun merge(intervals: Array<IntArray>): Array<IntArray> {
+        intervals.sortBy { it[0] }
+
+        val result = mutableListOf<IntArray>()
+        var prevInterval: IntArray? = null
+        for (interval in intervals) {
+            if (prevInterval != null && isOverlap(interval, prevInterval)) {
+                prevInterval = merge(interval, prevInterval)
+            } else {
+                if (prevInterval != null) {
+                    result.add(prevInterval)
+                }
+                prevInterval = interval
+            }
+        }
+        if (prevInterval != null) {
+            result.add(prevInterval)
+        }
+
+        return result.toTypedArray()
+    }
+}
--- src/site/lvkun/leetcode/merge_intervals/SolutionTest.kt
+++ src/site/lvkun/leetcode/merge_intervals/SolutionTest.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.merge_intervals
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMerge() {
+        val solution = Solution()
+        var result = solution.merge(arrayOf(intArrayOf(1, 3), intArrayOf(2, 6), intArrayOf(8, 10), intArrayOf(15, 18)))
+        var expected = arrayOf(intArrayOf(1, 6), intArrayOf(8, 10), intArrayOf(15, 18))
+        Assert.assertArrayEquals(expected, result)
+
+        result = solution.merge(arrayOf(intArrayOf(1, 2), intArrayOf(2, 3)))
+        expected = arrayOf(intArrayOf(1, 3))
+        Assert.assertArrayEquals(expected, result)
+
+        result = solution.merge(arrayOf(intArrayOf(1, 4), intArrayOf(2, 3)))
+        expected = arrayOf(intArrayOf(1, 4))
+        Assert.assertArrayEquals(expected, result)
+    }
+}
