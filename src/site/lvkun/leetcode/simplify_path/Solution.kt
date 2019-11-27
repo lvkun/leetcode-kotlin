3c6e5a128da10b70639f18f5f8099ab7f60f2ed7
Bruce Lv <bruce@duolingo.com>
2019-11-28 00:19:19 +0800

    Solve simplify_path

--- src/site/lvkun/leetcode/simplify_path/Solution.kt
+++ src/site/lvkun/leetcode/simplify_path/Solution.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.simplify_path
 
+import java.util.*
+
+class Solution {
+    fun simplifyPath(path: String): String {
+        val stack = Stack<String>()
+
+        for (dir in path.split("/")) {
+            when {
+                dir.isEmpty() -> {
+                }
+                dir == "." -> {
+                }
+                dir == ".." -> {
+                    if (stack.isNotEmpty()) {
+                        stack.pop()
+                    }
+                }
+                else -> {
+                    stack.push(dir)
+                }
+            }
+        }
+
+        return "/" + stack.joinToString("/")
+    }
+}
--- src/site/lvkun/leetcode/simplify_path/SolutionTest.kt
+++ src/site/lvkun/leetcode/simplify_path/SolutionTest.kt
@@ -1,2 +1,17 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.simplify_path
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testSimplifyPath() {
+        val solution = Solution()
+        Assert.assertEquals("/home", solution.simplifyPath("/home/"))
+        Assert.assertEquals("/", solution.simplifyPath("/../"))
+        Assert.assertEquals("/home/foo", solution.simplifyPath("/home//foo/"))
+        Assert.assertEquals("/c", solution.simplifyPath("/a/./b/../../c/"))
+        Assert.assertEquals("/c", solution.simplifyPath("/a/../../b/../c//.//"))
+        Assert.assertEquals("/a/b/c", solution.simplifyPath("/a//b////c/d//././/.."))
+    }
+}
