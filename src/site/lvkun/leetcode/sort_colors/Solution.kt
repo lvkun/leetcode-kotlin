29453c2768ecc9abcde02c272b3713fed5682829
Bruce Lv <bruce@duolingo.com>
2019-11-28 00:45:13 +0800

    Solve sort_colors

--- src/site/lvkun/leetcode/sort_colors/Solution.kt
+++ src/site/lvkun/leetcode/sort_colors/Solution.kt
@@ -1,2 +1,33 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.sort_colors
 
+class Solution {
+    fun IntArray.swap(i1: Int, i2: Int) {
+        val temp = get(i1)
+        set(i1 ,get(i2))
+        set(i2, temp)
+    }
+
+    fun sortColors(nums: IntArray): Unit {
+        var low = 0
+        var high = nums.lastIndex
+
+        var cur = 0
+
+        while (cur <= high) {
+            when (nums[cur]) {
+                0 -> {
+                    nums.swap(cur, low)
+                    cur++
+                    low++
+                }
+                2 -> {
+                    nums.swap(cur, high)
+                    high--
+                }
+                1 -> {
+                    cur++
+                }
+            }
+        }
+    }
+}
--- src/site/lvkun/leetcode/sort_colors/SolutionTest.kt
+++ src/site/lvkun/leetcode/sort_colors/SolutionTest.kt
@@ -1,2 +1,26 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.sort_colors
 
+import org.junit.Assert
+import org.junit.Test
+import kotlin.math.exp
+
+class SolutionTest {
+    @Test
+    fun testSortColors() {
+        val solution = Solution()
+        var result = intArrayOf(2,0,2,1,1,0)
+        solution.sortColors(result)
+        var expected = intArrayOf(0,0,1,1,2,2)
+        Assert.assertArrayEquals(expected, result)
+
+        result = intArrayOf(2,0,1)
+        solution.sortColors(result)
+        expected = intArrayOf(0,1,2)
+        Assert.assertArrayEquals(expected, result)
+
+        result = intArrayOf(1,2,0)
+        solution.sortColors(result)
+        expected = intArrayOf(0,1,2)
+        Assert.assertArrayEquals(expected, result)
+    }
+}
