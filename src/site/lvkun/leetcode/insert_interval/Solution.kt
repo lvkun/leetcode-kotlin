5c16a75845677894a4681db490952da6f12765e4
Bruce Lv <bruce@duolingo.com>
2019-11-28 00:04:44 +0800

    Solve insert_interval

--- src/site/lvkun/leetcode/insert_interval/Solution.kt
+++ src/site/lvkun/leetcode/insert_interval/Solution.kt
@@ -1,2 +1,41 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.insert_interval
 
+class Solution {
+    private fun isOverlap(interval: IntArray, otherInterval: IntArray) =
+            interval[0] <= otherInterval[0] && interval[1] >= otherInterval[0] ||
+                    otherInterval[0] <= interval[0] && otherInterval[1] >= interval[0]
+
+    private fun merge(interval: IntArray, otherInterval: IntArray) =
+            intArrayOf(minOf(otherInterval[0], interval[0]), maxOf(otherInterval[1], interval[1]))
+
+    fun insert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
+        if (intervals.isEmpty()) {
+            return arrayOf(newInterval)
+        }
+
+        val result = mutableListOf<IntArray>()
+
+        var mergedInterval: IntArray = newInterval
+        var hasAdded = false
+        for (interval in intervals) {
+            when {
+                interval[1] < mergedInterval[0] -> result.add(interval)
+                interval[0] > mergedInterval[1] -> {
+                    if (!hasAdded) {
+                        result.add(mergedInterval)
+                        hasAdded = true
+                    }
+                    result.add(interval)
+                }
+                isOverlap(interval, mergedInterval) -> mergedInterval = merge(interval, mergedInterval)
+                else -> result.add(interval)
+            }
+        }
+
+        if (!hasAdded) {
+            result.add(mergedInterval)
+        }
+
+        return result.toTypedArray()
+    }
+}
--- src/site/lvkun/leetcode/insert_interval/SolutionTest.kt
+++ src/site/lvkun/leetcode/insert_interval/SolutionTest.kt
@@ -1,2 +1,30 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.insert_interval
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testInsert() {
+        val solution = Solution()
+        var result = solution.insert(arrayOf(intArrayOf(1, 3), intArrayOf(6, 9)), intArrayOf(2, 5))
+        var expected = arrayOf(intArrayOf(1, 5), intArrayOf(6, 9))
+        Assert.assertArrayEquals(expected, result)
+
+        result = solution.insert(arrayOf(intArrayOf(1, 2), intArrayOf(3, 5), intArrayOf(6, 7), intArrayOf(8, 10), intArrayOf(12, 16)), intArrayOf(4, 8))
+        expected = arrayOf(intArrayOf(1, 2), intArrayOf(3, 10), intArrayOf(12, 16))
+        Assert.assertArrayEquals(expected, result)
+
+        result = solution.insert(arrayOf(), intArrayOf(5, 7))
+        expected = arrayOf(intArrayOf(5, 7))
+        Assert.assertArrayEquals(expected, result)
+
+        result = solution.insert(arrayOf(intArrayOf(1, 5)), intArrayOf(2, 3))
+        expected = arrayOf(intArrayOf(1, 5))
+        Assert.assertArrayEquals(expected, result)
+
+        result = solution.insert(arrayOf(intArrayOf(1, 5)), intArrayOf(6, 8))
+        expected = arrayOf(intArrayOf(1, 5), intArrayOf(6, 8))
+        Assert.assertArrayEquals(expected, result)
+    }
+}
