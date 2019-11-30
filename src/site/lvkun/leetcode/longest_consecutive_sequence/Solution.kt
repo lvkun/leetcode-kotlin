10020721eb0952c57b24784c522cf82416367852
Bruce Lv <bruce@duolingo.com>
2019-11-30 16:41:23 +0800

    Solve longest_consecutive_sequence

--- src/site/lvkun/leetcode/longest_consecutive_sequence/Solution.kt
+++ src/site/lvkun/leetcode/longest_consecutive_sequence/Solution.kt
@@ -1,2 +1,30 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.longest_consecutive_sequence
 
+class Solution {
+    fun longestConsecutive(nums: IntArray): Int {
+        val set = mutableSetOf<Int>()
+        for (i in nums) {
+            set.add(i)
+        }
+
+        var number = 0
+        var length = 0
+
+        var maxLength = 0
+        for (i in set) {
+            if (!set.contains(i - 1)) {
+                number = i
+                length = 1
+
+                while (set.contains(number + 1)) {
+                    number++
+                    length++
+                }
+
+                maxLength = maxOf(maxLength, length)
+            }
+        }
+
+        return maxLength
+    }
+}
--- src/site/lvkun/leetcode/longest_consecutive_sequence/SolutionTest.kt
+++ src/site/lvkun/leetcode/longest_consecutive_sequence/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.longest_consecutive_sequence
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testLongestConsecutive() {
+        val solution = Solution()
+        Assert.assertEquals(4, solution.longestConsecutive(intArrayOf(100, 4, 200, 1, 3, 2)))
+    }
+}
