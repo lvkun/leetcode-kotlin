bc760cb35717ea3129fa71ee69cc1972deef2a71
Bruce Lv <bruce@duolingo.com>
2019-11-30 14:27:33 +0800

    Solve largest_rectangle_in_histogram

--- src/site/lvkun/leetcode/largest_rectangle_in_histogram/Solution.kt
+++ src/site/lvkun/leetcode/largest_rectangle_in_histogram/Solution.kt
@@ -1,2 +1,25 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.largest_rectangle_in_histogram
 
+import java.util.*
+
+class Solution {
+    fun largestRectangleArea(heights: IntArray): Int {
+        val stack = Stack<Int>()
+        stack.push(-1)
+
+        var maxArea = 0
+        for (i in heights.indices) {
+            while (stack.peek() != -1 && heights[i] <= heights[stack.peek()]) {
+                maxArea = maxOf(maxArea, heights[stack.pop()] * (i - stack.peek() - 1))
+            }
+
+            stack.push(i)
+        }
+
+        while (stack.peek() != -1) {
+            maxArea = maxOf(maxArea, heights[stack.pop()] * (heights.size - stack.peek() - 1))
+        }
+
+        return maxArea
+    }
+}
--- src/site/lvkun/leetcode/largest_rectangle_in_histogram/SolutionTest.kt
+++ src/site/lvkun/leetcode/largest_rectangle_in_histogram/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.largest_rectangle_in_histogram
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testLargestRectangleArea() {
+        val solution = Solution()
+        Assert.assertEquals(10, solution.largestRectangleArea(intArrayOf(2,1,5,6,2,3)))
+    }
+}
