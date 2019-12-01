f26822fce8f2c69dfd019effc2849f68c868759d
Bruce Lv <bruce@duolingo.com>
2019-12-01 20:03:58 +0800

    Solve best_time_to_buy_and_sell_stock_with_cooldown

--- src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock_with_cooldown/Solution.kt
+++ src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock_with_cooldown/Solution.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.best_time_to_buy_and_sell_stock_with_cooldown
 
+/** TODO: Redo */
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+        var sold = 0
+        var rest = 0
+        var hold = Int.MIN_VALUE;
+        for (p in prices) {
+            val pre_sold = sold
+            sold = hold + p
+            hold = maxOf(hold, rest - p)
+            rest = maxOf(rest, pre_sold)
+        }
+
+        return maxOf(sold, rest)
+    }
+}
--- src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock_with_cooldown/SolutionTest.kt
+++ src/site/lvkun/leetcode/best_time_to_buy_and_sell_stock_with_cooldown/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.best_time_to_buy_and_sell_stock_with_cooldown
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMaxProfit() {
+        val solution = Solution()
+        Assert.assertEquals(3, solution.maxProfit(intArrayOf(1,2,3,0,2)))
+    }
+}
