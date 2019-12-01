3e7dcabfa06b732e6dca533e8633982a696ed8a5
Bruce Lv <bruce@duolingo.com>
2019-12-01 13:21:45 +0800

    Solve the_skyline_problem(need fix)

--- src/site/lvkun/leetcode/the_skyline_problem/Solution.kt
+++ src/site/lvkun/leetcode/the_skyline_problem/Solution.kt
@@ -1,2 +1,39 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.the_skyline_problem
 
+import java.util.*
+
+class Solution {
+    fun getSkyline(buildings: Array<IntArray>): List<List<Int>> {
+        val lines = PriorityQueue<Pair<Int, Int>>() { o1, o2 ->
+            return@PriorityQueue o1.first - o2.first
+        }
+
+        for (building in buildings) {
+            lines.add(Pair(building[0], -building[2]))
+            lines.add(Pair(building[1], building[2]))
+        }
+
+        val result = mutableListOf<MutableList<Int>>()
+        var last = mutableListOf(0, 0)
+        val pq = PriorityQueue<Int>() { i1, i2 ->
+            i2 - i1
+        }
+
+        while (lines.isNotEmpty()) {
+            val line = lines.poll()
+            if (line.second < 0) {
+                pq.add(-line.second)
+            } else {
+                pq.remove(line.second)
+            }
+
+            val maxHeight = pq.peek() ?: 0
+            if (last[1] != maxHeight) {
+                last = mutableListOf(line.first, maxHeight)
+                result.add(last)
+            }
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/the_skyline_problem/SolutionTest.kt
+++ src/site/lvkun/leetcode/the_skyline_problem/SolutionTest.kt
@@ -1,2 +1,36 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.the_skyline_problem
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testGetSkyline() {
+        val solution = Solution()
+        var result = solution.getSkyline(arrayOf(
+                intArrayOf(2, 9, 10),
+                intArrayOf(3, 7, 15),
+                intArrayOf(5, 12, 12),
+                intArrayOf(15, 20, 10),
+                intArrayOf(19, 24, 8)))
+        var expected = listOf(
+                listOf(2, 10),
+                listOf(3, 15),
+                listOf(7, 12),
+                listOf(12, 0),
+                listOf(15, 10),
+                listOf(20, 8),
+                listOf(24, 0))
+        Assert.assertEquals(expected, result)
+
+        result = solution.getSkyline(arrayOf(
+                intArrayOf(0, 2, 3),
+                intArrayOf(2, 5, 3)
+        ))
+        expected = listOf(
+                listOf(0, 3),
+                listOf(5, 0)
+        )
+        Assert.assertEquals(expected, result)
+    }
+}
