d5c1aad9f056c058f71ef6d620d020dcc2783a52
Bruce Lv <bruce@duolingo.com>
2019-12-01 20:37:33 +0800

    Solve work_break

--- src/site/lvkun/leetcode/word_break/Solution.kt
+++ src/site/lvkun/leetcode/word_break/Solution.kt
@@ -1,2 +1,20 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.word_break
 
+class Solution {
+    fun wordBreak(s: String, wordDict: List<String>): Boolean {
+        val set = wordDict.toSet()
+        val dp = BooleanArray(s.length + 1)
+        dp[0] = true
+
+        for (i in 1..s.length) {
+            for (j in 0 until i) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true
+                    break
+                }
+            }
+        }
+
+        return dp[s.length]
+    }
+}
--- src/site/lvkun/leetcode/word_break/SolutionTest.kt
+++ src/site/lvkun/leetcode/word_break/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.word_break
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testWordBreak() {
+        val solution = Solution()
+        Assert.assertTrue(solution.wordBreak("leetcode", listOf("leet", "code")))
+        Assert.assertTrue(solution.wordBreak("applepenapple", listOf("apple", "pen")))
+        Assert.assertFalse(solution.wordBreak("catsandog", listOf("cats", "dog", "sand", "and", "cat")))
+    }
+}
