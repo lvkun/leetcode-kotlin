8cf79b9ccb283d312bfc3137f28c8edc2102069c
Bruce Lv <bruce@duolingo.com>
2019-12-01 18:35:08 +0800

    Solve decode_ways

--- src/site/lvkun/leetcode/decode_ways/Solution.kt
+++ src/site/lvkun/leetcode/decode_ways/Solution.kt
@@ -1,2 +1,23 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.decode_ways
 
+class Solution {
+    fun numDecodings(s: String): Int {
+        if (s.isEmpty() || s[0] == '0') {
+            return 0
+        }
+
+        val dp = IntArray(s.length + 1)
+        dp[0] = 1
+        for (i in 1 .. dp.lastIndex) {
+            if (s[i - 1] != '0') {
+                dp[i] += dp[i - 1]
+            }
+
+            if (i >= 2 && (s[i - 2] == '1' || s[i - 2] == '2' && s[i - 1] <= '6')) {
+                dp[i] += dp[i - 2]
+            }
+        }
+
+        return dp.last()
+    }
+}
--- src/site/lvkun/leetcode/decode_ways/SolutionTest.kt
+++ src/site/lvkun/leetcode/decode_ways/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.decode_ways
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testNumDecodings() {
+        val solution = Solution()
+        Assert.assertEquals(2, solution.numDecodings("12"))
+        Assert.assertEquals(3, solution.numDecodings("226"))
+    }
+}
