0b226bf353b605e3959779ada9d31af5b1d85ad7
Bruce Lv <bruce@duolingo.com>
2019-12-09 22:29:41 +0800

    Solve container_with_most_water

--- src/site/lvkun/leetcode/container_with_most_water/Solution.kt
+++ src/site/lvkun/leetcode/container_with_most_water/Solution.kt
@@ -1,2 +1,26 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.container_with_most_water
 
+class Solution {
+    fun maxArea(height: IntArray): Int {
+        var left = 0
+        var right = height.lastIndex
+
+        var result = 0
+        while (left < right) {
+
+            val hLeft = height[left]
+            val hRight = height[right]
+
+            val area = minOf(hLeft, hRight) * (right - left)
+            result = maxOf(result, area)
+
+            if (hLeft <= hRight) {
+                left++
+            } else {
+                right--
+            }
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/container_with_most_water/SolutionTest.kt
+++ src/site/lvkun/leetcode/container_with_most_water/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.container_with_most_water
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMaxArea() {
+        val solution = Solution()
+        Assert.assertEquals(49, solution.maxArea(intArrayOf(1,8,6,2,5,4,8,3,7)))
+    }
+}
