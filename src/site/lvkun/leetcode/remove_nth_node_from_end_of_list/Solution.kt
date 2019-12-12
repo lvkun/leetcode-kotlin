b3a8d701ffdf365fc81136071dd2e2a1e35558a9
Bruce Lv <bruce@duolingo.com>
2019-12-12 23:48:02 +0800

    Solve remove_nth_node_from_end_of_list

--- src/site/lvkun/leetcode/remove_nth_node_from_end_of_list/Solution.kt
+++ src/site/lvkun/leetcode/remove_nth_node_from_end_of_list/Solution.kt
@@ -1,2 +1,32 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.remove_nth_node_from_end_of_list
 
+import site.lvkun.leetcode.common.ListNode
+
+class Solution {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        val dummy = ListNode(-1)
+        dummy.next = head
+
+        var fast = dummy.next
+
+        for (i in 0 until n) {
+            fast = fast?.next
+            if (fast == null) {
+                break
+            }
+        }
+
+        var cur = dummy.next
+        var prev: ListNode? = dummy
+        while (fast != null) {
+            fast = fast.next
+            cur = cur?.next
+            prev = prev?.next
+        }
+
+        prev?.next = cur?.next
+        cur?.next = null
+
+        return dummy.next
+    }
+}
--- src/site/lvkun/leetcode/remove_nth_node_from_end_of_list/SolutionTest.kt
+++ src/site/lvkun/leetcode/remove_nth_node_from_end_of_list/SolutionTest.kt
@@ -1,2 +1,17 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.remove_nth_node_from_end_of_list
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.ListNode
+
+class SolutionTest {
+    @Test
+    fun testRemoveNthFromEnd() {
+        val solution = Solution()
+        var result = solution.removeNthFromEnd(ListNode.fromString("1->2->3->4->5"), 2)
+        Assert.assertEquals(ListNode.fromString("1->2->3->5"), result)
+
+        result = solution.removeNthFromEnd(ListNode.fromString("1->2"), 2)
+        Assert.assertEquals(ListNode.fromString("2"), result)
+    }
+}
