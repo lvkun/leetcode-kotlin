d4d482b2aaa797d1897d6360d6a44186b304a09c
Bruce Lv <bruce@duolingo.com>
2019-12-15 23:59:29 +0800

    Solve next_permutation

--- src/site/lvkun/leetcode/next_permutation/Solution.kt
+++ src/site/lvkun/leetcode/next_permutation/Solution.kt
@@ -1,2 +1,38 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.next_permutation
 
+/** TODO: REDO */
+class Solution {
+    fun nextPermutation(nums: IntArray): Unit {
+        var i = nums.lastIndex - 1
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--
+        }
+
+        if (i >= 0) {
+            var j = nums.lastIndex
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--
+            }
+            swap(nums, i, j)
+        }
+        reverse(nums, i + 1)
+    }
+
+    private fun swap(nums: IntArray, i: Int, j:Int) {
+        val temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+    }
+
+    private fun reverse(nums: IntArray, i: Int) {
+        var start = i
+        var end = nums.lastIndex
+
+        while (start <= end) {
+            swap(nums, start, end)
+            start++
+            end--
+        }
+    }
+
+}
--- src/site/lvkun/leetcode/next_permutation/SolutionTest.kt
+++ src/site/lvkun/leetcode/next_permutation/SolutionTest.kt
@@ -1,2 +1,30 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.next_permutation
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testNextPermutation() {
+        val solution = Solution()
+//        var result = intArrayOf(1,2,3)
+//        solution.nextPermutation(result)
+//        Assert.assertArrayEquals(intArrayOf(1,3,2), result)
+//
+//        result = intArrayOf(3,2,1)
+//        solution.nextPermutation(result)
+//        Assert.assertArrayEquals(intArrayOf(1,2,3), result)
+//
+//        result = intArrayOf(1,1,5)
+//        solution.nextPermutation(result)
+//        Assert.assertArrayEquals(intArrayOf(1,5,1), result)
+//
+//        result = intArrayOf(1,2)
+//        solution.nextPermutation(result)
+//        Assert.assertArrayEquals(intArrayOf(2,1), result)
+
+        val result = intArrayOf(1,5,1)
+        solution.nextPermutation(result)
+        Assert.assertArrayEquals(intArrayOf(5,1,1), result)
+    }
+}
