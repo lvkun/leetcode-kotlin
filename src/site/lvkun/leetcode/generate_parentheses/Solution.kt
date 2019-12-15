c5736fe33dd2f8562d6ae5d06301d59689f705be
Bruce Lv <bruce@duolingo.com>
2019-12-15 11:46:44 +0800

    Solve generate_parentheses

--- src/site/lvkun/leetcode/generate_parentheses/Solution.kt
+++ src/site/lvkun/leetcode/generate_parentheses/Solution.kt
@@ -1,2 +1,24 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.generate_parentheses
 
+class Solution {
+    fun generateParenthesis(n: Int): List<String> {
+        val result = mutableListOf<String>()
+        backtrack(result, "", 0, 0, n)
+        return result
+    }
+
+    private fun backtrack(result: MutableList<String>, cur: String, open: Int, close: Int, max: Int) {
+        if (cur.length == max * 2) {
+            result.add(cur)
+            return
+        }
+
+        if (open < max) {
+            backtrack(result, "$cur(", open + 1, close, max)
+        }
+
+        if (close < open) {
+            backtrack(result, "$cur)", open, close + 1, max)
+        }
+    }
+}
--- src/site/lvkun/leetcode/generate_parentheses/SolutionTest.kt
+++ src/site/lvkun/leetcode/generate_parentheses/SolutionTest.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.generate_parentheses
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testGenerateParenthesis() {
+        val solution = Solution()
+        val result = solution.generateParenthesis(3)
+        val expected = listOf("((()))",
+                "(()())",
+                "(())()",
+                "()(())",
+                "()()()")
+        Assert.assertEquals(expected.toSet(), result.toSet())
+    }
+}
