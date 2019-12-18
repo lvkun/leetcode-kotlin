d7b293bf1a6dc11a230dbbf1e27e15bdda2cf7f0
Bruce Lv <bruce@duolingo.com>
2019-12-19 00:30:12 +0800

    Solve find_first_and_last_position_of_element_in_sorted_array

--- src/site/lvkun/leetcode/find_first_and_last_position_of_element_in_sorted_array/Solution.kt
+++ src/site/lvkun/leetcode/find_first_and_last_position_of_element_in_sorted_array/Solution.kt
@@ -1,2 +1,45 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.find_first_and_last_position_of_element_in_sorted_array
 
+class Solution {
+    private fun searchBoundary(nums: IntArray, target: Int, left: Boolean): Int {
+        var low = 0
+        var high = nums.size
+
+        while (low < high) {
+            val mid = (low + high) / 2
+            when {
+                nums[mid] == target -> {
+                    if (left) {
+                        high = mid
+                    } else {
+                        low = mid + 1
+                    }
+                }
+                nums[mid] < target -> {
+                    low = mid + 1
+                }
+                nums[mid] > target -> {
+                    high = mid
+                }
+            }
+        }
+
+        return low
+    }
+
+    fun searchRange(nums: IntArray, target: Int): IntArray {
+        val targetRange = intArrayOf(-1, -1)
+
+        val left = searchBoundary(nums, target, true)
+        if (left == nums.size || nums[left] != target) {
+            return targetRange
+        }
+
+        val right = searchBoundary(nums, target, false) - 1
+
+        targetRange[0] = left
+        targetRange[1] = right
+
+        return targetRange
+    }
+}
--- src/site/lvkun/leetcode/find_first_and_last_position_of_element_in_sorted_array/SolutionTest.kt
+++ src/site/lvkun/leetcode/find_first_and_last_position_of_element_in_sorted_array/SolutionTest.kt
@@ -1,2 +1,16 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.find_first_and_last_position_of_element_in_sorted_array
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testSearchRange() {
+        val solution = Solution()
+        var result = solution.searchRange(intArrayOf(5,7,7,8,8,10), 8)
+        Assert.assertArrayEquals(intArrayOf(3,4), result)
+
+        result = solution.searchRange(intArrayOf(5,7,7,8,8,10), 6)
+        Assert.assertArrayEquals(intArrayOf(-1,-1), result)
+    }
+}
