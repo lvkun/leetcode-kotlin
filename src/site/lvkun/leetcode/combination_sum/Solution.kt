683458ea5ace6df54cee332e6f7fec7d86cdf0fa
Bruce Lv <bruce@duolingo.com>
2019-12-25 00:31:08 +0800

    Solve combination_sum

--- src/site/lvkun/leetcode/combination_sum/Solution.kt
+++ src/site/lvkun/leetcode/combination_sum/Solution.kt
@@ -1,2 +1,36 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.combination_sum
 
+import java.util.*
+
+class Solution {
+    fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
+        return combinationSumInternal(candidates, 0, target)
+    }
+
+    fun combinationSumInternal(candidates: IntArray, index: Int, target: Int): LinkedList<LinkedList<Int>> {
+        val resultList = LinkedList<LinkedList<Int>>()
+
+        for (i in index until candidates.size) {
+            val candidate = candidates[i]
+            val nextTarget = target - candidate
+
+            when {
+                nextTarget == 0 -> {
+                    val result = LinkedList<Int>()
+                    result.add(candidate)
+                    resultList.add(result)
+                }
+                nextTarget > 0 -> {
+                    val partialResult = combinationSumInternal(candidates, i, nextTarget)
+
+                    for (result in partialResult) {
+                        result.addFirst(candidate)
+                        resultList.add(result)
+                    }
+                }
+            }
+        }
+
+        return resultList
+    }
+}
--- src/site/lvkun/leetcode/combination_sum/SolutionTest.kt
+++ src/site/lvkun/leetcode/combination_sum/SolutionTest.kt
@@ -1,2 +1,23 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.combination_sum
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testCombinationSum() {
+        val solution = Solution()
+        var result = solution.combinationSum(intArrayOf(2,3,6,7), 7)
+        Assert.assertEquals(listOf(
+                listOf(7),
+                listOf(2,2,3)
+        ).toSet(), result.toSet())
+
+        result = solution.combinationSum(intArrayOf(2,3,5), 8)
+        Assert.assertEquals(listOf(
+                listOf(2,2,2,2),
+                listOf(2,3,3),
+                listOf(3,5)
+        ).toSet(), result.toSet())
+    }
+}
