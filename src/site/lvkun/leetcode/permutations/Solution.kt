843a41a1b4369a740ae4d54f34733c4aeae3a1a9
Bruce Lv <bruce@duolingo.com>
2019-12-25 23:19:35 +0800

    Solve permutations

--- src/site/lvkun/leetcode/permutations/Solution.kt
+++ src/site/lvkun/leetcode/permutations/Solution.kt
@@ -1,2 +1,33 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.permutations
 
+import java.util.*
+
+class Solution {
+    fun permute(nums: IntArray): List<List<Int>> {
+        return permuteInternal(nums.toMutableSet())
+    }
+
+    fun permuteInternal(set: MutableSet<Int>): LinkedList<LinkedList<Int>> {
+        val resultList = LinkedList<LinkedList<Int>>()
+
+        if (set.size == 1) {
+            resultList.add(LinkedList(set))
+            return resultList
+        }
+
+        for (i in set.toList()) {
+            set.remove(i)
+
+            val partialResultList = permuteInternal(set)
+
+            for (result in partialResultList) {
+                result.addFirst(i)
+                resultList.add(result)
+            }
+
+            set.add(i)
+        }
+
+        return resultList
+    }
+}
--- src/site/lvkun/leetcode/permutations/SolutionTest.kt
+++ src/site/lvkun/leetcode/permutations/SolutionTest.kt
@@ -1,2 +1,21 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.permutations
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testPermute() {
+        val solution = Solution()
+        val result = solution.permute(intArrayOf(1,2,3))
+        val expected = listOf(
+                listOf(1,2,3),
+                listOf(1,3,2),
+                listOf(2,1,3),
+                listOf(2,3,1),
+                listOf(3,1,2),
+                listOf(3,2,1)
+        )
+        Assert.assertEquals(expected.toSet(), result.toSet())
+    }
+}
