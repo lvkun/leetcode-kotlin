0d72c8479887e3a03cbc65f1566c4d21647ce33e
Bruce Lv <bruce@duolingo.com>
2019-12-25 22:08:29 +0800

    Solve trapping_rain_water

--- src/site/lvkun/leetcode/trapping_rain_water/Solution.kt
+++ src/site/lvkun/leetcode/trapping_rain_water/Solution.kt
@@ -1,2 +1,33 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.trapping_rain_water
 
+class Solution {
+    fun trap(height: IntArray): Int {
+        val maxHeightLeft = IntArray(height.size)
+        val maxHeightRight = IntArray(height.size)
+
+        var maxHeight = 0
+        for (i in height.indices) {
+            val h = height[i]
+            maxHeight = maxOf(h, maxHeight)
+            maxHeightLeft[i] = maxHeight
+        }
+
+        maxHeight = 0
+        for (i in height.indices.reversed()) {
+            val h = height[i]
+            maxHeight = maxOf(h, maxHeight)
+            maxHeightRight[i] = maxHeight
+        }
+
+        var result = 0
+        for (i in height.indices) {
+            val h = height[i]
+            val hL = maxHeightLeft[i]
+            val hR = maxHeightRight[i]
+
+            result += minOf(hL, hR) - h
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/trapping_rain_water/SolutionTest.kt
+++ src/site/lvkun/leetcode/trapping_rain_water/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.trapping_rain_water
+
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testTrap() {
+        val solution = Solution()
+        Assert.assertEquals(6, solution.trap(intArrayOf(0,1,0,2,1,0,1,3,2,1,2,1)))
+    }
+}
 
