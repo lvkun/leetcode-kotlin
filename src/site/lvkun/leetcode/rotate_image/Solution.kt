336354253fe0125b11bf4c752c818a45c71bace1
Bruce Lv <bruce@duolingo.com>
2019-12-26 01:17:00 +0800

    Solve rotate_image

--- src/site/lvkun/leetcode/rotate_image/Solution.kt
+++ src/site/lvkun/leetcode/rotate_image/Solution.kt
@@ -1,2 +1,16 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.rotate_image
 
+class Solution {
+    fun rotate(matrix: Array<IntArray>): Unit {
+        val n = matrix.size
+        for (i in 0 until (n + 1) / 2) {
+            for (j in 0 until n / 2) {
+                val temp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = temp
+            }
+        }
+    }
+}
--- src/site/lvkun/leetcode/rotate_image/SolutionTest.kt
+++ src/site/lvkun/leetcode/rotate_image/SolutionTest.kt
@@ -1,2 +1,39 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.rotate_image
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.Utils
+
+class SolutionTest {
+    @Test
+    fun testRotate() {
+        val solution = Solution()
+        var result = Utils.intMatrixFromString("[\n" +
+                "  [1,2,3],\n" +
+                "  [4,5,6],\n" +
+                "  [7,8,9]\n" +
+                "]")
+//        solution.rotate(result)
+        var expected = Utils.intMatrixFromString("[\n" +
+                "  [7,4,1],\n" +
+                "  [8,5,2],\n" +
+                "  [9,6,3]\n" +
+                "]")
+//        Assert.assertArrayEquals(expected, result)
+
+        result = Utils.intMatrixFromString("[\n" +
+                "  [ 5, 1, 9,11],\n" +
+                "  [ 2, 4, 8,10],\n" +
+                "  [13, 3, 6, 7],\n" +
+                "  [15,14,12,16]\n" +
+                "]")
+        expected = Utils.intMatrixFromString("[\n" +
+                "  [15,13, 2, 5],\n" +
+                "  [14, 3, 4, 1],\n" +
+                "  [12, 6, 8, 9],\n" +
+                "  [16, 7,10,11]\n" +
+                "]")
+        solution.rotate(result)
+        Assert.assertArrayEquals(expected, result)
+    }
+}
