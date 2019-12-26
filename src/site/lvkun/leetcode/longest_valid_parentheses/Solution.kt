a43d28a4280dd708d1f21a9850b646fee3379888
Bruce Lv <bruce@duolingo.com>
2019-12-26 17:30:28 +0800

    Solve longest_valid_parentheses

--- src/site/lvkun/leetcode/longest_valid_parentheses/Solution.kt
+++ src/site/lvkun/leetcode/longest_valid_parentheses/Solution.kt
@@ -1,7 +1,46 @@
 package site.lvkun.leetcode.longest_valid_parentheses
 
+/** TODO: REDO */
 class Solution {
     fun longestValidParentheses(s: String): Int {
-        return 0
+        var left = 0
+        var right = 0
+        var maxLength = 0
+
+        for (i in 0 .. s.lastIndex) {
+            val c = s[i]
+            if (c == '(') {
+                left++
+            } else {
+                right++
+            }
+
+            if (left == right) {
+                maxLength = maxOf(maxLength, 2 * right)
+            } else if (right > left) {
+                left = 0
+                right = 0
+            }
+        }
+
+        left = 0
+        right = 0
+        for (i in s.lastIndex downTo 0) {
+            val c = s[i]
+            if (c == '(') {
+                left++
+            } else {
+                right++
+            }
+
+            if (left == right) {
+                maxLength = maxOf(maxLength, 2 * right)
+            } else if (left > right) {
+                left = 0
+                right = 0
+            }
+        }
+
+        return maxLength
     }
 }
