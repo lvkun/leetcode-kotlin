c74e92020fc7bec66e5618f3583938fc63ba003f
Bruce Lv <bruce@duolingo.com>
2019-12-26 23:06:54 +0800

    Solve subsets

--- src/site/lvkun/leetcode/subsets/Solution.kt
+++ src/site/lvkun/leetcode/subsets/Solution.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.subsets
 
+import java.util.*
+
+class Solution {
+    fun subsets(nums: IntArray): List<List<Int>> {
+        return subsets(nums, 0)
+    }
+
+    fun subsets(nums: IntArray, i: Int): LinkedList<LinkedList<Int>> {
+        val resultList = LinkedList<LinkedList<Int>>()
+        if (i == nums.size) {
+            resultList.add(LinkedList())
+            return resultList
+        }
+
+        val partialResult = subsets(nums, i + 1)
+        resultList.addAll(partialResult)
+
+        for (result in partialResult) {
+            val newResult = LinkedList(result)
+            newResult.addFirst(nums[i])
+            resultList.add(newResult)
+        }
+
+        return resultList
+    }
+}
--- src/site/lvkun/leetcode/subsets/SolutionTest.kt
+++ src/site/lvkun/leetcode/subsets/SolutionTest.kt
@@ -1,2 +1,24 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.subsets
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testSubsets() {
+        val solution = Solution()
+        val result = solution.subsets(intArrayOf(1,2,3))
+        val expected = listOf(
+                listOf(3),
+                listOf(1),
+                listOf(2),
+                listOf(1,2,3),
+                listOf(1,3),
+                listOf(2,3),
+                listOf(1,2),
+                listOf()
+        )
+
+        Assert.assertEquals(expected.toSet(), result.toSet())
+    }
+}
