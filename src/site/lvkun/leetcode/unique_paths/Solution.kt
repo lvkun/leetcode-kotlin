671eae85ad640b7f23bbd7e8b1a70e9110a97cdc
Bruce Lv <bruce@duolingo.com>
2019-12-26 21:23:40 +0800

    Solve unique_paths

--- src/site/lvkun/leetcode/unique_paths/Solution.kt
+++ src/site/lvkun/leetcode/unique_paths/Solution.kt
@@ -1,2 +1,27 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.unique_paths
 
+import java.math.BigInteger
+
+class Solution {
+    fun uniquePaths(m: Int, n: Int): Int {
+        return combination(m + n - 2, m - 1).toInt()
+    }
+
+    fun combination(n: Int, k: Int): BigInteger {
+        return factorial(n) / (factorial(n - k) * factorial(k))
+    }
+
+    fun factorial(n: Int): BigInteger {
+        var result = BigInteger.valueOf(1)
+
+        if (n == 1) {
+            return result
+        }
+
+        for (i in 1..n) {
+            result *= BigInteger.valueOf(i.toLong())
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/unique_paths/SolutionTest.kt
+++ src/site/lvkun/leetcode/unique_paths/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.unique_paths
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testUniquePaths() {
+        val solution = Solution()
+        Assert.assertEquals(3, solution.uniquePaths(3, 2))
+        Assert.assertEquals(28, solution.uniquePaths(7, 3))
+    }
+}
