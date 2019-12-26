0647594f522d8023f1dd6088289204b98516fd73
Bruce Lv <bruce@duolingo.com>
2019-12-26 20:47:51 +0800

    Solve jump_game

--- src/site/lvkun/leetcode/jump_game/Solution.kt
+++ src/site/lvkun/leetcode/jump_game/Solution.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.jump_game
 
+class Solution {
+    fun canJump(nums: IntArray): Boolean {
+        val result = BooleanArray(nums.size)
+        result[nums.lastIndex] = true
+
+        for (i in nums.lastIndex - 1 downTo 0) {
+            val n = nums[i]
+            if (n > 0) {
+                for (j in 1..n) {
+                    if (result[i + j]) {
+                        result[i] = true
+                        break
+                    }
+                }
+            }
+        }
+
+        return result[0]
+    }
+}
--- src/site/lvkun/leetcode/jump_game/SolutionTest.kt
+++ src/site/lvkun/leetcode/jump_game/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.jump_game
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testCanJump() {
+        val solution = Solution()
+        Assert.assertTrue(solution.canJump(intArrayOf(2,3,1,1,4)))
+        Assert.assertFalse(solution.canJump(intArrayOf(3,2,1,0,4)))
+    }
+}
