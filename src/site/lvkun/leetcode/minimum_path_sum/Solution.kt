199694b0e1634f1d0f14353264cf85c2b9f0298f
Bruce Lv <bruce@duolingo.com>
2019-12-26 22:45:13 +0800

    Solve minimum_path_sum

--- src/site/lvkun/leetcode/minimum_path_sum/Solution.kt
+++ src/site/lvkun/leetcode/minimum_path_sum/Solution.kt
@@ -1,2 +1,24 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.minimum_path_sum
 
+class Solution {
+    fun minPathSum(grid: Array<IntArray>): Int {
+        val m = grid.size
+        val n = grid[0].size
+
+        for (i in 1 until m) {
+            grid[i][0] += grid[i - 1][0]
+        }
+
+        for (j in 1 until n) {
+            grid[0][j] += grid[0][j - 1]
+        }
+
+        for (i in 1 until m) {
+            for (j in 1 until n) {
+                grid[i][j] += minOf(grid[i - 1][j], grid[i][j -1])
+            }
+        }
+
+        return grid[m - 1][n - 1]
+    }
+}
--- src/site/lvkun/leetcode/minimum_path_sum/SolutionTest.kt
+++ src/site/lvkun/leetcode/minimum_path_sum/SolutionTest.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.minimum_path_sum
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.Utils
+
+class SolutionTest {
+    @Test
+    fun testMinPathSum() {
+        val solution = Solution()
+        val grid = Utils.intMatrixFromString("[\n" +
+                "  [1,3,1],\n" +
+                "  [1,5,1],\n" +
+                "  [4,2,1]\n" +
+                "]")
+        Assert.assertEquals(7, solution.minPathSum(grid))
+    }
+}
