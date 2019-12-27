55dba8178f44d3d3665e28cc1b70eb379fa09395
Bruce Lv <bruce@duolingo.com>
2019-12-27 21:43:55 +0800

    Solve validate_binary_search_tree

--- src/site/lvkun/leetcode/validate_binary_search_tree/Solution.kt
+++ src/site/lvkun/leetcode/validate_binary_search_tree/Solution.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.validate_binary_search_tree
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun isValidBST(root: TreeNode?): Boolean {
+        return isValidBST(root, LongRange(Long.MIN_VALUE, Long.MAX_VALUE))
+    }
+
+    fun isValidBST(node: TreeNode?, range: LongRange): Boolean {
+        if (node == null) {
+            return true
+        }
+
+        if (!range.contains(node.`val`)) {
+            return false
+        }
+
+        return isValidBST(node.left, LongRange(range.first, node.`val`.toLong() - 1)) &&
+                isValidBST(node.right, LongRange(node.`val`.toLong() + 1, range.last))
+    }
+}
--- src/site/lvkun/leetcode/validate_binary_search_tree/SolutionTest.kt
+++ src/site/lvkun/leetcode/validate_binary_search_tree/SolutionTest.kt
@@ -1,2 +1,20 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.validate_binary_search_tree
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testIsValidBST() {
+        val solution = Solution()
+        Assert.assertTrue(solution.isValidBST(TreeNode(2,
+                TreeNode(1),
+                TreeNode(3))))
+        Assert.assertFalse(solution.isValidBST(TreeNode(5,
+                TreeNode(1),
+                TreeNode(4,
+                        TreeNode(3),
+                        TreeNode(6)))))
+    }
+}
