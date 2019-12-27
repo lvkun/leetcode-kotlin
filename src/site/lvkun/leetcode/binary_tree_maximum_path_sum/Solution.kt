291a05f012aec6af07851aa238691c5fea92cf5f
Bruce Lv <bruce@duolingo.com>
2019-12-27 23:57:24 +0800

    Solve binary_tree_maximum_path_sum

--- src/site/lvkun/leetcode/binary_tree_maximum_path_sum/Solution.kt
+++ src/site/lvkun/leetcode/binary_tree_maximum_path_sum/Solution.kt
@@ -1,2 +1,26 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_tree_maximum_path_sum
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    private var maxSum = Int.MIN_VALUE
+
+    fun maxPathSum(root: TreeNode?): Int {
+        maxSum = Int.MIN_VALUE
+        maxPathSumInternal(root)
+        return maxSum
+    }
+
+    fun maxPathSumInternal(node: TreeNode?): Int {
+        if (node == null) {
+            return 0
+        }
+
+        val leftSum = maxOf(maxPathSumInternal(node.left),0)
+        val rightSum = maxOf(maxPathSumInternal(node.right), 0)
+
+        maxSum = maxOf(maxSum, node.`val` + leftSum + rightSum)
+
+        return node.`val` + maxOf(leftSum, rightSum)
+    }
+}
--- src/site/lvkun/leetcode/binary_tree_maximum_path_sum/SolutionTest.kt
+++ src/site/lvkun/leetcode/binary_tree_maximum_path_sum/SolutionTest.kt
@@ -1,2 +1,20 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_tree_maximum_path_sum
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testMaxPathSum() {
+        val solution = Solution()
+        Assert.assertEquals(6, solution.maxPathSum(TreeNode(1,
+                TreeNode(2),
+                TreeNode(3))))
+        Assert.assertEquals(42, solution.maxPathSum(TreeNode(-10,
+                TreeNode(9),
+                TreeNode(20,
+                        TreeNode(15),
+                        TreeNode(7)))))
+    }
+}
