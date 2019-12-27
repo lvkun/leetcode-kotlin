2979aacec7da08dbac0b97b65c8292f31b5f6941
Bruce Lv <bruce@duolingo.com>
2019-12-27 23:04:57 +0800

    Solve flatten_binary_tree_to_linked_list

--- src/site/lvkun/leetcode/flatten_binary_tree_to_linked_list/Solution.kt
+++ src/site/lvkun/leetcode/flatten_binary_tree_to_linked_list/Solution.kt
@@ -1,2 +1,35 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.flatten_binary_tree_to_linked_list
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun flatten(root: TreeNode?): Unit {
+        if (root == null) {
+            return
+        }
+
+        flatterInternal(root)
+    }
+
+    fun flatterInternal(root: TreeNode): Pair<TreeNode, TreeNode> {
+        val left = root.left
+        val right = root.right
+
+        var tail = root
+        if (left != null) {
+            val leftPair = flatterInternal(left)
+            tail.left = null
+            tail.right = leftPair.first
+            tail = leftPair.second
+        }
+
+        if (right != null) {
+            val rightPair = flatterInternal(right)
+            tail.left = null
+            tail.right = rightPair.first
+            tail = rightPair.second
+        }
+
+        return Pair(root, tail)
+    }
+}
--- src/site/lvkun/leetcode/flatten_binary_tree_to_linked_list/SolutionTest.kt
+++ src/site/lvkun/leetcode/flatten_binary_tree_to_linked_list/SolutionTest.kt
@@ -1,2 +1,32 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.flatten_binary_tree_to_linked_list
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testFlatten() {
+        val solution = Solution()
+        val result = TreeNode(1,
+                TreeNode(2,
+                        TreeNode(3),
+                        TreeNode(4)),
+                TreeNode(5,
+                        null,
+                        TreeNode(6)))
+        solution.flatten(result)
+        val expected = TreeNode(1,
+                null,
+                TreeNode(2,
+                        null,
+                        TreeNode(3,
+                                null,
+                                TreeNode(4,
+                                        null,
+                                        TreeNode(5,
+                                                null,
+                                                TreeNode(6))))))
+        Assert.assertEquals(expected, result)
+    }
+}
