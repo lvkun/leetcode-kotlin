396fa1566003975ce440834a5e91e98308fcc3ed
Bruce Lv <bruce@duolingo.com>
2019-12-27 20:58:11 +0800

    Solve unique_binary_search_trees

--- src/site/lvkun/leetcode/unique_binary_search_trees/Solution.kt
+++ src/site/lvkun/leetcode/unique_binary_search_trees/Solution.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.unique_binary_search_trees
 
+class Solution {
+    fun numTrees(n: Int): Int {
+        val G = IntArray(n + 1)
+
+        G[0] = 1
+        G[1] = 1
+
+        for (i in 2..n) {
+            for (j in 1..i) {
+                G[i] += G[j - 1] * G[i - j]
+            }
+        }
+
+        return G[n]
+    }
+}
--- src/site/lvkun/leetcode/unique_binary_search_trees/SolutionTest.kt
+++ src/site/lvkun/leetcode/unique_binary_search_trees/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.unique_binary_search_trees
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testNumTrees() {
+        val solution = Solution()
+        Assert.assertEquals(5, solution.numTrees(3))
+    }
+}
