f12e6788e739e95ac468b06a097087784985c434
Bruce Lv <bruce@duolingo.com>
2019-12-27 14:07:32 +0800

    Solve binary_tree_inorder_traversal

--- src/site/lvkun/leetcode/binary_tree_inorder_traversal/Solution.kt
+++ src/site/lvkun/leetcode/binary_tree_inorder_traversal/Solution.kt
@@ -1,2 +1,32 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_tree_inorder_traversal
 
+import site.lvkun.leetcode.common.TreeNode
+import java.util.*
+
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        if (root == null) {
+            return emptyList()
+        }
+
+        val stack = Stack<TreeNode>()
+        val result = mutableListOf<Int>()
+
+        var cur = root
+
+        while (cur != null || stack.isNotEmpty()) {
+
+            while (cur != null) {
+                stack.push(cur)
+                cur = cur.left
+            }
+
+            cur = stack.pop()
+            result.add(cur.`val`)
+
+            cur = cur.right
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/binary_tree_inorder_traversal/SolutionTest.kt
+++ src/site/lvkun/leetcode/binary_tree_inorder_traversal/SolutionTest.kt
@@ -1,2 +1,27 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_tree_inorder_traversal
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testInorderTraversal() {
+        val solution = Solution()
+        var root = TreeNode(1,
+                null,
+                TreeNode(2,
+                        TreeNode(3),
+                        null))
+//        var result = solution.inorderTraversal(root)
+//        Assert.assertEquals(listOf(1, 3, 2), result)
+
+        root = TreeNode(2,
+                TreeNode(3,
+                        TreeNode(1),
+                        null),
+                null)
+        var result = solution.inorderTraversal(root)
+        Assert.assertEquals(listOf(1, 3, 2), result)
+    }
+}
