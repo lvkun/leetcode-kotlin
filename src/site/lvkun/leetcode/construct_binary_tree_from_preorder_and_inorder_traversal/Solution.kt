027c339abbc1c65ba4bcd78b44e95d9cae4d000f
Bruce Lv <bruce@duolingo.com>
2019-12-28 00:53:04 +0800

    Solve construct_binary_tree_from_preorder_and_inorder_traversal

--- src/site/lvkun/leetcode/construct_binary_tree_from_preorder_and_inorder_traversal/Solution.kt
+++ src/site/lvkun/leetcode/construct_binary_tree_from_preorder_and_inorder_traversal/Solution.kt
@@ -1,2 +1,40 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.construct_binary_tree_from_preorder_and_inorder_traversal
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun buildTree(preorder: IntArray, inorder: IntArray): TreeNode? {
+        return buildTree(preorder, IntRange(0, preorder.lastIndex),
+                inorder, IntRange(0, inorder.lastIndex))
+    }
+
+    fun IntArray.indexOf(v: Int, range: IntRange): Int {
+        for (i in range) {
+            if (this[i] == v) {
+                return i
+            }
+        }
+
+        return -1
+    }
+
+    fun buildTree(preorder: IntArray, preRange: IntRange, inorder: IntArray, inRange: IntRange): TreeNode? {
+        if (preRange.isEmpty()) {
+            return null
+        }
+
+        val value = preorder[preRange.first]
+
+        val inIndex = inorder.indexOf(value, inRange)
+        val leftLength = inIndex - inRange.first
+
+        val node = TreeNode(value)
+
+        node.left = buildTree(preorder, IntRange(preRange.first + 1, preRange.first + leftLength),
+                inorder, IntRange(inRange.first, inIndex - 1))
+        node.right = buildTree(preorder, IntRange(preRange.first + 1 + leftLength, preRange.last),
+                inorder, IntRange(inIndex + 1, inRange.last))
+
+        return node
+    }
+}
--- src/site/lvkun/leetcode/construct_binary_tree_from_preorder_and_inorder_traversal/SolutionTest.kt
+++ src/site/lvkun/leetcode/construct_binary_tree_from_preorder_and_inorder_traversal/SolutionTest.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.construct_binary_tree_from_preorder_and_inorder_traversal
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testBuildTree() {
+        val solution = Solution()
+        val result = solution.buildTree(intArrayOf(3,9,20,15,7), intArrayOf(9,3,15,20,7))
+        val expected = TreeNode(3,
+                TreeNode(9),
+                TreeNode(20,
+                        TreeNode(15),
+                        TreeNode(7)))
+        Assert.assertEquals(expected, result)
+    }
+}
