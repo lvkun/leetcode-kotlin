e1cc0db182e44adbe1d050ae597b97caed7eaf95
Bruce Lv <bruce@duolingo.com>
2019-12-27 22:34:05 +0800

    Solve binary_tree_level_order_traversal

--- src/site/lvkun/leetcode/binary_tree_level_order_traversal/Solution.kt
+++ src/site/lvkun/leetcode/binary_tree_level_order_traversal/Solution.kt
@@ -1,2 +1,34 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_tree_level_order_traversal
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun levelOrder(root: TreeNode?): List<List<Int>> {
+        val resultList = mutableListOf<List<Int>>()
+
+        var queue = mutableListOf<TreeNode>()
+        queue.add(root!!)
+
+        while (queue.isNotEmpty()) {
+            val nextLevel = mutableListOf<TreeNode>()
+            val result = mutableListOf<Int>()
+            for (node in queue) {
+                result.add(node.`val`)
+
+                val left = node.left
+                val right = node.right
+                if (left != null) {
+                    nextLevel.add(left)
+                }
+                if (right != null) {
+                    nextLevel.add(right)
+                }
+            }
+
+            queue = nextLevel
+            resultList.add(result)
+        }
+
+        return resultList
+    }
+}
--- src/site/lvkun/leetcode/binary_tree_level_order_traversal/SolutionTest.kt
+++ src/site/lvkun/leetcode/binary_tree_level_order_traversal/SolutionTest.kt
@@ -1,2 +1,23 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_tree_level_order_traversal
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testLevelOrder() {
+        val solution = Solution()
+        val result = solution.levelOrder(TreeNode(3,
+                TreeNode(9),
+                TreeNode(20,
+                        TreeNode(15),
+                        TreeNode(7))))
+        val expected = listOf(
+                listOf(3),
+                listOf(9,20),
+                listOf(15,7)
+        )
+        Assert.assertEquals(expected, result)
+    }
+}
