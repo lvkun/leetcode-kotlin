83c2ecd1d1f96804c301a02affbf9344bf7cc009
Bruce Lv <bruce@duolingo.com>
2019-12-31 00:34:02 +0800

    Solve implement_trie_prefix_tree

--- src/site/lvkun/leetcode/implement_trie_prefix_tree/Solution.kt
+++ src/site/lvkun/leetcode/implement_trie_prefix_tree/Solution.kt
@@ -1,2 +1,66 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.implement_trie_prefix_tree
 
+class Trie() {
+
+    /** Initialize your data structure here. */
+    val root = Node()
+
+    /** Inserts a word into the trie. */
+    fun insert(word: String) {
+        var node = root
+        for (c in word) {
+            if (!node.contains(c)) {
+                node.addNode(c)
+            }
+            node = node.getNode(c)!!
+        }
+        node.isEnd = true
+    }
+
+    /** Returns if the word is in the trie. */
+    fun search(word: String): Boolean {
+        var node = root
+        for (c in word) {
+            if (!node.contains(c)) {
+                return false
+            }
+            node = node.getNode(c)!!
+        }
+
+        return node.isEnd
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    fun startsWith(prefix: String): Boolean {
+        var node = root
+        for (c in prefix) {
+            if (!node.contains(c)) {
+                return false
+            }
+            node = node.getNode(c)!!
+        }
+
+        return true
+    }
+
+    class Node {
+        private val children = Array<Node?>(26) {_ -> null}
+        var isEnd = false
+
+        private fun Char.toIndex(): Int {
+            return this - 'a'
+        }
+
+        fun addNode(c: Char) {
+            children[c.toIndex()] = Node()
+        }
+
+        fun getNode(c: Char): Node? {
+            return children[c.toIndex()]
+        }
+
+        fun contains(c: Char): Boolean {
+            return children[c.toIndex()] != null
+        }
+    }
+}
--- src/site/lvkun/leetcode/implement_trie_prefix_tree/SolutionTest.kt
+++ src/site/lvkun/leetcode/implement_trie_prefix_tree/SolutionTest.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.implement_trie_prefix_tree
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun test() {
+        val trie = Trie()
+
+        trie.insert("apple")
+        Assert.assertTrue(trie.search("apple"))
+        Assert.assertFalse(trie.search("app"))
+        Assert.assertTrue(trie.startsWith("app"))
+        trie.insert("app")
+        Assert.assertTrue(trie.search("app"))
+    }
+}
