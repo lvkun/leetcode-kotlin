96e0820063ffd967be8cea483935ca08aa4d2e0e
Bruce Lv <bruce@duolingo.com>
2019-12-30 23:12:30 +0800

    Solve maximum_product_subarray

--- src/site/lvkun/leetcode/maximum_product_subarray/Solution.kt
+++ src/site/lvkun/leetcode/maximum_product_subarray/Solution.kt
@@ -1,2 +1,25 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.maximum_product_subarray
 
+class Solution {
+    fun maxProduct(nums: IntArray): Int {
+        var iMax = 1
+        var iMin = 1
+        var rMax = Int.MIN_VALUE
+
+        for (n in nums) {
+            if (n < 0) {
+                val temp = iMax
+                iMax = iMin
+                iMin = temp
+            }
+
+            iMax = maxOf(iMax * n, n)
+            iMin = minOf(iMin * n, n)
+
+            rMax = maxOf(iMax, rMax)
+        }
+
+
+        return rMax
+    }
+}
--- src/site/lvkun/leetcode/maximum_product_subarray/SolutionTest.kt
+++ src/site/lvkun/leetcode/maximum_product_subarray/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.maximum_product_subarray
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMaxProduct() {
+        val solution = Solution()
+        Assert.assertEquals(6, solution.maxProduct(intArrayOf(2,3,-2,4)))
+        Assert.assertEquals(0, solution.maxProduct(intArrayOf(-2,0,-1)))
+    }
+}
