b19611dbb9db55df16de68ac1d90c775f15ad0b8
Bruce Lv <bruce@duolingo.com>
2020-01-01 00:44:08 +0800

    Solve maximal_square

--- src/site/lvkun/leetcode/maximal_square/Solution.kt
+++ src/site/lvkun/leetcode/maximal_square/Solution.kt
@@ -1,2 +1,60 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.maximal_square
 
+import kotlin.math.min
+
+class Solution {
+    fun maximalSquare(matrix: Array<CharArray>): Int {
+        val m = matrix.size
+        if (m == 0) {
+            return 0
+        }
+
+        val n = matrix[0].size
+
+        val dp = Array(m) {
+            Array(n) {
+                Pair(0, 0)
+            }
+        }
+
+        val length = Array(m) {
+            IntArray(n)
+        }
+
+        var maxArea = 0
+        if (matrix[0][0] == '1') {
+            dp[0][0] = Pair(1, 1)
+            length[0][0] = 1
+            maxArea = 1
+        }
+
+        for (row in 1 until m) {
+            if (matrix[row][0] == '1') {
+                dp[row][0] = Pair(1, 1 + dp[row - 1][0].second)
+                length[row][0] = 1
+                maxArea = 1
+            }
+        }
+
+        for (col in 1 until n) {
+            if (matrix[0][col] == '1') {
+                dp[0][col] = Pair(1 + dp[0][col - 1].first, 1)
+                length[0][col] = 1
+                maxArea = 1
+            }
+        }
+
+
+        for (row in 1 until m) {
+            for (col in 1 until n) {
+                if (matrix[row][col] == '1') {
+                    dp[row][col] = Pair(1 + dp[row][col - 1].first, 1 + dp[row - 1][col].second)
+                    length[row][col] = maxOf(1, minOf(length[row - 1][col - 1] + 1, dp[row][col].first, dp[row][col].second))
+                    maxArea = maxOf(maxArea, length[row][col] * length[row][col])
+                }
+            }
+        }
+
+        return maxArea
+    }
+}
--- src/site/lvkun/leetcode/maximal_square/SolutionTest.kt
+++ src/site/lvkun/leetcode/maximal_square/SolutionTest.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.maximal_square
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.Utils
+
+class SolutionTest {
+    @Test
+    fun testMaximalSquare() {
+        val solution = Solution()
+        val result = solution.maximalSquare(Utils.charMatrixFromString(
+                "1 0 1 0 0\n" +
+                "1 0 1 1 1\n" +
+                "1 1 1 1 1\n" +
+                "1 0 0 1 0", " "))
+        Assert.assertEquals(4, result)
+    }
+}
