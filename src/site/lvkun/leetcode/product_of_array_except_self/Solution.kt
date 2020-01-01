2ec8d5bec88258d2e6bf4b30b07589d0ee8c11d4
Bruce Lv <bruce@duolingo.com>
2020-01-01 20:28:33 +0800

    Solve product_of_array_except_self

--- src/site/lvkun/leetcode/product_of_array_except_self/Solution.kt
+++ src/site/lvkun/leetcode/product_of_array_except_self/Solution.kt
@@ -1,2 +1,21 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.product_of_array_except_self
 
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val result = IntArray(nums.size) { 1 }
+
+        var cur = 1
+        for (i in 1..nums.lastIndex) {
+            cur *= nums[i - 1]
+            result[i] *= cur
+        }
+
+        cur = 1
+        for (i in nums.lastIndex - 1 downTo 0) {
+            cur *= nums[i + 1]
+            result[i] *= cur
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/product_of_array_except_self/SolutionTest.kt
+++ src/site/lvkun/leetcode/product_of_array_except_self/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.product_of_array_except_self
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testProductExceptSelf() {
+        val solution = Solution()
+        Assert.assertArrayEquals(intArrayOf(24,12,8,6),
+                solution.productExceptSelf(intArrayOf(1,2,3,4)))
+    }
+}
