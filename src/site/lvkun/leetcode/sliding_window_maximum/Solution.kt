03f89e84e80ee42e6a1d73cbc557695360f073eb
Bruce Lv <bruce@duolingo.com>
2020-01-01 23:13:23 +0800

    Solve sliding_window_maximum

--- src/site/lvkun/leetcode/sliding_window_maximum/Solution.kt
+++ src/site/lvkun/leetcode/sliding_window_maximum/Solution.kt
@@ -1,2 +1,32 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.sliding_window_maximum
 
+import java.util.*
+
+class Solution {
+    fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
+        if (k == 0) {
+            return nums
+        }
+
+        val maxQueue = ArrayDeque<Int>()
+
+        val result = IntArray(nums.size - k + 1)
+        for (i in nums.indices) {
+            if (maxQueue.isNotEmpty() && maxQueue.peekFirst() < i - k + 1) {
+                maxQueue.pollFirst()
+            }
+
+            while (maxQueue.isNotEmpty() && nums[maxQueue.peekLast()] < nums[i]) {
+                maxQueue.removeLast()
+            }
+
+            maxQueue.offerLast(i)
+
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[maxQueue.peekFirst()]
+            }
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/sliding_window_maximum/SolutionTest.kt
+++ src/site/lvkun/leetcode/sliding_window_maximum/SolutionTest.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.sliding_window_maximum
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMaxSlidingWindow() {
+        val solution = Solution()
+        Assert.assertArrayEquals(intArrayOf(3,3,5,5,6,7),
+                solution.maxSlidingWindow(intArrayOf(1,3,-1,-3,5,3,6,7), 3))
+        Assert.assertArrayEquals(intArrayOf(),
+                solution.maxSlidingWindow(intArrayOf(), 0))
+        Assert.assertArrayEquals(intArrayOf(1, -1),
+                solution.maxSlidingWindow(intArrayOf(1, -1), 1))
+        Assert.assertArrayEquals(intArrayOf(3,3,2,5),
+                solution.maxSlidingWindow(intArrayOf(1,3,1,2,0,5), 3))
+    }
+}
