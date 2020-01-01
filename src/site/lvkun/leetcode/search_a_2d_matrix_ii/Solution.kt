6d723b9a855efc0670522b6cdbb3f8b404552a1f
Bruce Lv <bruce@duolingo.com>
2020-01-02 00:39:11 +0800

    Solve search_a_2d_matrix_ii

--- src/site/lvkun/leetcode/search_a_2d_matrix_ii/Solution.kt
+++ src/site/lvkun/leetcode/search_a_2d_matrix_ii/Solution.kt
@@ -1,2 +1,47 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.search_a_2d_matrix_ii
 
+class Solution {
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+        val m = matrix.size
+        if (m == 0) {
+            return false
+        }
+
+        val n = matrix[0].size
+        if (n == 0) {
+            return false
+        }
+
+        return searchMatrix(matrix, target, Pair(0, 0), Pair(m - 1, n - 1))
+    }
+
+    fun Array<IntArray>.at(pos: Pair<Int, Int>): Int {
+        return this[pos.first][pos.second]
+    }
+
+    fun searchMatrix(matrix: Array<IntArray>, target: Int, leftTop: Pair<Int, Int>, rightBottom: Pair<Int, Int>): Boolean {
+        if (target < matrix.at(leftTop) || target > matrix.at(rightBottom)) {
+            return false
+        }
+
+        if (rightBottom.first - leftTop.first <= 1 && rightBottom.second - leftTop.second <= 1) {
+            for (i in leftTop.first..rightBottom.first) {
+                for (j in leftTop.second..rightBottom.second) {
+                    if (matrix[i][j] == target) {
+                        return true
+                    }
+                }
+            }
+
+            return false
+        }
+
+        val midRow = (leftTop.first + rightBottom.first) / 2
+        val midCol = (leftTop.second + rightBottom.second) / 2
+
+        return searchMatrix(matrix, target, leftTop, Pair(midRow, midCol)) ||
+                searchMatrix(matrix, target, Pair(leftTop.first, midCol), Pair(midRow, rightBottom.second)) ||
+                searchMatrix(matrix, target, Pair(midRow, leftTop.second), Pair(rightBottom.first, midCol)) ||
+                searchMatrix(matrix, target, Pair(midRow, midCol), rightBottom)
+    }
+}
--- src/site/lvkun/leetcode/search_a_2d_matrix_ii/SolutionTest.kt
+++ src/site/lvkun/leetcode/search_a_2d_matrix_ii/SolutionTest.kt
@@ -1,2 +1,21 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.search_a_2d_matrix_ii
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.Utils
+
+class SolutionTest {
+    @Test
+    fun testSearchMatrix() {
+        val solution = Solution()
+        val matrix = Utils.intMatrixFromString("[\n" +
+                "  [1,   4,  7, 11, 15],\n" +
+                "  [2,   5,  8, 12, 19],\n" +
+                "  [3,   6,  9, 16, 22],\n" +
+                "  [10, 13, 14, 17, 24],\n" +
+                "  [18, 21, 23, 26, 30]\n" +
+                "]\n")
+        Assert.assertTrue(solution.searchMatrix(matrix, 5))
+        Assert.assertFalse(solution.searchMatrix(matrix, 20))
+    }
+}
