96f3858a534411910e1c81cc773fa4ba29eb6295
Bruce Lv <bruce@duolingo.com>
2020-01-03 00:29:01 +0800

    Solve counting_bits

--- src/site/lvkun/leetcode/counting_bits/Solution.kt
+++ src/site/lvkun/leetcode/counting_bits/Solution.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.counting_bits
 
+class Solution {
+    fun countBits(num: Int): IntArray {
+        val result = IntArray(num + 1)
+
+        var mul = 1
+        for (i in 1..num) {
+            if (i < mul * 2) {
+                result[i] = 1 + result[i - mul]
+            } else {
+                mul *= 2
+                result[i] = 1
+            }
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/counting_bits/SolutionTest.kt
+++ src/site/lvkun/leetcode/counting_bits/SolutionTest.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.counting_bits
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testCountBits() {
+        val solution = Solution()
+        Assert.assertArrayEquals(intArrayOf(0,1,1),
+                solution.countBits(2))
+        Assert.assertArrayEquals(intArrayOf(0,1,1,2,1,2),
+                solution.countBits(5))
+        Assert.assertArrayEquals(intArrayOf(0),
+                solution.countBits(0))
+        Assert.assertArrayEquals(intArrayOf(0,1),
+                solution.countBits(1))
+    }
+}
