b7c52fefcc0f8de81d429d6347329e2ab4ae10ef
Bruce Lv <bruce@duolingo.com>
2020-01-02 20:01:22 +0800

    Solve perfect_squares

--- src/site/lvkun/leetcode/perfect_squares/Solution.kt
+++ src/site/lvkun/leetcode/perfect_squares/Solution.kt
@@ -1,2 +1,21 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.perfect_squares
 
+class Solution {
+    fun numSquares(n: Int): Int {
+        val dp = IntArray(n + 1)
+
+        for (i in 0..n) {
+            dp[i] = i
+        }
+
+        for (i in 1..n) {
+            var j = 1
+            while (i - j * j >= 0) {
+                dp[i] = minOf(dp[i], dp[i - j * j] + 1)
+                j++
+            }
+        }
+
+        return dp[n]
+    }
+}
--- src/site/lvkun/leetcode/perfect_squares/SolutionTest.kt
+++ src/site/lvkun/leetcode/perfect_squares/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.perfect_squares
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testNumSquares() {
+        val solution = Solution()
+        Assert.assertEquals(3, solution.numSquares(12))
+        Assert.assertEquals(2, solution.numSquares(13))
+    }
+}
