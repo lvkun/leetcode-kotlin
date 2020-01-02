9a2b14e1ddc0236c846c70374c49451ab0c7c72c
Bruce Lv <bruce@duolingo.com>
2020-01-02 22:21:06 +0800

    Solve longest_increasing_subsequence

--- src/site/lvkun/leetcode/longest_increasing_subsequence/Solution.kt
+++ src/site/lvkun/leetcode/longest_increasing_subsequence/Solution.kt
@@ -1,2 +1,27 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.longest_increasing_subsequence
 
+/** TODO: REDO */
+class Solution {
+    fun lengthOfLIS(nums: IntArray): Int {
+        if (nums.isEmpty()) {
+            return 0
+        }
+
+        val dp = IntArray(nums.size) { 1 }
+
+        var maxLength = 1
+        for (i in nums.indices) {
+            var curMax = 0
+            for (j in 0 until i) {
+                if (nums[i] > nums[j]) {
+                    curMax = maxOf(dp[j], curMax)
+                }
+            }
+
+            dp[i] = curMax + 1
+            maxLength = maxOf(maxLength, dp[i])
+        }
+
+        return maxLength
+    }
+}
--- src/site/lvkun/leetcode/longest_increasing_subsequence/SolutionTest.kt
+++ src/site/lvkun/leetcode/longest_increasing_subsequence/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.longest_increasing_subsequence
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testLengthOfLIS() {
+        val solution = Solution()
+        Assert.assertEquals(4, solution.lengthOfLIS(intArrayOf(10,9,2,5,3,7,101,18)))
+    }
+}
