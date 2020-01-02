28e41006235932b1cdee423a733934cc7968dbda
Bruce Lv <bruce@duolingo.com>
2020-01-03 00:48:47 +0800

    Solve top_k_frequent_elements

--- src/site/lvkun/leetcode/top_k_frequent_elements/Solution.kt
+++ src/site/lvkun/leetcode/top_k_frequent_elements/Solution.kt
@@ -1,2 +1,32 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.top_k_frequent_elements
 
+import java.util.*
+
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): List<Int> {
+        val map = mutableMapOf<Int, Int>()
+
+        for (n in nums) {
+            map[n] = map.getOrDefault(n, 0) + 1
+        }
+
+        val queue = PriorityQueue<Int>() {
+            i1, i2 -> map[i1]!! - map[i2]!!
+        }
+
+        for (n in map.keys) {
+            queue.offer(n)
+            if (queue.size > k) {
+                queue.poll()
+            }
+        }
+
+        val result = mutableListOf<Int>()
+
+        while (queue.isNotEmpty()) {
+            result.add(queue.poll())
+        }
+
+        return result.reversed()
+    }
+}
--- src/site/lvkun/leetcode/top_k_frequent_elements/SolutionTest.kt
+++ src/site/lvkun/leetcode/top_k_frequent_elements/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.top_k_frequent_elements
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testTopKFrequent() {
+        val solution = Solution()
+        Assert.assertEquals(listOf(1,2), solution.topKFrequent(intArrayOf(1,1,1,2,2,3), 2))
+        Assert.assertEquals(listOf(1), solution.topKFrequent(intArrayOf(1), 1))
+    }
+}
