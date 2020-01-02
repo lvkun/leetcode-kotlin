7ebe6ea86c2632ce02021fd6b430b8177d9643ad
Bruce Lv <bruce@duolingo.com>
2020-01-02 23:57:13 +0800

    Solve house_robber_iii

--- src/site/lvkun/leetcode/house_robber_iii/Solution.kt
+++ src/site/lvkun/leetcode/house_robber_iii/Solution.kt
@@ -1,2 +1,26 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.house_robber_iii
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun rob(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+
+        return rob(root, true)
+    }
+
+    fun rob(root: TreeNode?, include: Boolean): Int {
+        if (root == null) {
+            return 0
+        }
+
+        if (include) {
+            return maxOf(root.`val` + rob(root.left, false) + rob(root.right, false),
+                    rob(root.left, true) + rob(root.right, true))
+        }
+
+        return rob(root.left, true) + rob(root.right, true)
+    }
+}
--- src/site/lvkun/leetcode/house_robber_iii/SolutionTest.kt
+++ src/site/lvkun/leetcode/house_robber_iii/SolutionTest.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.house_robber_iii
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testRob() {
+        val solution = Solution()
+        Assert.assertEquals(7, solution.rob(
+                TreeNode(3,
+                        TreeNode(2,
+                                null,
+                                TreeNode(3)),
+                        TreeNode(3,
+                                null,
+                                TreeNode(1)))))
+        Assert.assertEquals(9, solution.rob(
+                TreeNode(3,
+                        TreeNode(4,
+                                TreeNode(1),
+                                TreeNode(3)),
+                        TreeNode(5,
+                                null,
+                                TreeNode(1)))))
+    }
+}
