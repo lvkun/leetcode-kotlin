1b88bb1e687cb3e6f22464ac2ddfdab028c2b689
Bruce Lv <bruce@duolingo.com>
2020-01-02 23:13:36 +0800

    Solve coin_change

--- src/site/lvkun/leetcode/coin_change/Solution.kt
+++ src/site/lvkun/leetcode/coin_change/Solution.kt
@@ -1,2 +1,27 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.coin_change
 
+class Solution {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+        val dp = IntArray(amount + 1)
+
+        for (i in 1..amount) {
+            var minChange = Int.MAX_VALUE
+            for (coin in coins) {
+                if (i >= coin) {
+                    minChange = minOf(dp[i - coin], minChange)
+                }
+            }
+            if (minChange != Int.MAX_VALUE) {
+                dp[i] = minChange + 1
+            } else {
+                dp[i] = Int.MAX_VALUE
+            }
+        }
+
+        if (dp[amount] == Int.MAX_VALUE) {
+            return -1
+        }
+
+        return dp[amount]
+    }
+}
--- src/site/lvkun/leetcode/coin_change/SolutionTest.kt
+++ src/site/lvkun/leetcode/coin_change/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.coin_change
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testCoinChange() {
+        val solution = Solution()
+        Assert.assertEquals(3, solution.coinChange(intArrayOf(1, 2, 5), 11))
+        Assert.assertEquals(-1, solution.coinChange(intArrayOf(2), 3))
+    }
+}
