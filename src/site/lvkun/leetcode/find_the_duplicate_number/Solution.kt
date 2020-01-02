93364ba956e636cc80cdd8a1d4cef3b5dab75a3e
Bruce Lv <bruce@duolingo.com>
2020-01-02 21:25:38 +0800

    Solve find_the_duplicate_number

--- src/site/lvkun/leetcode/find_the_duplicate_number/Solution.kt
+++ src/site/lvkun/leetcode/find_the_duplicate_number/Solution.kt
@@ -1,2 +1,27 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.find_the_duplicate_number
 
+/** TODO: REDO */
+class Solution {
+    fun findDuplicate(nums: IntArray): Int {
+        var slow = 0
+        var fast = 0
+
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+
+        while (slow != fast) {
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        }
+
+        var p1 = 0
+        var p2 = slow
+
+        while (p1 != p2) {
+            p1 = nums[p1]
+            p2 = nums[p2]
+        }
+
+        return p1
+    }
+}
--- src/site/lvkun/leetcode/find_the_duplicate_number/SolutionTest.kt
+++ src/site/lvkun/leetcode/find_the_duplicate_number/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.find_the_duplicate_number
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testFindDuplicate() {
+        val solution = Solution()
+        Assert.assertEquals(2, solution.findDuplicate(intArrayOf(1,3,4,2,2)))
+        Assert.assertEquals(3, solution.findDuplicate(intArrayOf(3,1,3,4,2)))
+    }
+}
