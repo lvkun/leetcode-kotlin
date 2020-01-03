d4cc57e562fd541532e00a4e3c4cc8456f9e1c44
Bruce Lv <bruce@duolingo.com>
2020-01-03 23:25:12 +0800

    Solve decode_string

--- src/site/lvkun/leetcode/decode_string/Solution.kt
+++ src/site/lvkun/leetcode/decode_string/Solution.kt
@@ -1,2 +1,41 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.decode_string
 
+import java.util.*
+
+class Solution {
+    fun decodeString(s: String): String {
+        var curNumber = mutableListOf<Char>()
+        var curString = mutableListOf<Char>()
+
+        val stack = Stack<List<Char>>()
+        for (c in s) {
+            when {
+                c == '[' -> {
+                    stack.push(curNumber)
+                    curNumber = mutableListOf()
+                }
+                c == ']' -> {
+                    val n = stack.pop().joinToString("").toInt()
+                    val newString = mutableListOf<Char>()
+                    if (stack.isNotEmpty() && stack.peek().first().isLetter()) {
+                        newString.addAll(stack.pop())
+                    }
+                    repeat(n) {
+                        newString.addAll(curString)
+                    }
+                    curString = newString
+                }
+                c.isDigit() -> {
+                    if (curString.isNotEmpty()) {
+                        stack.push(curString)
+                        curString = mutableListOf()
+                    }
+                    curNumber.add(c)
+                }
+                c.isLetter() -> curString.add(c)
+            }
+        }
+
+        return curString.joinToString("")
+    }
+}
--- src/site/lvkun/leetcode/decode_string/SolutionTest.kt
+++ src/site/lvkun/leetcode/decode_string/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.decode_string
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testDecodeString() {
+        val solution = Solution()
+        Assert.assertEquals("aaabcbc", solution.decodeString("3[a]2[bc]"))
+        Assert.assertEquals("accaccacc", solution.decodeString("3[a2[c]]"))
+        Assert.assertEquals("abcabccdcdcdef", solution.decodeString("2[abc]3[cd]ef"))
+    }
+}
