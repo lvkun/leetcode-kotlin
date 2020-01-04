7a6db461e512658cd4b53479c5ef4ca0b13aadeb
Bruce Lv <bruce@duolingo.com>
2020-01-04 20:46:22 +0800

    Solve evaluate_division

--- src/site/lvkun/leetcode/evaluate_division/Solution.kt
+++ src/site/lvkun/leetcode/evaluate_division/Solution.kt
@@ -1,2 +1,75 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.evaluate_division
 
+class Solution {
+    fun calcEquation(equations: List<List<String>>, values: DoubleArray, queries: List<List<String>>): DoubleArray {
+        val graph = Graph()
+
+        for (i in equations.indices) {
+            val equation = equations[i]
+            val value = values[i]
+
+            graph.addEdge(equation[0], equation[1], value)
+        }
+
+        val result = DoubleArray(queries.size)
+        for (i in queries.indices) {
+            val query = queries[i]
+            result[i] = graph.dfs(query[0], query[1])
+        }
+
+        return result
+    }
+
+    class Graph {
+        val nodeMap = mutableMapOf<String, Node>()
+
+        fun addEdge(from: String, to: String, value: Double) {
+            nodeMap.putIfAbsent(from, Node())
+            nodeMap.putIfAbsent(to, Node())
+
+            nodeMap[from]?.addEdge(to, value)
+            nodeMap[to]?.addEdge(from, 1 / value)
+        }
+
+        fun dfs(from: String, target: String): Double {
+            if (!nodeMap.containsKey(from) || !nodeMap.containsKey(target)) {
+                return -1.0
+            }
+
+            val visited = mutableMapOf<String, Boolean>()
+            return dfs(from, target, visited) ?: -1.0
+        }
+
+        fun dfs(from: String, target: String, visited: MutableMap<String, Boolean>): Double? {
+            if (visited.getOrDefault(from, false)) {
+                return null
+            }
+
+            val fromNode = nodeMap[from]
+            visited[from] = true
+
+            if (fromNode != null) {
+                if (target in fromNode.edgeMap.keys) {
+                    return fromNode.edgeMap[target]
+                }
+
+                for (entry in fromNode.edgeMap.entries) {
+                    val v = dfs(entry.key, target, visited)
+                    if (v != null) {
+                        return entry.value * v
+                    }
+                }
+            }
+
+            return null
+        }
+    }
+
+    class Node {
+        val edgeMap = mutableMapOf<String, Double>()
+
+        fun addEdge(node: String, value: Double) {
+            edgeMap[node] = value
+        }
+    }
+}
--- src/site/lvkun/leetcode/evaluate_division/SolutionTest.kt
+++ src/site/lvkun/leetcode/evaluate_division/SolutionTest.kt
@@ -1,2 +1,20 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.evaluate_division
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testCalcEquation() {
+        val solution = Solution()
+        val result = solution.calcEquation(
+                listOf(listOf("a", "b"), listOf("b", "c")),
+                doubleArrayOf(2.0, 3.0),
+                listOf(listOf("a", "c"),
+                        listOf("b", "a"),
+                        listOf("a", "e"),
+                        listOf("a", "a"),
+                        listOf("x", "x")))
+        Assert.assertArrayEquals(doubleArrayOf(6.0, 0.5, -1.0, 1.0, -1.0), result, 0.01)
+    }
+}
