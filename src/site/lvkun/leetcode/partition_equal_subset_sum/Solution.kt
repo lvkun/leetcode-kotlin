87c887b83191a5140de1283b4d97c6b87a5b68de
Bruce Lv <bruce@duolingo.com>
2020-01-04 23:47:30 +0800

    Solve partition_equal_subset_sum

--- src/site/lvkun/leetcode/partition_equal_subset_sum/Solution.kt
+++ src/site/lvkun/leetcode/partition_equal_subset_sum/Solution.kt
@@ -1,2 +1,35 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.partition_equal_subset_sum
 
+/** TODO: REDO */
+class Solution {
+    fun canPartition(nums: IntArray): Boolean {
+        val s = nums.sum()
+        if (s % 2 != 0) {
+            return false
+        }
+
+        val target = s / 2
+
+        val dp = Array<BooleanArray>(nums.size) {
+            BooleanArray(target + 1)
+        }
+
+        dp[0][0] = true
+        if (nums[0] < target) {
+            dp[0][nums[0]] = true
+        }
+
+        for (i in 1..nums.lastIndex) {
+            for (j in 0..target) {
+                if (nums[i] == j) {
+                    dp[i][j] = true
+                    continue
+                }
+
+                dp[i][j] = dp[i - 1][j] || (nums[i] < j) && dp[i - 1][j - nums[i]]
+            }
+        }
+
+        return dp[nums.lastIndex][target]
+    }
+}
--- src/site/lvkun/leetcode/partition_equal_subset_sum/SolutionTest.kt
+++ src/site/lvkun/leetcode/partition_equal_subset_sum/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.partition_equal_subset_sum
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testCanPartition() {
+        val solution = Solution()
+        Assert.assertTrue(solution.canPartition(intArrayOf(1, 5, 11, 5)))
+        Assert.assertFalse(solution.canPartition(intArrayOf(1, 2, 3, 5)))
+    }
+}
