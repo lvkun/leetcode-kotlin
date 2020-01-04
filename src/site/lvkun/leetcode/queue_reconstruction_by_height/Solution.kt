7f8d78c361bd53276d30c871ca217d86db120a7d
Bruce Lv <bruce@duolingo.com>
2020-01-04 22:05:36 +0800

    Solve queue_reconstruction_by_height

--- src/site/lvkun/leetcode/queue_reconstruction_by_height/Solution.kt
+++ src/site/lvkun/leetcode/queue_reconstruction_by_height/Solution.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.queue_reconstruction_by_height
 
+import java.util.*
+
+class Solution {
+    fun reconstructQueue(people: Array<IntArray>): Array<IntArray> {
+        Arrays.sort(people) { p1, p2 ->
+            if (p1[0] == p2[0]) {
+                p1[1] - p2[1]
+            } else {
+                p2[0] - p1[0]
+            }
+        }
+
+        val result = mutableListOf<IntArray>()
+        for (p in people) {
+            result.add(p[1], p)
+        }
+
+        return result.toTypedArray()
+    }
+}
--- src/site/lvkun/leetcode/queue_reconstruction_by_height/SolutionTest.kt
+++ src/site/lvkun/leetcode/queue_reconstruction_by_height/SolutionTest.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.queue_reconstruction_by_height
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    fun parse(data: String): Array<IntArray> =
+        data.replace("[", "")
+                .replace("]", "")
+                .split(",").windowed(2, 2, true).map { d ->
+                    intArrayOf(d[0].trim().toInt(), d[1].trim().toInt())
+                }.toTypedArray()
+
+    @Test
+    fun testReconstructQueue() {
+        val solution = Solution()
+        val people = parse("[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]")
+        val result = solution.reconstructQueue(people)
+        val expected = parse("[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]")
+        Assert.assertArrayEquals(expected, result)
+    }
+}
