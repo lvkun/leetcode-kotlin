5f006c2a9c87b11e21cc6337239565246fd13107
Bruce Lv <bruce@duolingo.com>
2020-01-05 19:54:00 +0800

    Solve path_sum_iii

--- src/site/lvkun/leetcode/path_sum_iii/Solution.kt
+++ src/site/lvkun/leetcode/path_sum_iii/Solution.kt
@@ -1,2 +1,25 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.path_sum_iii
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun pathSum(root: TreeNode?, sum: Int): Int {
+        if (root == null) {
+            return 0
+        }
+
+        return pathSum(root.left, sum) + pathSum(root.right, sum) + pathSumPartial(root, sum)
+    }
+
+    fun pathSumPartial(root: TreeNode?, partialSum: Int): Int {
+        if (root == null) {
+            return 0
+        }
+
+        val newPartialSum = partialSum - root.`val`
+
+        return if (newPartialSum == 0) {
+            1
+        } else { 0 } + pathSumPartial(root.left, newPartialSum) + pathSumPartial(root.right, newPartialSum)
+    }
+}
--- src/site/lvkun/leetcode/path_sum_iii/SolutionTest.kt
+++ src/site/lvkun/leetcode/path_sum_iii/SolutionTest.kt
@@ -1,2 +1,34 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.path_sum_iii
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testPathSum() {
+        val solution = Solution()
+        Assert.assertEquals(3, solution.pathSum(
+                TreeNode(10,
+                    TreeNode(5,
+                            TreeNode(3,
+                                    TreeNode(3),
+                                    TreeNode(-2)),
+                            TreeNode(2,
+                                    null,
+                                    TreeNode(1))),
+                    TreeNode(-3,
+                            null,
+                            TreeNode(11))), 8))
+        Assert.assertEquals(2, solution.pathSum(
+                TreeNode(1,
+                        null,
+                        TreeNode(2,
+                                null,
+                                TreeNode(3,
+                                        null,
+                                        TreeNode(4,
+                                                null,
+                                                TreeNode(5))))),3))
+    }
+}
