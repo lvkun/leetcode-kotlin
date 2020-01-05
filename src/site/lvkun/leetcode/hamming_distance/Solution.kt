bd5838a0adf5300c7a3ae41aaedd9561458ee6e9
Bruce Lv <bruce@duolingo.com>
2020-01-05 23:14:18 +0800

    Solve hamming_distance

--- src/site/lvkun/leetcode/hamming_distance/Solution.kt
+++ src/site/lvkun/leetcode/hamming_distance/Solution.kt
@@ -1,2 +1,9 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.hamming_distance
 
+class Solution {
+    fun hammingDistance(x: Int, y: Int): Int {
+        var r = x.xor(y)
+
+        return Integer.bitCount(r)
+    }
+}
--- src/site/lvkun/leetcode/hamming_distance/SolutionTest.kt
+++ src/site/lvkun/leetcode/hamming_distance/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.hamming_distance
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testHammingDistance() {
+        val solution = Solution()
+        Assert.assertEquals(2, solution.hammingDistance(1, 4))
+    }
+}
