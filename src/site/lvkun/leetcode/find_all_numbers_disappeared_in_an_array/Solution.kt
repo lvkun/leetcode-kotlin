59090dd0a5a4bd9cc01ecd2ce9c2f02540420b24
Bruce Lv <bruce@duolingo.com>
2020-01-05 22:54:36 +0800

    Solve find_all_numbers_disappeared_in_an_array

--- src/site/lvkun/leetcode/find_all_numbers_disappeared_in_an_array/Solution.kt
+++ src/site/lvkun/leetcode/find_all_numbers_disappeared_in_an_array/Solution.kt
@@ -1,2 +1,33 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.find_all_numbers_disappeared_in_an_array
 
+class Solution {
+    fun findDisappearedNumbers(nums: IntArray): List<Int> {
+
+        for (i in nums.indices) {
+            while (nums[i] != i + 1 && nums[i] != -1) {
+                if (nums[i] == nums[nums[i] - 1]) {
+                    nums[i] = -1
+                    break
+                }
+
+                swap(nums, i, nums[i] - 1)
+            }
+        }
+
+        val result = mutableListOf<Int>()
+
+        for (i in nums.indices) {
+            if (nums[i] == -1) {
+                result.add(i + 1)
+            }
+        }
+
+        return result
+    }
+
+    fun swap(nums: IntArray, i: Int, j: Int) {
+        val temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+    }
+}
--- src/site/lvkun/leetcode/find_all_numbers_disappeared_in_an_array/SolutionTest.kt
+++ src/site/lvkun/leetcode/find_all_numbers_disappeared_in_an_array/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.find_all_numbers_disappeared_in_an_array
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testFindDisappearedNumbers() {
+        val solution = Solution()
+        Assert.assertEquals(listOf(5,6),
+                solution.findDisappearedNumbers(intArrayOf(4,3,2,7,8,2,3,1)))
+    }
+}
