f9ce3b31e11fca030cd6499778097c8153ef1bc3
Bruce Lv <bruce@duolingo.com>
2020-01-06 22:24:43 +0800

    Solve diameter_of_binary_tree

--- src/site/lvkun/leetcode/diameter_of_binary_tree/Solution.kt
+++ src/site/lvkun/leetcode/diameter_of_binary_tree/Solution.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.diameter_of_binary_tree
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    var diameter = 0
+
+    fun diameterOfBinaryTree(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+        diameterOfBinaryTreeInternal(root)
+        return diameter
+    }
+
+    fun diameterOfBinaryTreeInternal(node: TreeNode?): Int {
+        if (node == null) {
+            return 0
+        }
+
+        val left = diameterOfBinaryTreeInternal(node.left)
+        val right = diameterOfBinaryTreeInternal(node.right)
+
+        diameter = maxOf(diameter, left + right)
+
+        return 1 + maxOf(left, right)
+    }
+}
--- src/site/lvkun/leetcode/diameter_of_binary_tree/SolutionTest.kt
+++ src/site/lvkun/leetcode/diameter_of_binary_tree/SolutionTest.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.diameter_of_binary_tree
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testDiameterOfBinaryTree() {
+        val solution = Solution()
+        Assert.assertEquals(3, solution.diameterOfBinaryTree(
+                TreeNode(1,
+                        TreeNode(2,
+                                TreeNode(4),
+                                TreeNode(5)),
+                        TreeNode(3))
+        ))
+    }
+}
