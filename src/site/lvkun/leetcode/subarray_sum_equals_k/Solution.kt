b7300ac7e5b6d018035b9ba26d5af49e1789bafb
Bruce Lv <bruce@duolingo.com>
2020-01-06 23:02:18 +0800

    Solve subarray_sum_equals_k

--- src/site/lvkun/leetcode/subarray_sum_equals_k/Solution.kt
+++ src/site/lvkun/leetcode/subarray_sum_equals_k/Solution.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.subarray_sum_equals_k
 
+/** TODO: REDO */
+class Solution {
+    fun subarraySum(nums: IntArray, k: Int): Int {
+        var count = 0
+
+        val map = mutableMapOf<Int, Int>()
+        map[0] = 1
+
+        var sum = 0
+        for (i in nums) {
+            sum += i
+            if (map.containsKey(sum - k)) {
+                count += map[sum - k]!!
+            }
+            map[sum] = 1 + map.getOrDefault(sum, 0)
+        }
+
+        return count
+    }
+}
--- src/site/lvkun/leetcode/subarray_sum_equals_k/SolutionTest.kt
+++ src/site/lvkun/leetcode/subarray_sum_equals_k/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.subarray_sum_equals_k
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testSubarraySum() {
+        val solution = Solution()
+        Assert.assertEquals(2, solution.subarraySum(intArrayOf(1,1,1), 2))
+    }
+}
