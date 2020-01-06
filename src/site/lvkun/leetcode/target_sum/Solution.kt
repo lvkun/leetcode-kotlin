c40bf8c58a72e35179c4c1f55635c8cf7ce39890
Bruce Lv <bruce@duolingo.com>
2020-01-06 09:52:33 +0800

    Solve target_sum

--- src/site/lvkun/leetcode/target_sum/Solution.kt
+++ src/site/lvkun/leetcode/target_sum/Solution.kt
@@ -1,2 +1,26 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.target_sum
 
+/** TODO: REDO */
+class Solution {
+    fun findTargetSumWays(nums: IntArray, S: Int): Int {
+        if (S > 1000) {
+            return 0
+        }
+
+        val dp = Array<IntArray>(nums.size) { IntArray(2001) }
+
+        dp[0][nums[0] + 1000] = 1
+        dp[0][-nums[0] + 1000] += 1
+
+        for (i in 1..nums.lastIndex) {
+            for (j in -1000..1000) {
+                if (dp[i - 1][j + 1000] > 0) {
+                    dp[i][j + nums[i] + 1000] += dp[i - 1][j + 1000]
+                    dp[i][j - nums[i] + 1000] += dp[i - 1][j + 1000]
+                }
+            }
+        }
+
+        return dp[nums.lastIndex][S + 1000]
+    }
+}
--- src/site/lvkun/leetcode/target_sum/SolutionTest.kt
+++ src/site/lvkun/leetcode/target_sum/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.target_sum
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testFindTargetSumWays() {
+        val solution = Solution()
+        Assert.assertEquals(5, solution.findTargetSumWays(intArrayOf(1, 1, 1, 1, 1), 3))
+    }
+}
