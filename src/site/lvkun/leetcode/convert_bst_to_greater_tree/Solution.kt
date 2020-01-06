a6fb8b3f8316611fd5bc2adb8a4b5b81138f44a7
Bruce Lv <bruce@duolingo.com>
2020-01-06 20:40:16 +0800

    Solve convert_bst_to_greater_tree

--- src/site/lvkun/leetcode/convert_bst_to_greater_tree/Solution.kt
+++ src/site/lvkun/leetcode/convert_bst_to_greater_tree/Solution.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.convert_bst_to_greater_tree
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun convertBST(root: TreeNode?): TreeNode? {
+        convertBST(root, 0)
+        return root
+    }
+
+    fun convertBST(node: TreeNode?, fromTop: Int): Int {
+        if (node == null) {
+            return fromTop
+        }
+
+        val right = convertBST(node.right, fromTop)
+
+        node.`val` += right
+
+        return convertBST(node.left, node.`val`)
+    }
+}
--- src/site/lvkun/leetcode/convert_bst_to_greater_tree/SolutionTest.kt
+++ src/site/lvkun/leetcode/convert_bst_to_greater_tree/SolutionTest.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.convert_bst_to_greater_tree
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testConvertBST() {
+        val solution = Solution()
+        Assert.assertEquals(TreeNode(18,
+                TreeNode(20),
+                TreeNode(13)), solution.convertBST(
+                TreeNode(5,
+                        TreeNode(2),
+                        TreeNode(13))))
+    }
+}
