4112d2da9f567846e5cdfef3baa123854eb74e1c
Bruce Lv <bruce@duolingo.com>
2020-01-07 22:28:42 +0800

    Solve merge_two_binary_trees

--- src/site/lvkun/leetcode/merge_two_binary_trees/Solution.kt
+++ src/site/lvkun/leetcode/merge_two_binary_trees/Solution.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.merge_two_binary_trees
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun mergeTrees(t1: TreeNode?, t2: TreeNode?): TreeNode? {
+        if (t1 == null && t2 == null) {
+            return null
+        }
+
+        val n = (t1?.`val` ?: 0) + (t2?.`val` ?: 0)
+        val node = TreeNode(n)
+
+        node.left = mergeTrees(t1?.left, t2?.left)
+        node.right = mergeTrees(t1?.right, t2?.right)
+
+        return node
+    }
+}
--- src/site/lvkun/leetcode/merge_two_binary_trees/SolutionTest.kt
+++ src/site/lvkun/leetcode/merge_two_binary_trees/SolutionTest.kt
@@ -1,2 +1,33 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.merge_two_binary_trees
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testMergeTrees() {
+        val solution = Solution()
+        Assert.assertEquals(
+                TreeNode(3,
+                        TreeNode(4,
+                                TreeNode(5),
+                                TreeNode(4)),
+                        TreeNode(5,
+                                null,
+                                TreeNode(7))),
+                solution.mergeTrees(
+                        TreeNode(1,
+                                TreeNode(3,
+                                        TreeNode(5),
+                                        null),
+                                TreeNode(2)),
+                        TreeNode(2,
+                                TreeNode(1,
+                                        null,
+                                        TreeNode(4)),
+                                TreeNode(3,
+                                        null,
+                                        TreeNode(7)))))
+    }
+}
