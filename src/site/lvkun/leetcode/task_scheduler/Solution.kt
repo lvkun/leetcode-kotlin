91ee42d011f786a612749255939c96c50f6eb690
Bruce Lv <bruce@duolingo.com>
2020-01-07 23:07:19 +0800

    Solve task_scheduler

--- src/site/lvkun/leetcode/task_scheduler/Solution.kt
+++ src/site/lvkun/leetcode/task_scheduler/Solution.kt
@@ -1,2 +1,29 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.task_scheduler
 
+/** TODO: REDO */
+class Solution {
+    fun leastInterval(tasks: CharArray, n: Int): Int {
+        val map = IntArray(26)
+        for (c in tasks) {
+            map[c -  'A']++
+        }
+        map.sort()
+
+        val maxVal = map[25] - 1
+        var idleSlots = maxVal * n
+
+        for (i in 24 downTo 0) {
+            if (map[i] <= 0) {
+                break
+            }
+
+            idleSlots -= minOf(map[i], maxVal)
+        }
+
+        return if (idleSlots > 0) {
+            idleSlots + tasks.size
+        } else {
+            tasks.size
+        }
+    }
+}
--- src/site/lvkun/leetcode/task_scheduler/SolutionTest.kt
+++ src/site/lvkun/leetcode/task_scheduler/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.task_scheduler
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testLeastInterval() {
+        val solution = Solution()
+        Assert.assertEquals(8,
+                solution.leastInterval(charArrayOf('A','A','A','B','B','B'), 2))
+    }
+}
