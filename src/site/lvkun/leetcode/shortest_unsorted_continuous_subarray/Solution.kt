1bc8a75319a0938400cb059e3bff569d660cde9f
Bruce Lv <bruce@duolingo.com>
2020-01-07 22:18:45 +0800

    Solve shortest_unsorted_continuous_subarray/

--- src/site/lvkun/leetcode/shortest_unsorted_continuous_subarray/Solution.kt
+++ src/site/lvkun/leetcode/shortest_unsorted_continuous_subarray/Solution.kt
@@ -1,2 +1,33 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.shortest_unsorted_continuous_subarray
 
+import java.util.*
+
+/** TODO: REDO */
+class Solution {
+    fun findUnsortedSubarray(nums: IntArray): Int {
+        val stack = Stack<Int>()
+        var left = nums.size
+        var right = 0
+
+        for (i in nums.indices) {
+            while (stack.isNotEmpty() && nums[stack.peek()] > nums[i]) {
+                left = minOf(left, stack.pop())
+            }
+            stack.push(i)
+        }
+
+        stack.clear()
+        for (i in nums.lastIndex downTo 0) {
+            while (stack.isNotEmpty() && nums[stack.peek()] < nums[i]) {
+                right = maxOf(right, stack.pop())
+            }
+            stack.push(i)
+        }
+
+        return if (right - left > 0) {
+            right - left + 1
+        } else {
+            0
+        }
+    }
+}
--- src/site/lvkun/leetcode/shortest_unsorted_continuous_subarray/SolutionTest.kt
+++ src/site/lvkun/leetcode/shortest_unsorted_continuous_subarray/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.shortest_unsorted_continuous_subarray
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testFindUnsortedSubarray() {
+        val solution = Solution()
+        Assert.assertEquals(3, solution.findUnsortedSubarray(intArrayOf(1,2,4,5,3)))
+        Assert.assertEquals(5, solution.findUnsortedSubarray(intArrayOf(2, 6, 4, 8, 10, 9, 15)))
+    }
+}
