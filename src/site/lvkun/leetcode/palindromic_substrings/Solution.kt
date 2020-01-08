6eeebf1e4f3d4ef4e8017bc0da8fc734dc96c724
Bruce Lv <bruce@duolingo.com>
2020-01-08 22:11:23 +0800

    Solve palindromic_substrings

--- src/site/lvkun/leetcode/palindromic_substrings/Solution.kt
+++ src/site/lvkun/leetcode/palindromic_substrings/Solution.kt
@@ -1,2 +1,32 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.palindromic_substrings
 
+class Solution {
+    fun countSubstrings(s: String): Int {
+        val dp = Array(s.length) {
+            BooleanArray(s.length)
+        }
+
+        var count = 0
+        for (i in s.indices) {
+            dp[i][i] = true
+            count++
+        }
+
+        for (length in 1 until s.length) {
+            for (i in s.indices) {
+                val j = length + i
+                if (j < s.length) {
+                    dp[i][j] = s[i] == s[j] && (if (length > 1) {
+                        dp[i + 1][j - 1]
+                    } else true)
+
+                    if (dp[i][j]) {
+                        count++
+                    }
+                }
+            }
+        }
+
+        return count
+    }
+}
--- src/site/lvkun/leetcode/palindromic_substrings/SolutionTest.kt
+++ src/site/lvkun/leetcode/palindromic_substrings/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.palindromic_substrings
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testCountSubstrings() {
+        val solution = Solution()
+        Assert.assertEquals(3, solution.countSubstrings("abc"))
+        Assert.assertEquals(6, solution.countSubstrings("aaa"))
+        Assert.assertEquals(15, solution.countSubstrings("aaaaa"))
+    }
+}
