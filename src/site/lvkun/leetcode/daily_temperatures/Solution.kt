ac390964545138b1220a6123386304e27a5894db
Bruce Lv <bruce@duolingo.com>
2020-01-08 22:31:48 +0800

    Solve daily_temperatures

--- src/site/lvkun/leetcode/daily_temperatures/Solution.kt
+++ src/site/lvkun/leetcode/daily_temperatures/Solution.kt
@@ -1,2 +1,27 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.daily_temperatures
 
+import java.util.*
+
+/** TODO: REDO */
+class Solution {
+    fun dailyTemperatures(T: IntArray): IntArray {
+        val result = IntArray(T.size)
+        val stack = Stack<Int>()
+
+        for (i in T.lastIndex downTo 0) {
+            while (stack.isNotEmpty() && T[i] >= T[stack.peek()]) {
+                stack.pop()
+            }
+
+            result[i] = if (stack.isEmpty()) {
+                0
+            } else {
+                stack.peek() - i
+            }
+
+            stack.push(i)
+        }
+
+        return result
+    }
+}
--- src/site/lvkun/leetcode/daily_temperatures/SolutionTest.kt
+++ src/site/lvkun/leetcode/daily_temperatures/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.daily_temperatures
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testDailyTemperatures() {
+        val solution = Solution()
+        Assert.assertArrayEquals(intArrayOf(1, 1, 4, 2, 1, 1, 0, 0),
+                solution.dailyTemperatures(intArrayOf(73, 74, 75, 71, 69, 72, 76, 73)))
+    }
+}
