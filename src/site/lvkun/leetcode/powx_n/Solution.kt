9d08976c115923df439c9e469cd4a0f50f795a52
Bruce Lv <bruce@duolingo.com>
2020-01-11 00:28:59 +0800

    Fix powx_n

--- src/site/lvkun/leetcode/powx_n/Solution.kt
+++ src/site/lvkun/leetcode/powx_n/Solution.kt
@@ -2,24 +2,25 @@ package site.lvkun.leetcode.powx_n
 
 class Solution {
     fun myPow(x: Double, n: Int): Double {
+        val n = n.toLong()
         if (n < 0) {
             return powInternal(1 / x, -n)
         }
         return powInternal(x, n)
     }
 
-    private fun powInternal(x: Double, n: Int): Double {
+    private fun powInternal(x: Double, n: Long): Double {
         var n = n
         var x = x
         var result = 1.0
 
         while (n >= 1) {
-            if (n % 2 == 1) {
-                n -= 1
+            if (n % 2 == 1L) {
+                n = n - 1
                 result *= x
             } else {
-                n /= 2
-                x *= x
+                n = n / 2
+                x = x * x
             }
         }
 
--- src/site/lvkun/leetcode/powx_n/SolutionTest.kt
+++ src/site/lvkun/leetcode/powx_n/SolutionTest.kt
@@ -7,8 +7,9 @@ class SolutionTest {
     @Test
     fun testMyPow() {
         val solution = Solution()
-//        Assert.assertEquals(1024.00000, solution.myPow(2.0, 10), 0.01)
-//        Assert.assertEquals(9.26100, solution.myPow(2.10000, 3), 0.01)
+        Assert.assertEquals(1024.00000, solution.myPow(2.0, 10), 0.01)
+        Assert.assertEquals(9.26100, solution.myPow(2.10000, 3), 0.01)
         Assert.assertEquals(0.25000, solution.myPow(2.00000, -2), 0.01)
+        Assert.assertEquals(0.0, solution.myPow(2.00000, -2147483648), 0.01)
     }
 }
