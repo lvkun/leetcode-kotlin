e419f6cd460371635a987836d8864d3554e67df0
Bruce Lv <bruce@duolingo.com>
2020-01-11 00:11:56 +0800

    Solve divide_two_integers

--- src/site/lvkun/leetcode/divide_two_integers/Solution.kt
+++ src/site/lvkun/leetcode/divide_two_integers/Solution.kt
@@ -1,2 +1,51 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.divide_two_integers
 
+class Solution {
+    fun Int.abs(): Long {
+        val r = this.toLong()
+        return if (r > 0) {
+            r
+        } else {
+            -r
+        }
+    }
+
+    fun divide(dividend: Int, divisor: Int): Int {
+        when (divisor) {
+            0 -> return 0
+            1 -> return dividend
+            -1 -> return if (dividend > Int.MIN_VALUE) {
+                -dividend
+            } else {
+                Int.MAX_VALUE
+            }
+        }
+
+        val positive = !(dividend > 0 && divisor < 0 || dividend < 0 && divisor > 0)
+
+        val result = divideInternal(dividend.abs(), divisor.abs())
+
+        return if (positive) {
+            result
+        } else {
+            -result
+        }
+    }
+
+    fun divideInternal(dividend: Long, divisor: Long): Int {
+        if (dividend < divisor) {
+            return 0
+        }
+
+        var count = 1
+        var sum = divisor
+        var next = sum.shl(1)
+        while (next < dividend) {
+            sum = next
+            next = sum.shl(1)
+            count = count.shl(1)
+        }
+
+        return count + divideInternal(dividend - sum, divisor)
+    }
+}
--- src/site/lvkun/leetcode/divide_two_integers/SolutionTest.kt
+++ src/site/lvkun/leetcode/divide_two_integers/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.divide_two_integers
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testDivide() {
+        val solution = Solution()
+        Assert.assertEquals(-1073741824, solution.divide(-2147483648, 2))
+        Assert.assertEquals(3, solution.divide(10, 3))
+        Assert.assertEquals(-2, solution.divide(7, -3))
+    }
+}
