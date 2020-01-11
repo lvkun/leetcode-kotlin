201768e0294e861987b8fbc7bf75f2a3d97c8091
Bruce Lv <bruce@duolingo.com>
2020-01-12 00:56:54 +0800

    Solve average_of_levels_in_binary_tre

--- src/site/lvkun/leetcode/average_of_levels_in_binary_tree/Solution.kt
+++ src/site/lvkun/leetcode/average_of_levels_in_binary_tree/Solution.kt
@@ -1,2 +1,35 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.average_of_levels_in_binary_tree
 
+import site.lvkun.leetcode.common.TreeNode
+import java.util.*
+
+class Solution {
+    fun averageOfLevels(root: TreeNode?): DoubleArray {
+        if (root == null) {
+            return DoubleArray(0)
+        }
+
+        val result = mutableListOf<Double>()
+        var queue = ArrayDeque<TreeNode>()
+        queue.offer(root)
+        while (queue.isNotEmpty()) {
+            val next = ArrayDeque<TreeNode>()
+            val level = mutableListOf<Int>()
+            while (queue.isNotEmpty()) {
+                val node = queue.poll()
+                node.left?.let {
+                    next.offer(it)
+                }
+                node.right?.let {
+                    next.offer(it)
+                }
+
+                level.add(node.`val`)
+            }
+            result.add(level.average())
+            queue = next
+        }
+
+        return result.toDoubleArray()
+    }
+}
--- src/site/lvkun/leetcode/average_of_levels_in_binary_tree/SolutionTest.kt
+++ src/site/lvkun/leetcode/average_of_levels_in_binary_tree/SolutionTest.kt
@@ -1,2 +1,18 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.average_of_levels_in_binary_tree
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testAverageOfLevels() {
+        val solution = Solution()
+        val result = solution.averageOfLevels(TreeNode(3,
+                TreeNode(9),
+                TreeNode(20,
+                        TreeNode(15),
+                        TreeNode(7))))
+        Assert.assertArrayEquals(doubleArrayOf(3.0, 14.5, 11.0), result, 0.01)
+    }
+}
