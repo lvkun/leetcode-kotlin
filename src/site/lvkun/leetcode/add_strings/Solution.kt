290b3e6076ba73a3f7a67040f88b9bd805f4c8af
Bruce Lv <bruce@duolingo.com>
2020-01-12 00:38:53 +0800

    Solve add_strings

--- src/site/lvkun/leetcode/add_strings/Solution.kt
+++ src/site/lvkun/leetcode/add_strings/Solution.kt
@@ -1,2 +1,29 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.add_strings
 
+class Solution {
+    private fun String.at(i: Int): Int {
+        if (i < 0 || i >= this.length) {
+            return 0
+        }
+
+        return this[this.length - 1 - i] - '0'
+    }
+
+    fun addStrings(num1: String, num2: String): String {
+        val maxLength = maxOf(num1.length, num2.length)
+
+        var carry = 0
+        val result = mutableListOf<Char>()
+        for (i in 0 until maxLength) {
+            val sum = num1.at(i) + num2.at(i) + carry
+            carry = sum / 10
+            result.add('0' + sum % 10)
+        }
+
+        if (carry != 0) {
+            result.add('0' + carry)
+        }
+
+        return result.reversed().joinToString("")
+    }
+}
--- src/site/lvkun/leetcode/add_strings/SolutionTest.kt
+++ src/site/lvkun/leetcode/add_strings/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.add_strings
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testAddStrings() {
+        val solution = Solution()
+        Assert.assertEquals("3", solution.addStrings("1", "2"))
+        Assert.assertEquals("2", solution.addStrings("", "2"))
+        Assert.assertEquals("17", solution.addStrings("9", "8"))
+    }
+}
