31c77327d313a23852a18fee590cd9c9d13845c7
Bruce Lv <bruce@duolingo.com>
2020-01-12 00:07:20 +0800

    Solve intersection_of_two_arrays_ii

--- src/site/lvkun/leetcode/intersection_of_two_arrays_ii/Solution.kt
+++ src/site/lvkun/leetcode/intersection_of_two_arrays_ii/Solution.kt
@@ -1,2 +1,28 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.intersection_of_two_arrays_ii
 
+class Solution {
+    fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
+        return if (nums1.size > nums2.size) {
+            intersectInternal(nums1, nums2)
+        } else {
+            intersectInternal(nums2, nums1)
+        }
+    }
+
+    private fun intersectInternal(larger: IntArray, smaller: IntArray): IntArray {
+        val map = mutableMapOf<Int, Int>()
+        smaller.forEach { i ->
+            map[i] = map.getOrDefault(i, 0) + 1
+        }
+
+        val result = mutableListOf<Int>()
+        for (i in larger) {
+            if (map.getOrDefault(i, 0) > 0) {
+                result.add(i)
+                map[i] = map.getOrDefault(i, 0) - 1
+            }
+        }
+
+        return result.toIntArray()
+    }
+}
--- src/site/lvkun/leetcode/intersection_of_two_arrays_ii/SolutionTest.kt
+++ src/site/lvkun/leetcode/intersection_of_two_arrays_ii/SolutionTest.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.intersection_of_two_arrays_ii
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIntersect() {
+        val solution = Solution()
+        Assert.assertEquals(intArrayOf(2, 2).sorted(),
+                solution.intersect(intArrayOf(1, 2, 2, 1), intArrayOf(2, 2)).sorted())
+        Assert.assertEquals(intArrayOf(4, 9).sorted(),
+                solution.intersect(intArrayOf(4, 9, 5), intArrayOf(9, 4, 9, 8, 4)).sorted())
+    }
+}
