933bd4daeab0e2c65401f26ab71ca9394fe36157
Bruce Lv <bruce@duolingo.com>
2020-01-13 23:44:31 +0800

    Solve valid_palindrome_ii

--- src/site/lvkun/leetcode/valid_palindrome_ii/Solution.kt
+++ src/site/lvkun/leetcode/valid_palindrome_ii/Solution.kt
@@ -1,2 +1,36 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.valid_palindrome_ii
 
+class Solution {
+    fun validPalindrome(s: String): Boolean {
+        var start = 0
+        var end = s.lastIndex
+
+        while (start <= end) {
+            if (s[start] == s[end]) {
+                start++
+                end--
+            } else {
+                return validPalindromeNoRemove(s, IntRange(start, end - 1)) ||
+                        validPalindromeNoRemove(s, IntRange(start + 1, end))
+            }
+        }
+
+        return true
+    }
+
+    private fun validPalindromeNoRemove(s: String, range: IntRange): Boolean {
+        var start = range.first
+        var end = range.last
+
+        while (start <= end) {
+            if (s[start] != s[end]) {
+                return false
+            }
+
+            start++
+            end--
+        }
+
+        return true
+    }
+}
--- src/site/lvkun/leetcode/valid_palindrome_ii/SolutionTest.kt
+++ src/site/lvkun/leetcode/valid_palindrome_ii/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.valid_palindrome_ii
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testValidPalindrome() {
+        val solution = Solution()
+        Assert.assertTrue(solution.validPalindrome("aba"))
+        Assert.assertTrue(solution.validPalindrome("abca"))
+    }
+}
