f67f00b5f129b8866f228e979c94df4087f1915c
Bruce Lv <bruce@duolingo.com>
2020-01-14 23:12:27 +0800

    Solve verifying_an_alien_dictionary

--- src/site/lvkun/leetcode/verifying_an_alien_dictionary/Solution.kt
+++ src/site/lvkun/leetcode/verifying_an_alien_dictionary/Solution.kt
@@ -1,2 +1,54 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.verifying_an_alien_dictionary
 
+class Solution {
+
+    private fun Char.order(orderMap: IntArray): Int {
+        val i = this - 'a'
+        if (i in orderMap.indices) {
+            return orderMap[i]
+        }
+
+        return -1
+    }
+
+    private fun String.at(i: Int): Char {
+        if (i in this.indices) {
+            return this[i]
+        }
+        return 'a' - 1
+    }
+
+    private fun String.greaterThan(other: String, orderMap: IntArray): Boolean {
+        val maxLength = maxOf(this.length, other.length)
+
+        for (i in 0 until maxLength) {
+            if (this.at(i) == other.at(i)) {
+                continue
+            }
+
+            return this.at(i).order(orderMap) > other.at(i).order(orderMap)
+        }
+
+        return false
+    }
+
+    fun isAlienSorted(words: Array<String>, order: String): Boolean {
+        val orderMap = buildOrderMap(order)
+
+        for ((before, after) in words.indices.zipWithNext()) {
+            if (words[before].greaterThan(words[after], orderMap)) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    private fun buildOrderMap(order: String): IntArray {
+        val orderMap = IntArray(order.length)
+        for (i in order.indices) {
+            orderMap[order[i] - 'a'] = i
+        }
+        return orderMap
+    }
+}
--- src/site/lvkun/leetcode/verifying_an_alien_dictionary/SolutionTest.kt
+++ src/site/lvkun/leetcode/verifying_an_alien_dictionary/SolutionTest.kt
@@ -1,2 +1,14 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.verifying_an_alien_dictionary
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testIsAlienSorted() {
+        val solution = Solution()
+        Assert.assertTrue(solution.isAlienSorted(arrayOf("hello","leetcode"), "hlabcdefgijkmnopqrstuvwxyz"))
+        Assert.assertFalse(solution.isAlienSorted(arrayOf("word","world","row"), "worldabcefghijkmnpqstuvxyz"))
+        Assert.assertFalse(solution.isAlienSorted(arrayOf("apple","app"), "abcdefghijklmnopqrstuvwxyz"))
+    }
+}
