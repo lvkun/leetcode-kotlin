59f71cd066d1f3f82393b7e8710d6f3c96a80cf3
Bruce Lv <bruce@duolingo.com>
2020-01-15 00:09:31 +0800

    Solve search_in_rotated_sorted_array_ii

--- src/site/lvkun/leetcode/search_in_rotated_sorted_array_ii/Solution.kt
+++ src/site/lvkun/leetcode/search_in_rotated_sorted_array_ii/Solution.kt
@@ -1,2 +1,33 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.search_in_rotated_sorted_array_ii
 
+class Solution {
+    fun search(nums: IntArray, target: Int): Boolean {
+        var low = 0
+        var high = nums.lastIndex
+
+        while (low < high) {
+            val mid = (low + high) / 2
+
+            when {
+                nums[mid] == target -> return true
+                nums[low] == nums[mid] -> low++
+                nums[low] < nums[mid] -> {
+                    if (nums[low] <= target && target <= nums[mid]) {
+                        high = mid
+                    } else {
+                        low = mid + 1
+                    }
+                }
+                else -> {
+                    if (nums[mid] <= target && target <= nums[high]) {
+                        low = mid + 1
+                    } else {
+                        high = mid
+                    }
+                }
+            }
+        }
+
+        return low == high && nums[low] == target
+    }
+}
--- src/site/lvkun/leetcode/search_in_rotated_sorted_array_ii/SolutionTest.kt
+++ src/site/lvkun/leetcode/search_in_rotated_sorted_array_ii/SolutionTest.kt
@@ -1,2 +1,15 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.search_in_rotated_sorted_array_ii
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testSearch() {
+        val solution = Solution()
+        Assert.assertTrue(solution.search(intArrayOf(2,5,6,0,0,1,2), 0))
+        Assert.assertFalse(solution.search(intArrayOf(2,5,6,0,0,1,2), 3))
+        Assert.assertTrue(solution.search(intArrayOf(1,3,1,1,1), 3))
+        Assert.assertTrue(solution.search(intArrayOf(1,3,1,1,1), 3))
+    }
+}
