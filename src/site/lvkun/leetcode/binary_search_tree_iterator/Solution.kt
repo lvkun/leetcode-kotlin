473d4c638e82aef4526587dd41ddea05a4f6df3a
Bruce Lv <bruce@duolingo.com>
2020-01-15 00:30:37 +0800

    Solve binary_search_tree_iterator

--- src/site/lvkun/leetcode/binary_search_tree_iterator/Solution.kt
+++ src/site/lvkun/leetcode/binary_search_tree_iterator/Solution.kt
@@ -1,2 +1,34 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_search_tree_iterator
 
+import site.lvkun.leetcode.common.TreeNode
+import java.util.*
+
+class BSTIterator(root: TreeNode?) {
+    val stack = Stack<TreeNode>()
+    init {
+        addStack(root)
+    }
+
+    private fun addStack(node: TreeNode?) {
+        var cur = node
+        while (cur != null) {
+            stack.push(cur)
+            cur = cur.left
+        }
+    }
+
+    /** @return the next smallest number */
+    fun next(): Int {
+        val cur = stack.pop()
+        val right = cur.right
+        if (right != null) {
+            addStack(right)
+        }
+        return cur.`val`
+    }
+
+    /** @return whether we have a next smallest number */
+    fun hasNext(): Boolean {
+        return stack.isNotEmpty()
+    }
+}
--- src/site/lvkun/leetcode/binary_search_tree_iterator/SolutionTest.kt
+++ src/site/lvkun/leetcode/binary_search_tree_iterator/SolutionTest.kt
@@ -1,2 +1,27 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.binary_search_tree_iterator
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testBSTIterator() {
+        val root = TreeNode(7,
+                TreeNode(3),
+                TreeNode(15,
+                        TreeNode(9),
+                        TreeNode(20)))
+
+        val iterator = BSTIterator(root)
+        Assert.assertEquals(3, iterator.next())
+        Assert.assertEquals(7, iterator.next())
+        Assert.assertTrue(iterator.hasNext())
+        Assert.assertEquals(9, iterator.next())
+        Assert.assertTrue(iterator.hasNext())
+        Assert.assertEquals(15, iterator.next())
+        Assert.assertTrue(iterator.hasNext())
+        Assert.assertEquals(20, iterator.next())
+        Assert.assertFalse(iterator.hasNext())
+    }
+}
