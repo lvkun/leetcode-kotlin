e8b88320d2bbd01bfff85a7a3a3b10b514613a88
Bruce Lv <bruce@duolingo.com>
2020-01-14 20:29:16 +0800

    Solve range_sum_of_bst

--- src/site/lvkun/leetcode/range_sum_of_bst/Solution.kt
+++ src/site/lvkun/leetcode/range_sum_of_bst/Solution.kt
@@ -1,2 +1,22 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.range_sum_of_bst
 
+import site.lvkun.leetcode.common.TreeNode
+
+class Solution {
+    fun rangeSumBST(root: TreeNode?, L: Int, R: Int): Int {
+        if (root == null) {
+            return 0
+        }
+
+        val v = root.`val`
+        if (v in L..R) {
+            return v + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R)
+        }
+
+        if (v < L) {
+            return rangeSumBST(root.right, L, R)
+        }
+
+        return rangeSumBST(root.left, L, R)
+    }
+}
--- src/site/lvkun/leetcode/range_sum_of_bst/SolutionTest.kt
+++ src/site/lvkun/leetcode/range_sum_of_bst/SolutionTest.kt
@@ -1,2 +1,30 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.range_sum_of_bst
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.TreeNode
+
+class SolutionTest {
+    @Test
+    fun testRangeSumBST() {
+        val solution = Solution()
+        Assert.assertEquals(32, solution.rangeSumBST(TreeNode(10,
+                TreeNode(5,
+                        TreeNode(3),
+                        TreeNode(7)),
+                TreeNode(15,
+                        null,
+                        TreeNode(18))), 7, 15))
+        Assert.assertEquals(23, solution.rangeSumBST(TreeNode(10,
+                TreeNode(5,
+                        TreeNode(3,
+                                TreeNode(1),
+                                null),
+                        TreeNode(7,
+                                TreeNode(6),
+                                null)),
+                TreeNode(15,
+                        TreeNode(1),
+                        TreeNode(18))), 6, 10))
+    }
+}
