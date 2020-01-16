10646a929762b1fe9c06e4431b80770b14827af3
Bruce Lv <bruce@duolingo.com>
2020-01-16 23:17:04 +0800

    Solve minimum_size_subarray_sum

--- src/site/lvkun/leetcode/minimum_size_subarray_sum/Solution.kt
+++ src/site/lvkun/leetcode/minimum_size_subarray_sum/Solution.kt
@@ -1,2 +1,37 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.minimum_size_subarray_sum
 
+class Solution {
+    fun minSubArrayLen(s: Int, nums: IntArray): Int {
+        if (nums.isEmpty()) {
+            return 0
+        }
+
+        var start = 0
+        var end = 0
+        var sum = nums[0]
+        var maxLen = Int.MAX_VALUE
+
+        while (end < nums.size) {
+            when {
+                sum < s -> {
+                    end++
+                    if (end < nums.size) {
+                        sum += nums[end]
+                    }
+                }
+                sum >= s -> {
+                    maxLen = minOf(end - start + 1, maxLen)
+
+                    sum -= nums[start]
+                    start++
+                    if (start > end) {
+                        end++
+                    }
+                }
+            }
+        }
+
+
+        return if (maxLen == Int.MAX_VALUE) 0 else maxLen
+    }
+}
--- src/site/lvkun/leetcode/minimum_size_subarray_sum/SolutionTest.kt
+++ src/site/lvkun/leetcode/minimum_size_subarray_sum/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.minimum_size_subarray_sum
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testMinSubArrayLen() {
+        val solution = Solution()
+//        Assert.assertEquals(2, solution.minSubArrayLen(7, intArrayOf(2,3,1,2,4,3)))
+        Assert.assertEquals(3, solution.minSubArrayLen(11, intArrayOf(1,2,3,4,5)))
+    }
+}
