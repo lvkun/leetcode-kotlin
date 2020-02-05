8d1088ff9489ece35f30bd831bc633a7d42eb307
Bruce Lv <bruce@duolingo.com>
2020-02-05 22:32:30 +0800

    Solve add_and_search_word_data_structure_design (partly)

--- src/site/lvkun/leetcode/add_and_search_word_data_structure_design/Solution.kt
+++ src/site/lvkun/leetcode/add_and_search_word_data_structure_design/Solution.kt
@@ -1,2 +1,53 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.add_and_search_word_data_structure_design
 
+class WordDictionary() {
+
+    /** Initialize your data structure here. */
+    private val root = Node()
+
+    /** Adds a word into the data structure. */
+    fun addWord(word: String) {
+        root.addWord(word, 0)
+    }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    fun search(word: String): Boolean {
+        return root.search(word, 0)
+    }
+
+    private class Node {
+        private val children = Array<Node?>(26) { null }
+        private var isEnd = false
+
+        fun addWord(word: String, pos: Int) {
+            if (pos == word.length) {
+                isEnd = true
+                return
+            }
+
+            val index = word[pos] - 'a'
+            children[index] = Node()
+            children[index]?.addWord(word, pos + 1)
+        }
+
+        fun search(word: String, pos: Int): Boolean {
+            if (pos == word.length) {
+                return isEnd
+            }
+
+            val c = word[pos]
+
+            if (c == '.') {
+                for (n in children) {
+                    if (n != null && n.search(word, pos + 1)) {
+                        return true
+                    }
+                }
+                return false
+            } else {
+                val index = c - 'a'
+                return children[index]?.search(word, pos + 1) ?: false
+            }
+        }
+    }
+}
--- src/site/lvkun/leetcode/add_and_search_word_data_structure_design/SolutionTest.kt
+++ src/site/lvkun/leetcode/add_and_search_word_data_structure_design/SolutionTest.kt
@@ -1,2 +1,19 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.add_and_search_word_data_structure_design
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testWordDictionary() {
+        val dictionary = WordDictionary()
+        dictionary.addWord("bad")
+        dictionary.addWord("dad")
+        dictionary.addWord("mad")
+        print(dictionary.search("."))
+        Assert.assertFalse(dictionary.search("pad"))
+        Assert.assertTrue(dictionary.search("bad"))
+        Assert.assertTrue(dictionary.search(".ad"))
+        Assert.assertTrue(dictionary.search("b.."))
+    }
+}
