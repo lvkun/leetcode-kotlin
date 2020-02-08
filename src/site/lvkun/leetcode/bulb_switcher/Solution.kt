0781401061ae9880222368ed7ffd47a9c696be21
Bruce Lv <bruce@duolingo.com>
2020-02-08 22:43:41 +0800

    Solve bulb_switcher

--- src/site/lvkun/leetcode/bulb_switcher/Solution.kt
+++ src/site/lvkun/leetcode/bulb_switcher/Solution.kt
@@ -1,2 +1,9 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.bulb_switcher
 
+import kotlin.math.sqrt
+
+class Solution {
+    fun bulbSwitch(n: Int): Int {
+        return sqrt(n.toDouble()).toInt()
+    }
+}
--- src/site/lvkun/leetcode/bulb_switcher/SolutionTest.kt
+++ src/site/lvkun/leetcode/bulb_switcher/SolutionTest.kt
@@ -1,2 +1,12 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.bulb_switcher
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testBulbSwitch() {
+        val solution = Solution()
+        Assert.assertEquals(1, solution.bulbSwitch(3))
+    }
+}
