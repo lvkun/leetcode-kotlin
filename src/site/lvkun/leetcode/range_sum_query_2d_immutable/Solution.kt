187c432850674bbcd55d90cb535d3629e5e8d6df
Bruce Lv <bruce@duolingo.com>
2020-02-08 20:39:34 +0800

    Solve range_sum_query_2d_immutable

--- src/site/lvkun/leetcode/range_sum_query_2d_immutable/Solution.kt
+++ src/site/lvkun/leetcode/range_sum_query_2d_immutable/Solution.kt
@@ -1,2 +1,49 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.range_sum_query_2d_immutable
 
+class NumMatrix(matrix: Array<IntArray>) {
+    private var row = 0
+    private var col = 0
+    private lateinit var sumLeftTop: Array<IntArray>
+
+    fun Array<IntArray>.at(r: Int, c: Int): Int {
+        if (r in this.indices && c in this[r].indices) {
+            return this[r][c]
+        }
+
+        return 0
+    }
+
+    init {
+        if (matrix.isNotEmpty() && matrix[0].isNotEmpty()) {
+            row = matrix.size
+            col = matrix[0].size
+
+            sumLeftTop = zeroMatrix(row, col)
+
+            for (r in 0 until row) {
+                for (c in 0 until col) {
+                    sumLeftTop[r][c] = sumLeftTop.at(r - 1, c) +
+                            sumLeftTop.at(r, c - 1) -
+                            sumLeftTop.at(r - 1, c - 1) +
+                            matrix[r][c]
+                }
+            }
+        }
+
+
+    }
+
+    private fun zeroMatrix(row: Int, col: Int) =
+            Array<IntArray>(row) { IntArray(col) }
+
+    fun sumRegion(row1: Int, col1: Int, row2: Int, col2: Int): Int {
+        if (row == 0 || col == 0) {
+            return 0
+        }
+
+        return sumLeftTop.at(row2, col2) -
+                sumLeftTop.at(row1 - 1, col2) -
+                sumLeftTop.at(row2, col1 - 1) +
+                sumLeftTop.at(row1 - 1, col1 - 1)
+    }
+}
--- src/site/lvkun/leetcode/range_sum_query_2d_immutable/SolutionTest.kt
+++ src/site/lvkun/leetcode/range_sum_query_2d_immutable/SolutionTest.kt
@@ -1,2 +1,24 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.range_sum_query_2d_immutable
 
+import org.junit.Assert
+import org.junit.Test
+import site.lvkun.leetcode.common.Utils
+
+class SolutionTest {
+    @Test
+    fun testSumRegion() {
+        val matrix = NumMatrix(Utils.intMatrixFromString("[\n" +
+                "  [3, 0, 1, 4, 2],\n" +
+                "  [5, 6, 3, 2, 1],\n" +
+                "  [1, 2, 0, 1, 5],\n" +
+                "  [4, 1, 0, 1, 7],\n" +
+                "  [1, 0, 3, 0, 5]\n" +
+                "]"))
+
+        Assert.assertEquals(8, matrix.sumRegion(2, 1, 4, 3))
+        Assert.assertEquals(11, matrix.sumRegion(1, 1, 2, 2))
+        Assert.assertEquals(12, matrix.sumRegion(1, 2, 2, 4))
+    }
+
+
+}
