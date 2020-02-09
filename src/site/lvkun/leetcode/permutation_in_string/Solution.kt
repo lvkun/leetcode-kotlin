047546cf4e32d81058727b6da45afd1e4dc09430
Bruce Lv <bruce@duolingo.com>
2020-02-09 12:43:39 +0800

    Solve permutation_in_string

--- src/site/lvkun/leetcode/permutation_in_string/Solution.kt
+++ src/site/lvkun/leetcode/permutation_in_string/Solution.kt
@@ -1,2 +1,61 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.permutation_in_string
 
+class Solution {
+    fun checkInclusion(s1: String, s2: String): Boolean {
+        if (s1.length > s2.length) {
+            return false
+        }
+
+        val target = Count(s1, s1.indices)
+
+        var start = 0
+        var end = s1.lastIndex
+
+        val count = Count(s2, IntRange(start, end))
+
+        if (count.equals(target)) {
+            return true
+        }
+
+        while (end + 1 < s2.length) {
+            count.remove(s2[start++])
+            count.add(s2[++end])
+
+            if (count.equals(target)) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    private class Count(val s: String, val range: IntRange) {
+        private val array = IntArray(26)
+
+        private fun Char.toIndex(): Int {
+            return this - 'a'
+        }
+
+        init {
+            for (i in range) {
+                array[s[i].toIndex()] += 1
+            }
+        }
+
+        fun add(c: Char) {
+            array[c.toIndex()] += 1
+        }
+
+        fun remove(c: Char) {
+            array[c.toIndex()] -= 1
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (other == null || other !is Count) {
+                return false
+            }
+
+            return array.contentEquals(other.array)
+        }
+    }
+}
--- src/site/lvkun/leetcode/permutation_in_string/SolutionTest.kt
+++ src/site/lvkun/leetcode/permutation_in_string/SolutionTest.kt
@@ -1,2 +1,13 @@
-package site.lvkun.leetcode.accounts_merge
+package site.lvkun.leetcode.permutation_in_string
 
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+    @Test
+    fun testCheckInclusion() {
+        val solution = Solution()
+        Assert.assertTrue(solution.checkInclusion("ab", "eidbaooo"))
+        Assert.assertFalse(solution.checkInclusion("ab", "eidboaoo"))
+    }
+}
